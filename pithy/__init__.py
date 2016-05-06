2e00e3d8810218acd500b392f8b8820e81b22526
George King <george.w.king@gmail.com>
2016-05-06 17:58:30 -0400

    pithy exports defaultdict, Counter.

--- pithy/__init__.py
+++ pithy/__init__.py
@@ -1,6 +1,7 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from functools import singledispatch
+from collections import defaultdict, Counter
 
 from .fs import *
 from .io import *
