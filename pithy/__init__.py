f11a7918bd0a503e921c42a51ecc9ea6396d6e7a
George King <george.w.king@gmail.com>
2016-05-10 10:25:42 -0400

    export sys.exit.

--- pithy/__init__.py
+++ pithy/__init__.py
@@ -1,7 +1,8 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from functools import singledispatch
 from collections import defaultdict, Counter
+from functools import singledispatch
+from sys import exit
 
 from .fs import *
 from .io import *
