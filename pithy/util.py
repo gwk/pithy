369ec8f5ddd9d47447345682cca3fb40fff90e20
George King <george.w.king@gmail.com>
2016-06-09 18:07:10 -0400

    util: clip_sufix, format_byte_count_dec.

--- pithy/util.py
+++ pithy/util.py
@@ -10,6 +10,36 @@ def set_defaults(d: dict, defaults: dict):
   return d
 
 
+def  clip_suffix(string, suffix):
+  if len(suffix) == 0: return string # need this case because string[:-0] == ''.
+  if not string.endswith(suffix): raise ValueError(string, suffix)
+  return string[:-len(suffix)]
+
+
+_byte_count_dec_magnitudes = [
+  ('B',  'byte'),
+  ('kB', 'kilobyte'),
+  ('MB', 'megabyte'),
+  ('GB', 'gigabyte'),
+  ('TB', 'terabyte'),
+  ('PB', 'petabyte'),
+  ('EB', 'exabyte'),
+  ('ZB', 'zettabyte'),
+  ('YB', 'yottabyte'),
+]
+
+def format_byte_count_dec(count, precision=2, abbreviated=True, small_ints=True):
+  if small_ints and count < 1000:
+    fmt = '{} {}'
+  else:
+    fmt = '{:.{precision}f} {}'
+    count = float(count)
+  for abbrev, full in _byte_count_dec_magnitudes:
+    if count < 1000: break
+    count /= 1000
+  return fmt.format(count, abbrev if abbreviated else plural_s(full), precision=precision)
+
+
 def memoize(sentinal=Ellipsis):
   '''
   recursive function memoization decorator.
