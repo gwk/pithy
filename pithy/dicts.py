413880d59396ef288e80a960bf5eabe8c632dddf
George King <george.w.king@gmail.com>
2016-06-11 12:16:38 -0400

    dicts DefaultByKeyDict.

---
+++ pithy/dicts.py
@@ -0,0 +1,11 @@
+
+class DefaultByKeyDict(dict):
+
+  def __init__(self, default_factory, *args, **kwargs):
+    self.default_factory = default_factory
+    super().__init__(*args, **kwargs)
+
+  def __missing__(self, key):
+    val = self.default_factory(key)
+    self[key] = val
+    return val
