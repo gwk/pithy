38c471fb1446edc7e24a56992eebfc2b5e55d10c
George King <george.w.king@gmail.com>
2016-08-03 11:41:44 -0400

    lists: list_push_index.

---
+++ pithy/lists.py
@@ -0,0 +1,8 @@
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+
+def list_push_index(list_):
+  index = len(list_)
+  list_.append(index)
+  return index
+
