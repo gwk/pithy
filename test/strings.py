7fb8dd85a9371420ff0388e08e17dc495df8951f
Emlyn Mileaf-Patel <gremleaf@gmail.com>
2016-08-03 12:39:33 -0400

    Add unit tests for all functions in strings.

--- test/strings.py
+++ test/strings.py
@@ -9,33 +9,49 @@ utest(True, string_contains, 'a', '')
 utest(True, string_contains, 'a', 'a')
 utest(False, string_contains, '', 'a')
 
-utest('postmortem', clip_prefix, 'postpostmortem', 'post')
-utest('postmortem', clip_prefix, 'postmortem', 'test', req=False)
-utest_exc(ValueError('postmortem'), clip_prefix, 'postmortem', 'test')
+utest('-rest', clip_prefix, 'prefix-rest', 'prefix')
+utest('prefix-rest', clip_prefix, 'prefix-rest', 'notpresent', req=False)
+utest_exc(ValueError('prefix-rest'), clip_prefix, 'prefix-rest', 'notpresent')
 
-utest('abcde', clip_suffix, 'abcdefg', 'fg')
-utest('antemortem', clip_suffix, 'antemortem', 'test', req=False)
-utest_exc(ValueError('antemortem'), clip_suffix, 'antemortem', 'test')
+utest('rest-', clip_suffix, 'rest-suffix', 'suffix')
+utest('rest-suffix', clip_suffix, 'rest-suffix', 'notpresent', req=False)
+utest_exc(ValueError('rest-suffix'), clip_suffix, 'rest-suffix', 'notpresent')
 # What is the difference between this and clip definition wise, and why does it work like this
 
-utest('nopetest', clip_first_prefix, 'yesnopetest', ['yes','nope'])
-utest('yesnopetest', clip_first_prefix, 'yesnopetest', ['nope','wontfind'], req=False)
-utest_exc(ValueError('yesnopetest'), clip_first_prefix, 'yesnopetest', ['nope','wontfind'])
+utest('-rest', clip_first_prefix, 'firstprefix-rest', ['firstprefix','first'])
+utest('firstprefix-rest', clip_first_prefix, 'firstprefix-rest', ['notpresent','notpresenteither'], req=False)
+utest_exc(ValueError('firstprefix-rest'), clip_first_prefix, 'firstprefix-rest', ['notpresent','notpresenteither'])
 
 utest('', plural_s, 1)
 utest('s', plural_s, 9)
 
-utest('abcde', clip_suffix, 'abcdefg', 'fg')
-utest_exc(ValueError('antemortem'), clip_suffix, 'antemortem', 'test')
+utest('rest-', clip_suffix, 'rest-suffix', 'suffix')
+utest_exc(ValueError('rest-suffix'), clip_suffix, 'rest-suffix', 'notpresent')
 
-utest('999 s', format_byte_count_dec, 999, abbreviated=False) # Broken.
-utest('123.00 B', format_byte_count_dec, 123, small_ints=False)
-utest('1000.00 kB', format_byte_count_dec, 999999)
-utest('1000.00 MB', format_byte_count_dec, 999999999)
-utest('1000.00 GB', format_byte_count_dec, 999999999999)
-utest('1000.00 TB', format_byte_count_dec, 999999999999999)
-utest('100.000 PB', format_byte_count_dec,   99999999999999999, precision=3)
-utest('100.00 EB', format_byte_count_dec,   99999999999999999999)
-utest('100.00 ZB', format_byte_count_dec,   99999999999999999999999)
-utest('100.00 YB', format_byte_count_dec,   99999999999999999999999999)
+format_test_vals = [
+  ('999 B', '999 bytes', '999.0000 B', '999.00 B', 999),
+  ('1000.00 kB', '1000.00 kilobytes', '999.9990 kB', '1000.00 kB',  999999),
+  ('1000.00 MB', '1000.00 megabytes', '1000.0000 MB', '1000.00 MB', 999999999),
+  ('1000.00 GB', '1000.00 gigabytes', '1000.0000 GB', '1000.00 GB', 999999999999),
+  ('1000.00 TB', '1000.00 terabytes', '1000.0000 TB', '1000.00 TB', 999999999999999),
+  ('100.00 PB', '100.00 petabytes', '100.0000 PB', '100.00 PB',     99999999999999999),
+  ('100.00 EB', '100.00 exabytes', '100.0000 EB', '100.00 EB',      99999999999999999999),
+  ('100.00 ZB', '100.00 zettabytes', '100.0000 ZB', '100.00 ZB',    99999999999999999999999),
+  ('100.00 YB', '100.00 yottabytes', '100.0000 YB', '100.00 YB',    99999999999999999999999999)
+]
+
+for (exp_abbrev, exp_non_abbrev, exp_precision, exp_small_ints, test_count) in format_test_vals:
+  utest(exp_abbrev, format_byte_count_dec, test_count)
+  utest(exp_precision, format_byte_count_dec, test_count, precision=4, small_ints=False)
+  utest(exp_non_abbrev, format_byte_count_dec, test_count, abbreviated=False)
+  utest(exp_small_ints, format_byte_count_dec, test_count, small_ints=False)
+
+def iter_excluding_str_test(val):
+  'iter_excluding_str returns an iterator, this is testable.'
+  try: it = iter_excluding_str(val)
+  except TypeError: return ('atom:', val)
+  else: return ('collection:', *val)
+
+utest(('atom:', 'string'), iter_excluding_str_test, 'string')
+utest(('collection:', 1, 2, 3), iter_excluding_str_test, [1,2,3])
 
