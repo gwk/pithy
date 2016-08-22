51461ab845185e0984023681a786653bfcd4953d
George King <george.w.king@gmail.com>
2016-08-22 16:47:21 -0400

    ansi: _ERR, _OUT variants based on stderr, stdout `isatty`.

--- pithy/ansi.py
+++ pithy/ansi.py
@@ -50,6 +50,11 @@ CSI n T: Scroll whole page down by n (default 1) lines. New lines are added at t
 
 import re as _re
 
+from sys import stderr, stdout
+
+is_err_tty = stderr.isatty
+is_out_tty = stdout.isatty
+
 
 # ANSI control sequence indicator.
 CSI = '\x1B['
@@ -59,9 +64,9 @@ CSI = '\x1B['
 ansi_ctrl_seq_re = _re.compile(r'\x1B\[.*?[hHJKlmsu]')
 
 
-def ansi_ctrl_seq(c, *args):
+def ansi_ctrl_seq(c, *args, on=True):
   'Format a control sequence string for command character `c` and arguments.'
-  return '{}{}{}'.format(CSI, ';'.join(str(a) for a in args), c)
+  return '{}{}{}'.format(CSI, ';'.join(str(a) for a in args), c) if on else ''
 
 
 def strip_ansi_ctrl_seq(text):
@@ -77,25 +82,50 @@ def len_strip_ansi_ctrl_seq(s):
   return l
 
 
-def ansi_sgr(*seq):
+def ansi_sgr(*seq, on=True):
   'Select Graphic Rendition control sequence string.'
-  return ansi_ctrl_seq('m', *seq)
+  return ansi_ctrl_seq('m', *seq, on=on)
 
 
 # reset command strings.
-RST, RST_BOLD, RST_UNDERLINE, RST_BLINK, RST_INVERT, RST_TXT, RST_BG = \
-(ansi_sgr(i) for i in (0, 22, 24, 25, 27, 39, 49))
+( RST, RST_ERR, RST_OUT,
+  RST_BOLD, RST_BOLD_ERR, RST_BOLD_OUT,
+  RST_UNDERLINE, RST_UNDERLINE_ERR, RST_UNDERLINE_OUT,
+  RST_BLINK, RST_BLINK_ERR, RST_BLINK_OUT,
+  RST_INVERT, RST_INVERT_ERR, RST_INVERT_OUT,
+  RST_TXT, RST_TXT_ERR, RST_TXT_OUT,
+  RST_BG, RST_BG_ERR, RST_BG_OUT,
+) = (ansi_sgr(i, on=on)
+  for i in (0, 22, 24, 25, 27, 39, 49)
+  for on in (True, is_err_tty, is_out_tty))
 
 # effect command strings.
-BOLD, UNDERLINE, BLINK, INVERT = (ansi_sgr(i) for i in (1, 4, 5, 7))
+( BOLD, BOLD_ERR, BOLD_OUT,
+  UNDERLINE, UNDERLINE_ERR, UNDERLINE_OUT,
+  BLINK, BLINK_ERR, BLINK_OUT,
+  INVERT, INVERT_ERR, INVERT_OUT
+) = (ansi_sgr(i, on=on)
+  for i in (1, 4, 5, 7)
+  for on in (True, is_err_tty, is_out_tty))
 
 # color text: dark gray, red, green, yellow, blue, magenta, cyan, light gray.
-ansi_text_primaries = tuple(ansi_sgr(i) for i in range(30, 38))
-TXT_D, TXT_R, TXT_G, TXT_Y, TXT_B, TXT_M, TXT_C, TXT_L = ansi_text_primaries
+ansi_txt_primary_indices = range(30, 38)
+ansi_txt_primaries = tuple(ansi_sgr(i) for i in ansi_txt_primary_indices)
+TXT_D, TXT_R, TXT_G, TXT_Y, TXT_B, TXT_M, TXT_C, TXT_L = ansi_txt_primaries
+TXT_D_ERR, TXT_R_ERR, TXT_G_ERR, TXT_Y_ERR, TXT_B_ERR, TXT_M_ERR, TXT_C_ERR, TXT_L_ERR = (
+  (c if is_err_tty else '') for c in  ansi_txt_primaries)
+TXT_D_OUT, TXT_R_OUT, TXT_G_OUT, TXT_Y_OUT, TXT_B_OUT, TXT_M_OUT, TXT_C_OUT, TXT_L_OUT = (
+  (c if is_out_tty else '') for c in  ansi_txt_primaries)
+
 
 # color background: dark gray, red, green, yellow, blue, magenta, cyan, light gray
-ansi_background_primaries = tuple(ansi_sgr(i) for i in range(40, 48))
-BG_D, BG_R, BG_G, BG_Y, BG_B, BG_M, BG_C, BG_L = ansi_background_primaries
+ansi_bg_primary_indices = range(40, 48)
+ansi_bg_primaries = tuple(ansi_sgr(i) for i in ansi_bg_primary_indices)
+BG_D, BG_R, BG_G, BG_Y, BG_B, BG_M, BG_C, BG_L = ansi_bg_primaries
+BG_D_ERR, BG_R_ERR, BG_G_ERR, BG_Y_ERR, BG_B_ERR, BG_M_ERR, BG_C_ERR, BG_L_ERR = (
+  (c if is_err_tty else '') for c in  ansi_bg_primaries)
+BG_D_OUT, BG_R_OUT, BG_G_OUT, BG_Y_OUT, BG_B_OUT, BG_M_OUT, BG_C_OUT, BG_L_OUT = (
+  (c if is_out_tty else '') for c in  ansi_bg_primaries)
 
 
 ERASE_LINE_F, ERASE_LINE_B, ERASE_LINE = (ansi_ctrl_seq('K', i) for i in range(3))
