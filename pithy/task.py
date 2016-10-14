6a3481e221af6f8f0b911b3a0cb1356186b1f8e7
George King <george.w.king@gmail.com>
2016-10-14 14:36:06 -0400

    missing import.

--- pithy/task.py
+++ pithy/task.py
@@ -1,5 +1,6 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
+import signal
 import shlex as _shlex
 
 from subprocess import PIPE as _pipe, Popen as _Popen
