41ec06430ad08613468ce646b94408a2e6409d29
George King <george.w.king@gmail.com>
2016-10-18 10:51:49 -0400

    fix project dir detection.

--- iotest.py
+++ iotest.py
@@ -19,7 +19,9 @@ from pithy.ansi import RST, TXT_B, TXT_R
 from pithy.immutable import Immutable
 from pithy.io import errFL, errL, errSL, fail, failF, outF, outFL, outL, outSL, raiseF, read_from_path, read_first_line_from_path, write_to_path, writeLSSL
 from pithy.string_utils import string_contains
-from pithy.fs import abs_path, is_dir, is_python3_file, list_dir, make_dirs, normalize_path, path_descendants, path_dir, path_exists, path_ext, path_join, path_name, path_name_stem, path_rel_to_current_or_abs, path_stem, find_project_dir, rel_path, remove_dir_contents, walk_dirs_up
+from pithy.fs import (abs_path, find_project_dir, is_dir, is_python3_file, list_dir, make_dirs, normalize_path,
+  path_descendants, path_dir, path_dir_or_dot, path_exists, path_ext, path_join,
+  path_name, path_name_stem, path_rel_to_current_or_abs, path_stem, rel_path, remove_dir_contents, walk_dirs_up)
 from pithy.seq import fan_seq_by_key
 from pithy.task import ProcessExpectation, ProcessTimeout, run, runC
 from pithy.type_util import is_bool, is_dict_of_str, is_dict, is_int, is_list, is_pos_int, is_set, is_set_of_str, is_str, is_str_or_list, req_type
@@ -83,7 +85,7 @@ def main():
       dir_path = path + '/'
       specified_name_stem = None
     else:
-      dir_path = path_dir(path) + '/'
+      dir_path = path_dir_or_dot(path) + '/'
       specified_name_stem = path_name_stem(path)
     proto = collect_proto(ctx, dir_path)
     collect_cases(ctx, cases, proto, dir_path, specified_name_stem)
