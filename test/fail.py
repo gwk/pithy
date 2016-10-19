a05567aeb3f806cae91742cde61093ecd18a1554
George King <george.w.king@gmail.com>
2016-10-19 13:07:48 -0400

    use single quotes.

--- test/fail.py
+++ test/fail.py
@@ -3,13 +3,13 @@
 from utest import *
 
 
-def raise_unexpected(): raise Exception("unexpected")
+def raise_unexpected(): raise Exception('unexpected')
 
 utest(True, lambda *args, **kwargs: False, 0, 1, x='x', y='y')
 utest(True, raise_unexpected)
 
-utest_exc(Exception("expected"), lambda: True)
-utest_exc(Exception("expected"), raise_unexpected)
+utest_exc(Exception('expected'), lambda: True)
+utest_exc(Exception('expected'), raise_unexpected)
 
 def gen(seq): yield from seq
 
