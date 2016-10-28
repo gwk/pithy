8c416bcfee369a00622dc2f8475e5ab7e9ff5d89
George King <george.w.king@gmail.com>
2016-10-28 12:02:49 -0400

    add out_csv function.

--- pithy/csv_utils.py
+++ pithy/csv_utils.py
@@ -1,11 +1,15 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 import csv
-from typing import Iterable, Sequence, TypeVar
+from sys import stdout
+from typing import Any, Iterable, Sequence, TypeVar
+# from typing.io import TextIOBase # mypy 0.46 chokes.
 
-
-def write_csv(f: Iterable[str], header: Sequence[str], rows: Iterable[Sequence]):
+def write_csv(f: Any, header: Sequence[str], rows: Iterable[Sequence]) -> None:
   w = csv.writer(f)
   w.writerow(header)
   w.writerows(rows)
 
+
+def out_csv(header: Sequence[str], rows: Iterable[Sequence]) -> None:
+  write_csv(f=stdout, header=header, rows=rows)
