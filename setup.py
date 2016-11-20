f5af1d9395df77227c3e25ca0f96db3d8f4a25e1
George King <george.w.king@gmail.com>
2016-11-20 14:10:57 -0500

    normalize gitignore, readme.wu, setup.py.

--- setup.py
+++ setup.py
@@ -1,33 +1,24 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-# distutils setup script.
-# users should install with: `$ pip3 install pithy`
-# developers can make a local install with: `$ pip3 install -e .`
-# upload to pypi test server with: `$ python3 setup.py sdist upload -r pypitest`
-# upload to pypi prod server with: `$ python3 setup.py sdist upload`
-
 from setuptools import setup
 
-long_description = '''
-Pithy is a small library of python utilities. Many of the functions are simple wrappers around standard library functions, but named to make scripts read more concisely. There are several families of functions with abbreviation conventions, e.g. errZ, errL, errSL, errLL all print to stderr, but with different `sep` and `end` values. Similarly, there is a family of 'run' functions for spawning subprocesses and then checking or returning various combinations of exit status code, stdout, and stderr.
 
-The result is that code may look a bit more cryptic or less traditional, but is made more correct and concise by handling boring details like stdout versus stderr and error checking/reporting minutiae correctly.
-
-The project is hosted at 'https://github.com/gwk/pithy'.
-'''
+name = 'pithy'
 
 setup(
-  name='pithy',
-  license='CC0',
+  name=name,
   version='0.0.0',
+  license='CC0',
   author='George King',
   author_email='george.w.king@gmail.com',
-  url='https://github.com/gwk/pithy',
+  url='https://github.com/gwk/' + name,
   description='Pithy is a collection of utilities for scripting in Python 3.',
-  long_description=long_description,
+  long_description=open('readme.wu').read(),
   packages=['pithy'],
-  keywords=['scripting', 'testing', 'utilities'],
-  classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+  keywords=[
+    'scripting', 'testing', 'utilities'
+  ],
+  classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers.
     'Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
