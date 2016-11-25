5284333a791ddb9415132c550e357639443f4e7d
George King <george.w.king@gmail.com>
2016-11-25 12:28:31 -0800

    normalize gitignore, makefile, setup.

--- makefile
+++ makefile
@@ -1,9 +1,14 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
+# $@: The file name of the target of the rule.
+# $<: The name of the first prerequisite.
+# $^: The names of all the prerequisites, with spaces between them.
 
-.PHONY: clean default test
 
-default: cov
+.PHONY: _default clean cov pip-develop pip-uninstall pypi-dist pypi-register pypi-upload test
+
+# First target of a makefile is the default.
+_default: cov
 
 clean:
 	rm -rf _build/*
@@ -11,5 +16,20 @@ clean:
 cov:
 	test-meta/meta-coverage.sh
 
+pip-develop:
+	pip3 install -e .
+
+pip-uninstall:
+	pip3 uninstall --yes pithy
+
+pypi-dist:
+	./setup.py sdist
+
+pypi-register:
+	./setup.py sdist register
+
+pypi-upload: pypi-dist
+	./setup.py sdist upload
+
 test:
-	./iotest.py test
+	./iotest.py -fail-fast
--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-# setuptools setup script.
-# users should install with: `$ pip3 install iotest`
-# developers can make a local install with: `$ pip3 install -e .`
-# upload to pypi test server with: `$ python3 setup.py sdist upload -r pypitest`
-# upload to pypi prod server with: `$ python3 setup.py sdist upload`
-
 from setuptools import setup
 
-
-long_description = '''\
-iotest is a small tool for testing programs based on input and output.
-'''
+name = 'iotest'
 
 setup(
-  name='iotest',
-  license='CC0',
+  name=name,
   version='0.0.0',
+  license='CC0',
   author='George King',
   author_email='george.w.king@gmail.com',
-  url='https://github.com/gwk/iotest',
+  url='https://github.com/gwk/' + name,
   description='iotest is a small tool for testing programs based on input and output.',
-  long_description=long_description,
-  install_requires=['pithy'],
-  py_modules=['iotest'],
-  entry_points = {'console_scripts': ['iotest=iotest:main']},
-  keywords=['testing'],
+  long_description=open('readme.wu').read(),
+  py_modules=[name],
+  entry_points = {'console_scripts': [
+    'iotest=iotest:main'
+  ]},
+  install_requires=[
+    'pithy'
+  ],
+  keywords=[
+    'testing'
+  ],
   classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     'Development Status :: 4 - Beta',
     'Environment :: Console',
