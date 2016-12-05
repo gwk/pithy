ab5a1d86ab428b3cc955381decf39488f054734b
George King <george.w.king@gmail.com>
2016-12-04 23:35:02 -0500

    meta: type annotations.

--- pithy/meta.py
+++ pithy/meta.py
@@ -1,9 +1,12 @@
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
 import inspect
-
+from inspect import FrameInfo
 from types import FunctionType
+from typing import Any, Callable, List, Optional, Tuple, TypeVar
 
 
-def bindings_matching(prefix=None, val_type=None, strip_prefix=True, frame='<module>'):
+def bindings_matching(prefix: str=None, val_type: type=None, strip_prefix=True, frame='<module>') -> List[Tuple[str, Any]]:
   '''
   Return (name, value) pairs of bindings from the specified frame,
   that match the specified prefix and type filters.
@@ -11,6 +14,7 @@ def bindings_matching(prefix=None, val_type=None, strip_prefix=True, frame='<mod
   or a string (the name of the target frame's function, or '<module>', the default).
   '''
   stack = inspect.stack()
+  info: Optional[FrameInfo]
   if isinstance(frame, int):
     info = stack[frame]
   elif isinstance(frame, str):
@@ -36,7 +40,7 @@ def bindings_matching(prefix=None, val_type=None, strip_prefix=True, frame='<mod
   return pairs
 
 
-def dispatcher_for_names(prefix=None, default_name=None, default_fn=None, **renames):
+def dispatcher_for_names(prefix: str=None, default_name: str=None, default_fn: Callable=None, **renames) -> Callable:
   'Creates a dispatcher function for functions starting with prefix.'
   assert prefix
   bindings = { renames.get(name, name) : fn
@@ -45,7 +49,7 @@ def dispatcher_for_names(prefix=None, default_name=None, default_fn=None, **rena
   if default_name is not None:
     if default_fn is not None:
       raise ValueError('default_name and default_fn cannot both be specified.')
-    default_fn = bindings[default]
+    default_fn = bindings[default_name]
 
   def dispatch_fn(name, *args, **kwargs):
     try:
@@ -58,14 +62,16 @@ def dispatcher_for_names(prefix=None, default_name=None, default_fn=None, **rena
   return dispatch_fn
 
 
-def rename(obj, name):
+A = TypeVar('A', bound=Any)
+
+def rename(obj: A, name: str) -> A:
   'Returns a renamed object.'
   obj.__name__ = name
   obj.__qualname__ = name
   return obj
 
 
-def main_file_path():
+def main_file_path() -> str:
   'Returns the main file path.'
-  import __main__ # type: ignore.
+  import __main__ # type: ignore
   return __main__.__file__
