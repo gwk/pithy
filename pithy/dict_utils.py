e41a241873ad9db14cf86edfdcfc43987d5c4464
George King <george.w.king@gmail.com>
2016-12-04 23:30:03 -0500

    dict_utils: type annotations.

--- pithy/dict_utils.py
+++ pithy/dict_utils.py
@@ -1,10 +1,10 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from typing import Any, Dict, Iterable, List, Mapping, MutableMapping, Sequence, MutableSequence, TypeVar, Union
+from typing import Any, Dict, Hashable, Iterable, List, Mapping, MutableMapping, MutableSequence, Sequence, Tuple, TypeVar, Union
 
 
-K = Any # TypeVar('K') # mypy 0.46 does not like this? Also, should be Hashable, which does not appear to exist.
-V = Any # TypeVar('V') # same.
+K = TypeVar('K', bound=Hashable)
+V = TypeVar('V')
 
 
 def dict_put(d: MutableMapping[K, V], k: K, v: V) -> MutableMapping[K, V]:
@@ -29,7 +29,7 @@ def dict_list_append(d: Dict[K, List[V]], k: K, v: V) -> Dict[K, List[V]]:
   return d
 
 
-def dict_list_extend(d: Dict[K, List[V]], k: K, v: V) -> Mapping[K, List[V]]:
+def dict_list_extend(d: Dict[K, List[V]], k: K, v: Iterable[V]) -> Dict[K, List[V]]:
   '''
   Extend a value on the list stored under the specified key in the dictionary.
   If the key is not present, an empty list is first inserted.
@@ -40,14 +40,17 @@ def dict_list_extend(d: Dict[K, List[V]], k: K, v: V) -> Mapping[K, List[V]]:
   return d
 
 
-def dict_set_defaults(d: MutableMapping[K, V], defaults: Union[Mapping[K, V], Sequence[V]]) -> MutableMapping[K, V]:
+def dict_set_defaults(d: MutableMapping[K, V], defaults: Union[Mapping[K, V], Iterable[Tuple[K, V]]]) -> MutableMapping[K, V]:
   '''
   Call setdefault on the dictionary for each item in `defaults`,
   which can be either dictionary-like object implementing `items()` or a sequence of pairs.
   Returns the dictionary.
   '''
-  try: it = defaults.items() # type: ignore.
-  except AttributeError: it = defaults
+  it: Iterable[Tuple[K, V]]
+  if isinstance(defaults, Mapping):
+    it = defaults.items()
+  else:
+    it = defaults
   for k, v in it:
     d.setdefault(k, v)
   return d
@@ -62,7 +65,7 @@ def dict_filter_map(d: MutableMapping[K, V], seq: Sequence[K]) -> Iterable[V]:
       pass
 
 
-class DefaultByKeyDict(dict):
+class DefaultByKeyDict(dict): # TODO: typing.
   '''
   Subclass of Dict, similar to defaultdict.
   When a key is missing, default_factory is called with the key as the sole argument.
