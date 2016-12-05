8d0c7dc9e6dec90ad3f956e0079a4ab2f1a2e135
George King <george.w.king@gmail.com>
2016-12-05 00:04:41 -0500

    transform: type annotations.

--- pithy/transform.py
+++ pithy/transform.py
@@ -1,42 +1,53 @@
 from collections import Counter
 from enum import Enum
+from typing import cast, Any, Callable, Generic, Iterable, List, TextIO, TypeVar
 
 from .fs import path_ext, path_join, path_stem
 from .io import errFL, err_progress, writeFL
-from .meta import main_file_path
 
 
-class Transformer:
+T = TypeVar('T')
+I = TypeVar('I')
+O = TypeVar('O')
+Stage = Callable[[I], O]
+Pred = Callable[[I], bool]
+Put = Callable[[I], None]
+
+
+class _DropItem(Exception):
+  'Exception signals the Transformer to immediately drop the current item and continue to the next one.'
+
+
+class Transformer(Generic[T]):
   '''
   A data transformation pipeline.
   '''
 
-  class DropItem(Exception):
-    'Exception signals the Transformer to immediately drop the current item and continue to the next one.'
 
 
-  def __init__(self, seq, log_stem='_build/', log_index_width=2, progress_frequency=0.1):
+  def __init__(self, iterable: Iterable[T], log_stem='_build/', log_index_width=2, progress_frequency=0.1) -> None:
 
-    self.seq = seq
+    self.iterable = iterable
     self.log_stem = log_stem
     self.log_index_width = log_index_width
     self.progress_frequency = progress_frequency
 
-    self.stages = []
-    self.stage_names = []
-    self.counts = []
-    self.log_files = []
+    self.stages: List[Callable] = []
+    self.stage_names: List[str] = []
+    self.counts: List[int] = []
+    self.log_files: List[TextIO] = []
 
 
-  def __enter__(self):
+  def __enter__(self) -> Transformer[T]:
     return self
 
-  def __exit__(self, exc_type, exc_val, exc_tb):
-    if exc_type: return None
+
+  def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    if exc_type: return
     self.run()
 
 
-  def _add_stage(self, name, fn):
+  def _add_stage(self, name: str, fn: Stage) -> None:
     if name in self.stage_names: # makes adding all stages quadratic time, but len(stages) is low.
       raise KeyError('Transformer: stage name is a duplicate: {}'.format(name))
     self.stage_names.append(name)
@@ -46,63 +57,62 @@ class Transformer:
       self.counts.append(-1)
 
 
-  def _mk_log_fn(self, mode, name, ext):
+  def _mk_log_fn(self, mode: str, name: str, ext: str) -> Callable[..., None]:
     'create a dedicated log file and logging function for use by a particular stage.'
     index = len(self.stages)
     path = '{}{:0{width}}-{}-{}{}'.format(self.log_stem, index, mode, name, ext,
       width=self.log_index_width)
-    f = open(path, 'w')
+    f = cast(TextIO, open(path, 'w'))
     self.log_files.append(f)
 
     counts = self.counts
     assert len(counts) == index
     counts.append(0)
 
-    def log_fn(fmt, *items):
+    def log_fn(fmt: str, *items: Any) -> None:
       counts[index] += 1
       writeFL(f, fmt, *items)
 
     return log_fn
 
 
-  def flag(self, fn):
+  def flag(self, pred: Pred) -> None:
     '''
     add a flag stage.
     the decorated function function should take an input item,
     and return a truth value indicating whether the item should be logged.
     '''
-    name = fn.__name__
+    name = pred.__name__
     log_fn = self._mk_log_fn('flag', name, '.txt')
 
-    def flag_fn(item):
-      if fn(item):
+    def flag_fn(item: I) -> I:
+      if pred(item):
         log_fn('{!r}\n', item)
       return item
 
-    self._add_stage(name, edit_fn)
+    self._add_stage(name, flag_fn)
 
 
-  def drop(self, fn):
+  def drop(self, pred: Pred) -> None:
     '''
     add a drop stage.
     the decorated function should be a predicate that takes an input item,
     and returns a truth value indicating whether the item should be dropped from the pipeline.
     drops are logged as deltas.
     '''
-    name = fn.__name__
+    name = pred.__name__
     log_fn = self._mk_log_fn('drop', name, '.diff')
-    DropItem = self.DropItem
 
-    def drop_fn(item):
-      if fn(item):
+    def drop_fn(item: I) -> I:
+      if pred(item):
         log_fn('- {!r}\n', item)
-        raise DropItem
+        raise _DropItem
       return item
 
     self._add_stage(name, drop_fn)
 
 
-  def keep(self, fn):
+  def keep(self, pred: Pred) -> None:
     '''
     add a keep stage.
     the decorated function should be a predicate that takes an input item,
@@ -110,15 +120,15 @@ class Transformer:
     no logging is performed.
     '''
 
-    def keep_fn(item):
-      if fn(item):
+    def keep_fn(item: I) -> I:
+      if pred(item):
         return item
-      raise DropItem
+      raise _DropItem
 
-    self.add_stage(fn.__name__, keep_fn)
+    self._add_stage(pred.__name__, keep_fn)
 
 
-  def edit(self, fn):
+  def edit(self, fn: Stage) -> None:
     '''
     add an edit stage.
     the decorated function should return either the original item or an edited item.
@@ -127,7 +137,7 @@ class Transformer:
     name = fn.__name__
     log_fn = self._mk_log_fn('edit', name, '.diff')
 
-    def edit_fn(item):
+    def edit_fn(item: I) -> O:
       edited = fn(item)
       if edited != item:
         log_fn('- {!r}\n+ {!r}\n', item, edited)
@@ -136,7 +146,7 @@ class Transformer:
     self._add_stage(name, edit_fn)
 
 
-  def convert(self, fn):
+  def convert(self, fn: Stage) -> None:
     '''
     add a conversion stage.
     the decorated function should convert the input item to an output item.
@@ -145,22 +155,22 @@ class Transformer:
     self._add_stage(fn.__name__, fn)
 
 
-  def put(self, fn):
+  def put(self, fn: Put) -> None:
     '''
     add an output stage.
     the decorated function should output the item, either to stdout or to a file.
-    The decorated function does not need to return the item;
+    The decorated function does not return the item;
     this is handled by the decorator wrapper.
     no logging is performed.
     '''
-    def put_fn(item):
+    def put_fn(item: I) -> I:
       fn(item)
       return item
 
     self._add_stage(fn.__name__, put_fn)
 
 
-  def run(self):
+  def run(self) -> None:
 
     if not self.stages:
       errFL('Transformer: WARNING: no transform functions found; '
@@ -168,21 +178,20 @@ class Transformer:
 
     # create local bindings to avoid attribute lookups in the loop.
     stages = self.stages
-    DropItem = self.DropItem
 
     errFL('◊ transform stages: {}.', ', '.join(self.stage_names))
 
-    for item in err_progress(self.seq, 'transform', 'items', frequency=self.progress_frequency):
+    for item in err_progress(self.iterable, 'transform', 'items', frequency=self.progress_frequency):
       try:
         for fn in stages:
           item = fn(item)
-      except DropItem:
+      except _DropItem:
         continue
 
     for f in self.log_files:
       f.close()
 
     for i, (name, count) in enumerate(zip(self.stage_names, self.counts)):
-      if count == -1: count = '-'
-      errFL('◊   {:0{width}}-{}: {}', i, name, count, width=self.log_index_width)
+      c = '-' if count == -1 else count
+      errFL('◊   {:0{width}}-{}: {}', i, name, c, width=self.log_index_width)
 
