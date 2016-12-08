5c09f4e1a6006d60e75fe43b940a5db26165568e
George King <george.w.king@gmail.com>
2016-12-07 21:54:10 -0500

    normalize makefile, setup.py.

--- setup.py
+++ setup.py
@@ -1,31 +1,30 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-# setuptools setup script.
-# users should install with: `$ pip3 install legs`
-# developers can make a local install with: `$ pip3 install -e .`
-# upload to pypi test server with: `$ python3 setup.py sdist upload -r pypitest`
-# upload to pypi prod server with: `$ python3 setup.py sdist upload`
-
 from setuptools import setup
 
 
-long_description = '''\
-Legs is a lexical analyzer generator.
-'''
+name = 'legs'
 
 setup(
-  name='legs',
-  license='CC0',
+  name=name,
   version='0.0.0',
+  license='CC0',
   author='George King',
   author_email='george.w.king@gmail.com',
-  url='https://github.com/gwk/iotest',
+  url='https://github.com/gwk/' + name,
   description='A lexical analyzer generator.',
-  long_description=long_description,
-  install_requires=['pithy'],
-  entry_points = {'console_scripts': ['legs=legs.__main__:main']},
-  keywords=['testing'],
-  classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+  long_description=open('readme.wu').read(),
+  packages=[name],
+  entry_points = {'console_scripts': [
+    'legs=legs.__main__:main'
+  ]},
+  install_requires=[
+    'pithy'
+  ],
+  keywords=[
+    'testing'
+  ],
+  classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers.
     'Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
