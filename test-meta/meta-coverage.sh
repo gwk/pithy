1e3cdcdae368641d91c9148df21c680fc7ee1218
George King <george.w.king@gmail.com>
2016-12-07 19:22:36 -0500

    comments.

--- test-meta/meta-coverage.sh
+++ test-meta/meta-coverage.sh
@@ -1,18 +1,20 @@
 
 set -e
 set -x
-# run the basic test suite under cove.
+# Run the basic test suite under cove.
 cove -output _build/basic.cove -- iotest.py -fail-fast test/basic
 
-# run the meta test suite under cove, with the main iotest process in -coverage mode.
-# this produces a .cove file for each child iotest process.
+# Run the meta test suite under cove, with the main iotest process in -coverage mode.
+# This produces a .cove file for each child iotest process.
 cove -output _build/meta.cove -- iotest.py -coverage -no-coverage-report -fail-fast test/meta
 
-# a few of the meta tests invoke `iotest -coverage` as their test command;
-# these will produce cove files where __main__ refers to something else and must be excluded;
-# we only want the ones that cover iotest.
+# A few of the meta tests invoke `iotest -coverage` as their test command;
+# These will produce cove files where __main__ refers to something else and must be excluded;
+# We only want the ones that cover iotest.
+# Tests to exclude:
+#   test-meta/coverage-gap.
 iotest_cove_files=$(find _build -name '*.cove' | grep -v coverage-gap)
 
 set +x
-# display coverage for those .cove files.
+# Display coverage for those .cove files.
 cove -coalesce $iotest_cove_files
