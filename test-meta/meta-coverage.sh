6e0dec0d021e7ff7680a6c74cf240ac0b06051ad
George King <george.w.king@gmail.com>
2017-01-18 20:42:59 -0500

    improve coverage by not excluding test/meta/coverage-gap.

--- test-meta/meta-coverage.sh
+++ test-meta/meta-coverage.sh
@@ -13,7 +13,7 @@ cove -output _build/meta.cove -- iotest.py -coverage -no-coverage-report -fail-f
 # We only want the ones that cover iotest.
 # Tests to exclude:
 #   test-meta/coverage-gap.
-iotest_cove_files=$(find _build -name '*.cove' | grep -v coverage-gap)
+iotest_cove_files=$(find _build -name '*.cove' | grep -v test-meta/coverage-gap)
 
 set +x
 # Display coverage for those .cove files.
