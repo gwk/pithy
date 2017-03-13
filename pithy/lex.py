0b81af8a5127328026268728713fce93c43aca31
George King <george.w.king@gmail.com>
2017-03-13 18:22:23 -0400

    improve lex module.

--- pithy/lex.py
+++ pithy/lex.py
@@ -2,43 +2,76 @@
 
 '''
 Simple lexing.
-* Allow first variant to be Invalid. Required?
-* If one pattern specifies a flag, all patterns are affected; maybe require all flags at the beginning?'
-* or hack in a @setFlags decorator or class function?
+Subclass Lexer and Enum to create a concrete lexer.
+
+| class Numbers(Lexer, Enum):
+|   Line = r'\n'
+|   Space = r' +'
+|   Num = r'\d+'
+
+Caveats:
+* If one pattern specifies a flag, all patterns are affected.
+  * maybe require all flags at the beginning?
+  * or hack in a @setFlags decorator or class function?
 '''
 
 import re
 from enum import Enum
-from typing import AnyStr, Iterable, re as Re
+from typing import AnyStr, Iterable, re as Re, Tuple
 
 
 class LexError(Exception): pass
 
-class LexUnescapedParenError(Exception): pass
+class LexDefinitionError(Exception): pass
 
-_unescaped_paren_re = re.compile(r'(?!\\)\((?!\?)')
+inv_re = re.compile(r'.+') # used to match all characters that did not otherwise match.
 
 
 class Lexer:
 
-  _index = None
   _regex = None
+  _inv_member = None
+
 
   @classmethod
-  def lex(cls, string: AnyStr) -> Iterable[AnyStr]:
-    if not cls._index:
-      cls._index = (None,) + tuple(cls.__members__.values())
-      for member in cls.__members__.values():
-        if _unescaped_paren_re.search(member._value_):
-          raise LexUnescapedParenError(member._value_)
-      pattern = '|'.join(f'({m._value_})' for m in cls.__members__.values())
-      cls._regex = re.compile(pattern)
+  def _compile(cls):
+    members = tuple(cls.__members__.values())
+    for i, m in enumerate(members):
+      n = m._name_
+      v = m._value_
+      if v is None:
+        if i: raise LexDefinitionError(f'member {i} {n!r} value is None (only member 0 may be signify the invalid token)')
+        continue
+      if not isinstance(v, str):
+        raise LexDefinitionError(f'member {i} {n!r} value must be a string; found {v!r}')
+      try: r = re.compile(v) # compile each expression by itself to improve error clarity.
+      except Exception as e: raise LexDefinitionError(f'member {i} {n!r} pattern is invalid: {v}') from e
+      for group_name in r.groupindex:
+        if group_name in cls.__members__:
+          raise LexDefinitionError(f'member {i} {n!r} pattern contains a conflicting capture group name: {group_name!r}')
+    if members and members[0]._value_ is None: # first member represents invalid token.
+      cls._inv_member = members[0]
+      pattern_members = members[1:]
+    else: # no invalid token; will raise on invalid input.
+      cls._inv_member = None
+      pattern_members = members
+    pattern = '|'.join(f'(?P<{m._name_}>{m._value_})' for m in pattern_members)
+    cls._regex = re.compile(pattern)
 
+
+  @classmethod
+  def lex(cls, string: AnyStr) -> Iterable[Tuple['Lexer', Re.Match]]:
+    if not cls._regex: cls._compile()
     def lex_gen():
-      prev = None
+      prev_end = 0
+      def lex_inv(pos):
+        inv_match = inv_re.match(string, prev_end, pos) # create a real match object.
+        assert inv_match is not None
+        if cls._inv_member: return (cls._inv_member, inv_match)
+        else: raise LexError(inv_match)
       for match in cls._regex.finditer(string):
-        if prev and prev.end() != match.start():
-          raise LexError(string, prev.end())
-        yield (cls._index[match.lastindex], match.group())
-
+        if prev_end < match.start(): yield lex_inv(match.start())
+        yield (cls.__members__[match.lastgroup], match)
+        prev_end = match.end()
+      if prev_end < len(string): yield lex_inv(len(string))
     return lex_gen()
--- test/lex.py
+++ test/lex.py
@@ -6,19 +6,52 @@ from enum import Enum
 from pithy.lex import *
 
 
-class Tok(Lexer, Enum):
+def test_lexer(lexer, string):
+  for token, match in lexer.lex(string):
+    yield token, match.group()
+
+
+class Numbers(Lexer, Enum):
   Line = r'\n'
   Space = r' +'
   Num = r'\d+'
-  Sym = r'(?!\d)\w+'
+
+utest_seq([(Numbers.Num, '1'), (Numbers.Space, ' '), (Numbers.Num, '20'), (Numbers.Line, '\n')],
+  test_lexer, Numbers, '1 20\n')
+
+utest_seq_exc("LexError(<_sre.SRE_Match object; span=(2, 3), match='x'>,)", test_lexer, Numbers, '1 x 2')
+utest_seq_exc("LexError(<_sre.SRE_Match object; span=(4, 5), match='x'>,)", test_lexer, Numbers, '1 2 x')
 
 
-utest_seq([(Tok.Num, '1'), (Tok.Space, ' '), (Tok.Sym, 'a'), (Tok.Line, '\n')],
-  Tok.lex, '1 a\n')
+class Words(Lexer, Enum):
+  Inv = None
+  Word = r'\w+'
 
+utest_seq([(Words.Inv, '!'), (Words.Word, 'a'), (Words.Inv, ' '), (Words.Word, 'b2'), (Words.Inv, '.')],
+  test_lexer, Words, '!a b2.')
+
+
+class NoneErrorLexer(Lexer, Enum):
+  Num = r'\d+'
+  Inv = None
+utest_exc(LexDefinitionError("member 1 'Inv' value is None (only member 0 may be signify the invalid token)"),
+  NoneErrorLexer.lex, '')
+
+class NonStringErrorLexer(Lexer, Enum):
+  Num = 0
+utest_exc(LexDefinitionError("member 0 'Num' value must be a string; found 0"),
+  NonStringErrorLexer.lex, '')
 
 class ParenErrorLexer(Lexer, Enum):
-  Paren = '('
+  Star = '*'
+utest_exc(LexDefinitionError("member 0 'Star' pattern is invalid: *"),
+  ParenErrorLexer.lex, '')
 
-utest_exc(LexUnescapedParenError('('), ParenErrorLexer.lex, '')
+class GroupNameErrorLexer(Lexer, Enum):
+  A = 'a'
+  B = '(?P<A>a)'
+utest_exc(LexDefinitionError("member 1 'B' pattern contains a conflicting capture group name: 'A'"),
+  GroupNameErrorLexer.lex, '')
+
+
 
