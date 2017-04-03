793b0df87f42166f751a678e38ef318019c1b62a
George King <george.w.king@gmail.com>
2017-04-03 09:47:37 -0400

    date: months, months_range.

--- pithy/date.py
+++ pithy/date.py
@@ -1,7 +1,11 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from datetime import date, datetime, timedelta
+from datetime import *
 from typing import Iterator
+
+
+months = ('January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December')
 
 
 def parse_datetime(string: str, fmt='%Y-%m-%d %H:%M:%S') -> datetime:
@@ -27,3 +31,13 @@ def days_range(start: date, end: date, step: int=1) -> Iterator[date]:
   while d < end:
     yield d
     d = days_from(d, days=step)
+
+
+def months_range(start: date, end: date) -> Iterator[date]:
+  '''
+  Note: this will fail for days > 28 due to February.
+  '''
+  d = start
+  while d < end:
+    yield d
+    d = d.replace(year=d.year+1, month=1) if d.month == 12 else d.replace(month=d.month+1)
