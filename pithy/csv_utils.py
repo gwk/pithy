dee10d0fc8952fe087a6037637bf0f11061c0d76
George King <george.w.king@gmail.com>
2017-05-14 12:02:02 -0400

    load_csv; header is now optional for write_csv, out_csv.

--- pithy/csv_utils.py
+++ pithy/csv_utils.py
@@ -2,23 +2,49 @@
 
 import csv
 from sys import stdout
-from typing import Any, Iterable, Iterator, Sequence, TextIO, TypeVar
+from typing import cast, Any, Iterable, Iterator, Optional, Sequence, TextIO, Tuple, TypeVar, Union
 
 
 T = TypeVar('T')
 
 
-def write_csv(f: TextIO, header: Sequence[str], rows: Iterable[Sequence]) -> None:
+def load_csv(file: TextIO,
+ dialect:Optional[str]=None,
+ delimiter:Optional[str]=None,
+ doublequote:Optional[bool]=None,
+ escapechar:Optional[str]=None,
+ quotechar:Optional[str]=None,
+ quoting:Optional[int]=None,
+ skipinitialspace:Optional[bool]=None,
+ strict:Optional[bool]=None,
+ header:Union[None, bool, Tuple[str, ...]]=None) -> Iterator[Sequence[str]]:
+  opts = { k : v for (k, v) in [
+    ('dialect', dialect),
+    ('delimiter', delimiter),
+    ('doublequote', doublequote),
+    ('escapechar', escapechar),
+    ('quotechar', quotechar),
+    ('quoting', quoting),
+    ('skipinitialspace', skipinitialspace),
+    ('strict', strict),
+    ] if v is not None }
+  reader = cast(Iterator[Sequence], csv.reader(file, dialect, **opts)) # type: ignore
+  if isinstance(header, bool):
+    if header: next(reader) # simply discard.
+  elif isinstance(header, (tuple, list)): # match expected against actual.
+    row = next(reader)
+    if row != list(header):
+      raise ValueError(f'load_csv expected header:\n{header}\nreceived:\n{row}')
+  else:
+    assert header is None
+  return reader
+
+
+def write_csv(f: TextIO, header: Optional[Sequence[str]], rows: Iterable[Sequence]) -> None:
   w = csv.writer(f) # type: ignore # not annotated in typeshed.
-  w.writerow(header)
+  if header is not None: w.writerow(header)
   w.writerows(rows)
 
 
-def out_csv(header: Sequence[str], rows: Iterable[Sequence]) -> None:
+def out_csv(header: Optional[Sequence[str]], rows: Iterable[Sequence]) -> None:
   write_csv(f=stdout, header=header, rows=rows)
-
-
-def drop1(reader: Iterable[T]) -> Iterator[T]:
-  it = iter(reader)
-  next(it)
-  return it
---
+++ test/csv_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from utest import *
+from pithy.csv_utils import *
+
+
+header = ['id', 'name']
+rows = [['a', '1'], ['b', '2']]
+
+out_csv(header=header, rows=rows)
+
+# Without header.
+with open('tmp.csv', 'w') as f:
+  write_csv(f, header=None, rows=rows)
+with open('tmp.csv', 'r') as f:
+  utest_seq(rows, load_csv, f)
+
+# With header.
+with open('tmp.csv', 'w') as f:
+  write_csv(f, header=header, rows=rows)
+with open('tmp.csv', 'r') as f:
+  utest_seq(rows, load_csv, f, header=header)
