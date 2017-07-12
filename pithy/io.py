ea1e10f5877669cd142c3079db202401104dc6bb
George King <george.w.king@gmail.com>
2017-07-11 22:53:28 -0400

    add stdin to pithy.io exports.

--- pithy/io.py
+++ pithy/io.py
@@ -1,7 +1,7 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from pprint import pprint
-from sys import argv, stdout, stderr
+from sys import argv, stdin, stdout, stderr
 from string import Template
 from typing import Any, Iterable, Iterator, TextIO, TypeVar, Union
 
