d692e2220a56e26d86ea2d19d08c9190a21bb0e3
George King <george.w.king@gmail.com>
2017-07-20 19:57:52 -0400

    task: simplify output type to str; add runCOE.

--- pithy/task.py
+++ pithy/task.py
@@ -10,7 +10,6 @@ from .alarm import AlarmManager, Timeout
 
 Env = Dict[str, str]
 Input = Union[None, int, str, bytes, BinaryIO] # int primarily for DEVNULL; could also be raw file descriptor?
-Output = Optional[str] # TODO: support binary output.
 File = Union[int, IO]
 
 
@@ -35,10 +34,6 @@ class TaskUnexpectedExit(Exception):
     self.act = act
 
 
-def _decode(s: Optional[bytes]) -> Output:
-  'Decode optional utf-8 bytes from a subprocess.'
-  return s if s is None else s.decode('utf-8')
-
 
 def launch(cmd: List[str], cwd: str=None, env: Env=None, stdin: Input=None, out: BinaryIO=None, err: BinaryIO=None, files: Sequence[File]=()) \
  -> Tuple[_Popen, Optional[bytes]]:
@@ -54,7 +49,7 @@ def launch(cmd: List[str], cwd: str=None, env: Env=None, stdin: Input=None, out:
   if isinstance(cmd, str):
     cmd = _shlex.split(cmd)
 
-  input_bytes: Union[None, bytes]
+  input_bytes: Optional[bytes]
   if isinstance(stdin, str):
     f_in = _pipe
     input_bytes = stdin.encode('utf-8')
@@ -85,7 +80,7 @@ def launch(cmd: List[str], cwd: str=None, env: Env=None, stdin: Input=None, out:
   return proc, input_bytes
 
 
-def communicate(proc: _Popen, input_bytes: bytes=None, timeout: int=0) -> Tuple[int, Optional[bytes], Optional[bytes]]:
+def communicate(proc: _Popen, input_bytes: bytes=None, timeout: int=0) -> Tuple[int, bytes, bytes]:
   '''
   Communicate with and wait for a task.
   '''
@@ -97,11 +92,11 @@ def communicate(proc: _Popen, input_bytes: bytes=None, timeout: int=0) -> Tuple[
   with AlarmManager(timeout=timeout, msg='process timed out after {timeout} seconds and was killed', on_signal=proc.kill):
     out_bytes, err_bytes = proc.communicate(input_bytes) # waits for process to complete.
 
-  return proc.returncode, out_bytes, err_bytes
+  return proc.returncode, b'' if out_bytes is None else out_bytes, b'' if err_bytes is None else err_bytes
 
 
 def run(cmd: List[str], cwd: str=None, env: Env=None, stdin: Input=None, out: BinaryIO=None, err: BinaryIO=None,
- timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> Tuple[int, Output, Output]:
+ timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> Tuple[int, str, str]:
   '''
   Run a command and return (exit_code, std_out, std_err).
   '''
@@ -117,8 +112,14 @@ def run(cmd: List[str], cwd: str=None, env: Env=None, stdin: Input=None, out: Bi
     if code != exp: # otherwise expect exact numeric code.
       raise TaskUnexpectedExit(cmd, exp, code)
 
-  return code, _decode(out_bytes), _decode(err_bytes)
+  return code, out_bytes.decode('utf8'), err_bytes.decode('utf8')
 
+
+
+def runCOE(cmd: List[str], cwd: str=None, stdin: Input=None, env: Env=None,
+ timeout: int=0, files: Sequence[File]=()) -> Tuple[int, str, str]:
+  'Run a command and return exit code, std out, std err.'
+  return run(cmd=cmd, cwd=cwd, env=env, stdin=stdin, out=_pipe, err=_pipe, timeout=timeout, files=files, exp=None)
 
 
 def runC(cmd: List[str], cwd: str=None, stdin: Input=None, out: BinaryIO=None, err: BinaryIO=None, env: Env=None,
@@ -127,50 +128,50 @@ def runC(cmd: List[str], cwd: str=None, stdin: Input=None, out: BinaryIO=None, e
   assert out is not _pipe
   assert err is not _pipe
   c, o, e = run(cmd=cmd, cwd=cwd, env=env, stdin=stdin, out=out, err=err, timeout=timeout, files=files, exp=None)
-  assert o is None
-  assert e is None
+  assert e == ''
+  assert o == ''
   return c
 
 
 def runCO(cmd: List[str], cwd: str=None, stdin: Input=None, err: BinaryIO=None, env: Env=None,
- timeout: int=0, files: Sequence[File]=()) -> Tuple[int, Output]:
+ timeout: int=0, files: Sequence[File]=()) -> Tuple[int, str]:
   'Run a command and return exit code, std out; optional err.'
   assert err is not _pipe
   c, o, e = run(cmd=cmd, cwd=cwd, env=env, stdin=stdin, out=_pipe, err=err, timeout=timeout, files=files, exp=None)
-  assert e is None
+  assert e == '', repr(e)
   return c, o
 
 
 def runCE(cmd: List[str], cwd: str=None, stdin: Input=None, out: BinaryIO=None, env: Env=None,
- timeout: int=0, files: Sequence[File]=()) -> Tuple[int, Output]:
+ timeout: int=0, files: Sequence[File]=()) -> Tuple[int, str]:
   'Run a command and return exit code, std err; optional out.'
   assert out is not _pipe
   c, o, e = run(cmd=cmd, cwd=cwd, env=env, stdin=stdin, out=out, err=_pipe, timeout=timeout, files=files, exp=None)
-  assert o is None
+  assert o == ''
   return c, e
 
 
 def runOE(cmd: List[str], cwd: str=None, stdin: Input=None, env: Env=None,
- timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> Tuple[Output, Output]:
+ timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> Tuple[str, str]:
   'Run a command and return (stdout, stderr) as strings; optional code expectation `exp`.'
   c, o, e = run(cmd=cmd, cwd=cwd, env=env, stdin=stdin, out=_pipe, err=_pipe, timeout=timeout, files=files, exp=exp)
   return o, e
 
 
 def runO(cmd: List[str], cwd: str=None, stdin: Input=None, err: BinaryIO=None, env: Env=None,
- timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> Output:
+ timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> str:
   'Run a command and return stdout as a string; optional err and code expectation `exp`.'
   assert err is not _pipe
   c, o, e = run(cmd=cmd, cwd=cwd, env=env, stdin=stdin, out=_pipe, err=err, timeout=timeout, files=files, exp=exp)
-  assert e is None
+  assert e == ''
   return o
 
 
 def runE(cmd: List[str], cwd: str=None, stdin: Input=None, out: BinaryIO=None, env: Env=None,
- timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> Output:
+ timeout: int=0, files: Sequence[File]=(), exp: TaskCodeExpectation=0) -> str:
   'Run a command and return stderr as a string; optional out and code expectation `exp`.'
   assert out is not _pipe
   c, o, e = run(cmd=cmd, cwd=cwd, env=env, stdin=stdin, out=out, err=_pipe, timeout=timeout, files=files, exp=exp)
-  assert o is None
+  assert o ==  ''
   return e
 
