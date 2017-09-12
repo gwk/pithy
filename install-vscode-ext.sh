48a5d96b0df760b4857037890bf6fea2a6e602d7
George King <george.w.king@gmail.com>
2017-09-12 15:12:12 -0400

    rename and update install-vscode-ext.sh.

---
+++ install-vscode-ext.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+set -eux
+
+VSCODE_EXT="$HOME/.vscode-insiders/extensions/plumage"
+
+rm -rf "$VSCODE_EXT"
+mkdir -p "$VSCODE_EXT"
+cp vscode/package.json "$VSCODE_EXT"
