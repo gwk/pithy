8d28b4e3a938908a579fc9cf9c6474d2dc736101
George King <george.w.king@gmail.com>
2017-09-22 11:03:53 -0400

    improve swift.py.

--- plumage/swift.py
+++ plumage/swift.py
@@ -1,113 +1,124 @@
 #!/usr/bin/env python3
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
+import re
+from argparse import ArgumentParser
+from itertools import chain
+from typing import *
 from pithy.ansi import *
 from pithy.io import *
 from pithy.fs import path_rel_to_current_or_abs
-from pithy.iterable import group_by_heads
+from pithy.iterable import group_by_heads, OnHeadless
 from pithy.lex import Lexer
-from sys import stdin
+from pithy.task import run_gen, TaskUnexpectedExit
 
 
 def main():
-    for token in lex_groups():
-      if token.lastgroup == 'path':
-        s = path_rel_to_current_or_abs(token[0])
-      else:
-        s = token[0]
+  arg_parser = ArgumentParser(description='Swift compiler wrapper.')
+  arg_parser.add_argument('-build-path', default='_build')
+  arg_parser.add_argument('-target-triple', default='x86_64-apple-macosx10.13')
+  arg_parser.add_argument('-product', default=None)
+  arg_parser.add_argument('-target', default=None)
+  arg_parser.add_argument('-test', action='store_true')
+  arg_parser.add_argument('args', nargs='*')
+  args = arg_parser.parse_args()
+
+  sub_cmd = 'test' if args.test else 'build'
+  cmd = ['swift', sub_cmd, '--build-path', args.build_path, '-Xswiftc=-target', '-Xswiftc=' + args.target_triple]
+  if args.product: cmd.extend(['--product', args.product])
+  if args.target: cmd.extend(['--target', args.target])
+  cmd.extend(args.args)
+  errSL(*cmd)
+
+  exit_code = 0
+  def run_cmd():
+    nonlocal exit_code
+    try: yield from run_gen(cmd, merge_err=True)
+    except TaskUnexpectedExit as e: exit_code = e.act
+
+  for token in lex_deduplicate_reorder(run_cmd()):
+    if token.lastgroup in diag_kinds:
+      path_abs, pos, msg = diag_re.fullmatch(token[0]).groups()
+      path = path_rel_to_current_or_abs(path_abs)
+      color = colors[token.lastgroup]
+      outZ(TXT_L, path, pos, color, msg, RST)
+    else:
+      s = token[0]
       try: color = colors[token.lastgroup]
       except KeyError: outZ(s)
       else: outZ(color, s, RST)
-      stdout.flush()
+    stdout.flush()
+  exit(exit_code)
 
 
-def lex_groups():
-  it = lexer.lex_stream(read_stdin_lines())
+def lex_deduplicate_reorder(swift_output_stream):
+  '''
+  Yield the toplevel heads, e.g. "Compile Swift Module ..." immediately.
+  Aggregate diagnostics into a buffer, then group by path heads.
+  These need to be deduplicated as of Xcode 9.0 GM.
+  '''
+  it = lexer.lex_stream(swift_output_stream)
   for token in it:
     yield token
-    if is_token_head(token): break
+    if is_toplevel(token): break
 
   group = []
+  def flush():
+    subgroups = list(group_by_heads(group, is_head=is_diag_head, headless=OnHeadless.keep))
+    group.clear()
+    d = { ''.join(t[0] for t in sg) : sg for sg in subgroups } # deduplicates.
+    return chain.from_iterable(v for (_, v) in sorted(d.items(), key=lambda p: key_by_splitting_ints(p[0])))
 
   for token in it:
-    if is_token_head(token):
-      yield from group
-      del group[:]
+    if is_toplevel(token):
+      yield from flush()
       yield token
     else:
       group.append(token)
-  yield from group
-
-
-def read_stdin_lines():
-  '''
-  Read stdin iteratively using readline.
-  This reads incrementally when stdin is connected to a pipe,
-  whereas using the file's iterator blocks.
-  '''
-  while True:
-    line = stdin.readline()
-    if not line: return
-    yield line
-
-
-def is_token_head(token) -> bool:
-  return token.lastgroup in head_tokens
-
-
-head_tokens = {'compile', 'linking', 'error_terminated', 'path'}
+  yield from flush()
 
 
 lexer = Lexer(invalid='invalid',
   patterns=dict(
     newline   = r'\n',
-    compile   = r'Compile Swift Module .+',
+    compile   = r'Compile Swift Module .+\n',
     linking   = r'Linking .+',
-    error_terminated = r'error: terminated.+',
-    path      = r'[^:]+:\d+:\d+: ', # note: do not highlight line/col separately; VSCode will not recognize the paths.
-    error     = r'error: .+',
-    warning   = r'warning: .+',
-    note      = r'note: .+',
-    hint      = r'.+',
+    error     = r'[^:\n]+:\d+:\d+: error: .+',
+    warning   = r'[^:\n]+:\d+:\d+: warning: .+',
+    note      = r'[^:\n]+:\d+:\d+: note: .+',
+    unknown_error   = '<unknown>:0: error: .+',
+    unknown_warning = '<unknown>:0: warning: .+',
+    error_terminated = r'error: terminated.+', # comes in on stderr.
     underline = r'[ ]*[~^][~^ ]*',
-    line      = r'.+',
-  ),
-  modes=dict(
-    main={
-      'newline',
-      'compile',
-      'linking',
-      'error_terminated',
-      'path',
-      'error',
-      'warning',
-      'note',
-      'hint',
-    },
-    code={
-      'newline',
-      'line',
-      'underline'
-    },
-  ),
-  transitions={
-    ('main', 'error')   : ('code', 'underline'),
-    ('main', 'warning') : ('code', 'underline'),
-    ('main', 'note')    : ('code', 'underline'),
-  })
+    other     = r'.+',
+))
+
+diag_re = re.compile(r'([^:]+)(:\d+:\d+: )(\w+: .+)')
+
+diag_kinds = ('error', 'warning', 'note')
+
+def is_toplevel(token) -> bool:
+  return token.lastgroup in {'compile', 'linking', 'error_terminated'}
+
+def is_diag_head(token) -> bool:
+  return token.lastgroup in {'error', 'warning', 'unknown_error', 'unknown_warning'}
+
+def key_by_splitting_ints(string: str) -> Tuple[Union[str, int], ...]:
+  return tuple(int(s) if s.isnumeric() else s for s in int_re.split(string))
+
+int_re = re.compile(r'(\d+)')
 
 
 colors = {
   'invalid'   : INVERT,
   'compile'   : TXT_M,
   'linking'   : TXT_M,
-  'error_terminated' : TXT_D,
-  'path'      : TXT_L,
-  #'line_col'  : TXT_C, # disabled
   'error'     : TXT_R,
   'warning'   : TXT_Y,
   'note'      : TXT_L,
-  'hint'      : TXT_G,
+  'unknown_error' : TXT_R,
+  'unknown_warning' : TXT_Y,
+  'error_terminated' : TXT_D,
   'underline' : TXT_B,
 }
 
