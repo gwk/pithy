4c08666c0cded77c8fa4782b2da48b2ddad61d41
George King <george.w.king@gmail.com>
2018-01-14 12:50:29 -0500

    change setup package name to `craft` because `craft-tools` causes weird installation errors.

--- setup.py
+++ setup.py
@@ -3,25 +3,23 @@
 from setuptools import setup
 
 
-name = 'craft'
-
 setup(
-  name='craft-tools',
+  name='craft',
   version='0.0.0',
   license='CC0',
   author='George King',
   author_email='george.w.king@gmail.com',
-  url='https://github.com/gwk/' + name,
+  url='https://github.com/gwk/craft',
   description='Craft is a build system.',
   long_description=open('readme.wu').read(),
-  packages=[name],
+  packages=['craft'],
   entry_points = {'console_scripts': [
     'craft-docs=craft.docs:main',
     'craft-mac-app=craft.mac_app:main',
     'craft-py-check=craft.py_check:main',
     'craft-swift=craft.swift:main',
     'craft-swift-utest=craft.swift_utest:main',
-    'craft-tsc=craft.tsc:main',
+    'craft-web=craft.web:main',
   ]},
   install_requires=[
     'pithy'
@@ -34,7 +32,7 @@ setup(
     'typescript',
   ],
   classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers.
-    'Development Status :: 4 - Beta',
+    'Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
