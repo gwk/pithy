a23b6f25138bb67362f00eacab64467bca2e8706
George King <george.w.king@gmail.com>
2018-05-31 15:57:36 -0400

    rewrite path_encode.

--- pithy/path_encode.py
+++ pithy/path_encode.py
@@ -1,51 +1,100 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-import os.path as _os_path
+
+from enum import Enum
+from os.path import join as path_join, split as path_split
 import urllib.parse as _parse
 
 
-def _path_encode_byte(b: int) -> str:
-  '''
-  Path encoding is similar to url percent encoding, but uses '+' as the escape character.
-  It is used to create convenient file system paths out of urls.
-  Just like url encoding, letters, digits, and the characters '_.-' are left unescaped.
-  Additionally, '%' characters are not escaped, to make prior url encoding more readable,
-  and '/' is translated to '\\' which makes the results more legible.
-  Note that '\\' is itself encoded, so the encoding is unambiguous.
-  All other bytes are encoded as "+XX", where XX is the capitalized hexadecimal byte value.
+_path_encode_table = [f'+{i:02x}' for i in range(0x100)]
 
-  ascii notes:
-  0x25: '%'
-  0x2d: '-'
-  0x2e: '.'
-  0x2f: '/'
-  0x30-39: '0'-'9'
-  0x41-5a: 'A'-'Z'
-  0x5f: '_'
-  0x61-7a: 'a'-'z'
-  '''
-  if \
-    (0x30 <= b <= 0x39) or \
-    (0x41 <= b <= 0x5a) or \
-    (0x61 <= b <= 0x7a) or \
-    b in (0x25, 0x2d, 0x2e, 0x5f):
-    return chr(b)
-  elif b == 0x2f:
-    return '\\'
-  else:
-    return '+{:2X}'.format(b)
+def _preserve(start:str, last:str) -> None:
+  for code in range(ord(start), ord(last) + 1):
+    _path_encode_table[code] = chr(code)
 
-_path_encode_table = tuple(_path_encode_byte(b) for b in range(0xff))
+_preserve('0', '9')
+_preserve('A', 'Z')
+_preserve('a', 'z')
+
+for c in '%-._': _path_encode_table[ord(c)] = c
+_path_encode_table[ord('/')] = ','
 
 
 def path_encode(string: str) -> str:
-  'Encode string into a path.'
+  '''
+  Encode string into a path.
+  Path encoding is similar to url percent encoding, but uses '+' as the indicator character.
+  It is used to create convenient file names out of urls.
+  Like url encoding, letters, digits, and the characters '_.-' are left unescaped.
+  '%' characters are not escaped, to make prior url encoding more readable.
+  '/' is translated to ',', which allows multiple path segments in the url to be squashed into a single filename.
+  Note that ',' is itself encoded, so the encoding is unambiguous.
+  All other bytes are encoded as "+XX", where XX is the uppercase hexadecimal byte value.
+  '''
   utf8 = string.encode()
   return ''.join(_path_encode_table[b] for b in utf8)
 
 
-def path_for_url(url: str) -> str:
-  'Return a path encoded from a url.'
+class PartMode(Enum):
+  OMIT = 0
+  SQUASH = 1
+  COMP = 2
+
+OMIT = PartMode.OMIT
+SQUASH = PartMode.SQUASH
+COMP = PartMode.COMP
+
+
+def path_for_url(url:str, *, split_path=False, scheme=OMIT, host:PartMode=COMP,
+ path:PartMode=COMP, query:PartMode=SQUASH, fragment:PartMode=OMIT) -> str:
+  '''
+  Return a path encoded from a url by first splitting the url into parts
+  and then applying `path_encode` to each part.
+  Empty parts are represented by a bare '+'.
+
+  `split_path`: if True, the path is translated into nested directories.
+  Otherwise, slashes are translated into commas.
+
+  `scheme`, `host`, `path`, `query`, `fragment` are all tristate PartMode flags.
+  * `None` causes that part to be omitted;
+  * `False` causes it to be squashed into the previous part;
+  * `True` causes it to be appended as a distinct part.
+
+  Note that `scheme` cannot be `SQUASH`.
+  '''
   parts = _parse.urlsplit(url) # returns five-element namedtuple.
-  name = path_encode(''.join((parts.path, parts.query, parts.fragment)))
-  return _os_path.join(parts.scheme, path_encode(parts.netloc), name)
+  comps = []
+  need_scheme_colon = False
+  def add_comp(mode:PartMode, joiner:str, part:str) -> None:
+    nonlocal need_scheme_colon
+    if mode is COMP:
+      comps.append(part)
+      need_scheme_colon = False
+    elif mode is SQUASH:
+      if need_scheme_colon:
+        assert len(comps) == 1
+        comps[0] += ':'
+        need_scheme_colon = False
+      if part:
+        comps[-1] = f'{comps[-1]}{joiner}{part}'
+
+  if scheme is not OMIT:
+    assert scheme is not SQUASH
+    comps.append(parts.scheme)
+    need_scheme_colon = True
+  add_comp(host, '//', parts.netloc)
+  if path is not OMIT:
+    p = parts.path
+    if p.startswith('/') and len(p) != 1: p = p[1:]
+    if split_path:
+      if path is SQUASH: raise Exception(f'`split_path` is set but `path` is `SQUASH`')
+      need_scheme_colon = False
+      comps.extend(path_split(p))
+    else: add_comp(path, '/', p)
+  add_comp(query, '?', parts.query)
+  add_comp(fragment, '#', parts.fragment)
+
+  res = path_join(*[path_encode(c) if c else '+' for c in comps])
+  assert res
+  assert res[0] != '/'
+  return res
--- test/path_encode.py
+++ test/path_encode.py
@@ -1,4 +1,63 @@
 #!/usr/bin/env python3
 
 from utest import *
+from urllib.parse import urlsplit
 from pithy.path_encode import *
+
+
+# Demonstrate the behavior of urlsplit.
+
+parts = urlsplit('scheme://host/dir/name?query=1&r=2#fragment')
+utest_val('scheme',       parts.scheme) # ':' suffix removed.
+utest_val('host',         parts.netloc) # '//' prefix removed.
+utest_val('/dir/name',    parts.path)
+utest_val('query=1&r=2',  parts.query) # '?' prefix removed.
+utest_val('fragment',     parts.fragment) # '#' prefix removed.
+
+# URLs with path only.
+
+parts = urlsplit('path')
+utest_val('',     parts.netloc)
+utest_val('path', parts.path)
+
+parts = urlsplit('path/')
+utest_val('',       parts.netloc)
+utest_val('path/',  parts.path)
+
+parts = urlsplit('/path')
+utest_val('',       parts.netloc)
+utest_val('/path',  parts.path)
+
+parts = urlsplit('d.com/path')
+utest_val('d.com/path', parts.path) # '.com' suffix has no effect.
+
+# URL with scheme and path.
+parts = urlsplit('scheme:path')
+utest_val('scheme', parts.scheme)
+utest_val('', parts.netloc)
+utest_val('path', parts.path)
+
+# URL with host (netloc).
+parts = urlsplit('//host')
+utest_val('', parts.scheme)
+utest_val('host', parts.netloc)
+utest_val('', parts.path)
+
+
+parts = urlsplit('scheme://')
+utest_val('scheme', parts.scheme)
+utest_val('', parts.netloc)
+utest_val('', parts.path)
+
+parts = urlsplit('scheme://host/')
+utest_val('scheme', parts.scheme)
+utest_val('host', parts.netloc)
+utest_val('/', parts.path)
+
+utest('+/+', path_for_url, 'scheme://')
+
+utest('scheme/host/path', path_for_url, 'scheme://host/path', scheme=COMP)
+utest('host/path,', path_for_url, '//host/path/')
+
+utest('scheme+3a,,host/path', path_for_url, 'scheme://host/path', scheme=COMP, host=SQUASH)
+
