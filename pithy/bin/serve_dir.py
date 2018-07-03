a41cd9b46923f5f7ad208219a82987463c3f3c5b
George King <george.w.king@gmail.com>
2018-07-03 13:22:51 -0400

    serve-dir: add `-stp` option for Safari Technology Preview.

--- pithy/bin/serve_dir.py
+++ pithy/bin/serve_dir.py
@@ -13,9 +13,10 @@ def main() -> None:
   parser = ArgumentParser(description='Serve files from a directory.')
   parser.add_argument('root', default='.', nargs='?', help='Root directory to serve from')
   parser.add_argument('-browse', action='store_true', help='Launch the default system browser')
-  parser.add_argument('-safari',  action='store_true', help='Launch Safari')
   parser.add_argument('-chrome',  action='store_true', help='Launch Google Chrome')
   parser.add_argument('-firefox', action='store_true', help='Launch Firefox')
+  parser.add_argument('-safari',  action='store_true', help='Launch Safari')
+  parser.add_argument('-stp',  action='store_true', help='Launch Safari Technology Preview')
 
   args = parser.parse_args()
   root = args.root
@@ -35,9 +36,10 @@ def main() -> None:
   # relying on the fact that together the OS and the launched browser take more time to initiate the request
   # after the `open` process completes than the server does to initialize.
   if args.browse:   run(['open', addr_str])
-  if args.safari:   run(['open', '-a', 'safari',        addr_str])
   if args.chrome:   run(['open', '-a', 'google chrome', addr_str])
   if args.firefox:  run(['open', '-a', 'firefox',       addr_str])
+  if args.safari:   run(['open', '-a', 'safari',        addr_str])
+  if args.stp:      run(['open', '-a', 'safari technology preview', addr_str])
 
   try: server.serve_forever()
   except KeyboardInterrupt:
