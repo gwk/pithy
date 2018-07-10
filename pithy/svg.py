8b5a81e14c5d59f2d609e34a043ff6e32ef096cd
George King <george.w.king@gmail.com>
2018-07-10 10:06:59 -0700

    svg plot series ids now setdefault to name.

--- pithy/svg.py
+++ pithy/svg.py
@@ -342,6 +342,7 @@ class XYSeries(PlotSeries):
     self.name = name
     self.points = list(points) # TODO: clamp points to visible range, possibly plus margin.
     self.plotter = plotter
+    attrs.setdefault('id', name)
     self.attrs = attrs
     self.bounds = None
     if self.points:
@@ -374,7 +375,7 @@ class LineSeries(XYSeries):
 
   def render(self, svg:SvgBase, transform:PointTransform) -> None:
     with svg.g(**self.attrs):
-      svg.polyline(points=(transform(p) for p in self.points), class_='series', fill='none', id=self.name)
+      svg.polyline(points=(transform(p) for p in self.points), class_='series', fill='none')
       # TODO: option to fill polylines.
       if self.plotter is not None:
         for p in self.points: self.plotter(svg, transform, p)
