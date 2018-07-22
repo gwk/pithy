8728730c74fccf3fc8c26af006698cbafc74a5c7
George King <george.w.king@gmail.com>
2018-07-22 15:01:21 -0400

    html forms.

--- pithy/html.py
+++ pithy/html.py
@@ -63,6 +63,26 @@ class HtmlWriter(XmlWriter):
    vx:Num=None, vy:Num=None, vw:Num=None, vh:Num=None, **attrs:Any) -> SvgWriter:
     return SvgWriter(file=self.file, pos=pos, size=size, x=x, y=y, w=w, h=h, vx=vx, vy=vy, vw=vw, vh=vh, **attrs)
 
+  # Forms.
+
+  def form(self, **attrs:Any) -> XmlWriter: return self.sub('form', attrs=attrs)
+
+  def input(self, type:str, **attrs:Any) -> None:
+    assert type in form_input_types
+    attrs['type'] = type
+    return self.leaf('input', attrs=attrs)
+
+  def input_submit(self, **attrs:Any) -> None:
+    attrs['type'] = 'submit'
+    return self.leaf('input', attrs=attrs)
+
+  def input_text(self, **attrs:Any) -> None:
+    attrs['type'] = 'text'
+    return self.leaf('input', attrs=attrs)
+
+  def label(self, text:str, **attrs:Any) -> None:
+    return self.leaf_text('label', text=text, attrs=attrs)
+
   # Tables.
 
   # TODO: return XmlWriter subclasses that enforce correct permitted parent/child structures.
@@ -109,3 +129,29 @@ class TH:
   def __init__(self, contents:Any, **attrs:Any) -> None:
     self.contents = contents
     self.attrs = attrs
+
+
+form_input_types = frozenset({
+  'button', # push button with no default behavior.
+  'checkbox', # check box allowing single values to be selected/deselected.
+  'color', # control for specifying a color. A color picker's UI has no required features other than accepting simple colors as text (more info).
+  'date', # control for entering a date (year, month, and day, with no time).
+  'datetime-local', # control for entering a date and time, with no time zone.
+  'email', # field for editing an e-mail address.
+  'file', # control that lets the user select a file. Use `accept` to define the types of files that the control can select.
+  'hidden', # control that is not displayed but whose value is submitted to the server.
+  'image', # graphical submit button. `src` specifies the image and `alt` specifies alternative text. Use the height and width attributes to define the size of the image in pixels.
+  'month', # control for entering a month and year, with no time zone.
+  'number', # control for entering a number.
+  'password', # single-line text field whose value is obscured. Use the maxlength and minlength attributes to specify the maximum length of the value that can be entered.
+  'radio', # radio button, allowing a single value to be selected out of multiple choices.
+  'range', # control for entering a number whose exact value is not important.
+  'reset', # button that resets the contents of the form to default values.
+  'search', # single-line text field for entering search strings. Line-breaks are automatically removed from the input value.
+  'submit', # button that submits the form.
+  'tel', # control for entering a telephone number.
+  'text', # single-line text field. Line-breaks are automatically removed from the input value.
+  'time', # control for entering a time value with no time zone.
+  'url', # field for entering a URL.
+  'week', # control for entering a date consisting of a week-year number and a week number with no time zone.
+})
