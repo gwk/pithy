eba0023d51d22c127e962e28824f1a232c4d9aca
George King <george.w.king@gmail.com>
2018-07-27 21:32:10 -0400

    plot titles and ticks.

--- pithy/svg.py
+++ pithy/svg.py
@@ -19,7 +19,9 @@ F2 = Tuple[float, float]
 F2OrF = Union[F2, float]
 PathCommand = Tuple
 
-PointTransform = Callable[[Tuple], Vec]
+PointTransform = Callable[[Tuple], F2]
+
+Tick = Union[bool, Callable[[float], Any]]
 
 
 class SvgBase(XmlWriter):
@@ -212,7 +214,7 @@ class SvgBase(XmlWriter):
     if alignment_baseline is not None and alignment_baseline not in alignment_baselines: raise ValueError(alignment_baseline)
     add_opt_attrs(attrs, x=fmt_num(x), y=fmt_num(y), alignment_baseline=alignment_baseline)
     assert text is not None
-    self.leaf_text('text', attrs, text)
+    self.leaf_text('text', attrs=attrs, text=text)
 
 
   def use(self, id:str, pos:Vec=None, size:VecOrNum=None, *, x:Num=None, y:Num=None, w:Num=None, h:Num=None, **attrs:Any) -> None:
@@ -234,7 +236,7 @@ class SvgBase(XmlWriter):
   # High level.
 
   def grid(self, pos:Vec=(0,0), size:VecOrNum=(256,256), *,
-   step:VecOrNum=16, off:Vec=(0, 0), r:VecOrNum=None, **attrs:Any) -> None:
+   step:VecOrNum=16, off:Vec=(0, 0), corner_radius:VecOrNum=None, **attrs:Any) -> None:
     # TODO: per-axis log-transform option.
     x, y = f2_for_vec(pos)
     w, h = unpack_VecOrNum(size)
@@ -251,17 +253,35 @@ class SvgBase(XmlWriter):
     with self.g(**attrs):
       for tick in NumRange(x_start, x_end, sx): self.line((tick, y), (tick, y_end)) # Vertical lines.
       for tick in NumRange(y_start, y_end, sy): self.line((x, tick), (x_end, tick)) # Horizontal lines.
-      self.rect(class_=class_+'-border', x=x, y=y, w=w, h=h, r=r, fill='none')
+      self.rect(class_=class_+'-border', x=x, y=y, w=w, h=h, r=corner_radius, fill='none')
 
 
-  def plot(self, pos:Vec=(0,0), size:Vec=(512,1024), *, series:Sequence['PlotSeries'],
-   min_x=None, max_x=None, min_y=None, max_y=None,
+  def plot(self, pos:Vec=(0,0), size:Vec=(512,1024),
+   *, series:Sequence['PlotSeries'],
+   title:str=None,
+   title_h:Num=0,
+   tick_h:Num=0, tick_w:Num=0,
+   min_x:Num=None, max_x:Num=None, min_y:Num=None, max_y:Num=None,
    visible_x0=False, visible_y0=False, symmetric_x=False, symmetric_y=False, symmetric_xy=False,
-   r:VecOrNum=None, grid_step:VecOrNum=None, **attrs:Any) -> 'Plot':
+   corner_radius:VecOrNum=None,
+   grid_step:VecOrNum=5,
+   tick_step:VecOrNum=10,
+   tick_x:Tick=False, tick_y:Tick=False,
+   dbg=False,
+   **attrs:Any) -> 'Plot':
     return Plot(file=self.file, pos=pos, size=size, series=series,
-     min_x=min_x, max_x=max_x, min_y=min_y, max_y=max_y,
-     visible_x0=visible_x0, visible_y0=visible_y0, symmetric_x=symmetric_x, symmetric_y=symmetric_y, symmetric_xy=symmetric_xy,
-     r=r, grid_step=grid_step, **attrs)
+      title=title,
+      title_h=title_h,
+      tick_h=tick_h, tick_w=tick_w,
+      min_x=min_x, max_x=max_x, min_y=min_y, max_y=max_y,
+      visible_x0=visible_x0, visible_y0=visible_y0, symmetric_x=symmetric_x, symmetric_y=symmetric_y, symmetric_xy=symmetric_xy,
+      corner_radius=corner_radius,
+      grid_step=grid_step,
+      tick_step=tick_step,
+      tick_x=tick_x,
+      tick_y=tick_y,
+      dbg=dbg,
+      **attrs)
 
 
 class SvgWriter(SvgBase):
@@ -323,21 +343,19 @@ class XYSeries(PlotSeries):
 
   def __init__(self, name:str, points:Sequence[Tuple], plotter:Optional[Plotter]=circle_plotter(), **attrs:Any) -> None:
     self.name = name
-    self.points = list(points) # TODO: clamp points to visible range, possibly plus margin.
+    self.points = list(points)
     self.plotter = plotter
     attrs.setdefault('id', name)
     attrs.setdefault('class_', 'series')
     self.attrs = attrs
-    self.bounds = None
+    self.bounds:Optional[Tuple[F2, F2]] = None
     if self.points:
       x, y = self.points[0][:2]
-      min_x = x
-      max_x = x
-      min_y = y
-      max_y = y
+      min_x = max_x = float(x)
+      min_y = max_y = float(y)
       for p in self.points:
-        x = p[0]
-        y = p[1]
+        x = float(p[0])
+        y = float(p[1])
         min_x = min(min_x, x)
         max_x = max(max_x, x)
         min_y = min(min_y, y)
@@ -370,32 +388,46 @@ class Plot(SvgBase):
 
   def __init__(self, file:TextIO, pos:Vec=(0,0), size:Vec=(512,1024),
    *, series:Sequence[PlotSeries],
-   min_x=None, max_x=None, min_y=None, max_y=None,
+   title:str=None,
+   title_h:Num=0,
+   tick_h:Num=0, tick_w:Num=0,
+   min_x:Num=None, max_x:Num=None, min_y:Num=None, max_y:Num=None,
    visible_x0=False, visible_y0=False, symmetric_x=False, symmetric_y=False, symmetric_xy=False,
-   r:VecOrNum=None, grid_step:VecOrNum=None,
-   title:str=None, title_h:float=0.0,
+   corner_radius:VecOrNum=None,
+   grid_step:VecOrNum=5,
+   tick_step:VecOrNum=10,
+   tick_x:Tick=False, tick_y:Tick=False,
+   dbg=False,
    **attrs:Any) -> None:
 
+    pos = f2_for_vec(pos)
+    # Initialize as `g` element.
+    attrs.setdefault('class_', 'plot')
+    attrs['transform'] = translate(*pos)
+    super().__init__(tag='g', file=file, attrs=attrs)
+
+    title_h = float(title_h)
+
     self.pos = pos
-    self.size = size
+    self.size = size = f2_for_vec(size)
     self.series = series
+    self.title = title
+    self.title_h = title_h = float(title_h)
+    self.tick_w = tick_w = float(tick_w)
+    self.tick_h = tick_h = float(tick_h)
     self.visible_x0 = visible_x0
     self.visible_y0 = visible_y0
     self.symmetric_x = symmetric_x
     self.symmetric_y = symmetric_y
     self.symmetric_xy = symmetric_xy
-    self.r = r
-    self.grid_step = grid_step
+    self.corner_radius = corner_radius
+    self.grid_step = grid_step = unpack_VecOrNum(grid_step)
+    self.tick_step = tick_step = unpack_VecOrNum(tick_step)
 
-    self.title = title
-    if title is None:
-      self.title_h = 0.0
-    elif title_h == 0.0: raise ValueError('Plot.title_h must be positive')
-    else:
-      self.title_h = title_h
-
+    self.tick_x = tick_x
+    self.tick_y = tick_y
     self.w = size[0]
-    self.h = size[1] + self.title_h
+    self.h = size[1]
 
     # Determine bounds.
     expand_min_x = min_x is None
@@ -411,17 +443,17 @@ class Plot(SvgBase):
         if expand_min_y: min_y = mny if (min_y is None) else min(min_y, mny)
         if expand_max_y: max_y = mxy if (max_y is None) else max(max_y, mxy)
 
-    if min_x is None: min_x = 0
-    if min_y is None: min_y = 0
-    if max_x is None or max_x <= min_x: max_x = min_x + 1
-    if max_y is None or max_y <= min_y: max_y = min_y + 1
+    if min_x is None: min_x = 0.0
+    if min_y is None: min_y = 0.0
+    if max_x is None or max_x <= min_x: max_x = min_x + 1.0
+    if max_y is None or max_y <= min_y: max_y = min_y + 1.0
 
     if visible_x0:
-      if min_x > 0: min_x = 0
-      elif max_x < 0: max_x = 0
+      if min_x > 0.0: min_x = 0.0
+      elif max_x < 0.0: max_x = 0.0
     if visible_y0:
-      if min_y > 0: min_y = 0
-      elif max_y < 0: max_y = 0
+      if min_y > 0.0: min_y = 0.0
+      elif max_y < 0.0: max_y = 0.0
 
     if symmetric_xy:
       max_x = max_y = max(max_x, -min_x, max_y, -min_y)
@@ -434,53 +466,109 @@ class Plot(SvgBase):
         max_y = max(max_y, -min_y)
         min_y = -max_y
 
-    self.min_x = min_x
-    self.max_x = max_x
-    self.min_y = min_y
-    self.max_y = max_y
 
-    data_w = max_x - min_x
-    data_h = max_y - min_y
-    self.data_w = data_w
-    self.data_h = data_h
+    min_x = float(min_x)
+    max_x = float(max_x)
+    min_y = float(min_y)
+    max_y = float(max_y)
 
-    scale_x = size[0] / data_w
-    scale_y = size[1] / data_h
+    self.min_x:float = min_x
+    self.max_x:float = max_x
+    self.min_y:float = min_y
+    self.max_y:float = max_y
+
+    self.data_w = data_w = max_x - min_x
+    self.data_h = data_h = max_y - min_y
+
+    self.grid_x = grid_x = tick_w
+    self.grid_y = grid_y = title_h
+    self.grid_pos = grid_pos = (grid_x, grid_y)
+
+    self.grid_w = grid_w = self.w - tick_w - 1
+    self.grid_h = grid_h = self.h - title_h - tick_h - 1
+    self.grid_size = grid_size = (grid_w, grid_h)
+
+    self.grid_r = grid_r = grid_x + grid_w
+    self.grid_b = grid_b = grid_y + grid_h
+
+    self.scale_x = scale_x = grid_w / data_w
+    self.scale_x = scale_y = grid_h / data_h
     self.scale = (scale_x, scale_y)
 
-    def transform(point:tuple) -> Vec:
-      return ((point[0]-min_x) * scale_x, (data_h - (point[1]-min_y)) * scale_y)
+    def transform(point:tuple) -> F2:
+      'Translate a point to appear coincident with the data space.'
+      x = float(point[0])
+      y = float(point[1])
+      return (grid_x + scale_x*(x-min_x), grid_y + scale_y*(data_h - (y-min_y))) # type: ignore
+
+    def transform_x(x:Num) -> float: return grid_x + scale_x*(float(x) - min_x) # type: ignore
+    def transform_y(y:Num) -> float: return grid_y + scale_y*(data_h - (float(y)-min_y)) # type: ignore
 
     self.transform = transform
+    self.transform_x = transform_x
+    self.transform_y = transform_y
 
-    y = self.title_h
-    # Initialize as `g` element.
-    attrs.setdefault('class_', 'plot')
-    attrs['transform'] = translate(pos[0], pos[1] + y)
-    super().__init__(tag='g', file=file, attrs=attrs)
+    if dbg:
+      def dbg_rect(pos:Vec, size:Vec, fill:str) -> None:
+        self.rect(pos, size, stroke=None, fill=fill, opacity=0.1)
+    else:
+      def dbg_rect(pos:Vec, size:Vec, fill:str) -> None: pass
+
+    dbg_rect((0, 0), self.size, fill='#000')
 
     # Title.
+    dbg_rect((0, 0), (self.w, title_h), fill='#F00')
+
     if self.title is not None:
-      self.text((0, -y), text=self.title, class_='title', text_anchor='left', alignment_baseline='hanging')
+      self.text((tick_w, title_h/2), text=self.title, class_='title', text_anchor='left', alignment_baseline='middle')
 
     # Grid.
+    grid_trans = translate(grid_x, grid_y+grid_h)
     if grid_step is not None:
-      if isinstance(grid_step, tuple):
-        step_x, step_y = grid_step
-      else:
-        step_x = step_y = grid_step
-      grid_w = data_w*scale_x
-      grid_h = data_h*scale_y
-      off_x = scale_x * (step_x - min_x % step_x)
-      off_y = scale_y * (step_y - min_y % step_y)
-      self.grid(pos=(0, 0), size=(grid_w, grid_h), step=(step_x*scale_x, step_y*scale_y), off=(off_x, off_y), r=r,
-        transform=f'{scale(1,-1)} {translate(0, -grid_h)}')
+      gsx, gsy = unpack_VecOrNum(grid_step)
+      g_start_x = (min_x//gsx + 1) * gsx # Skip line index 0 because it is always <= low border.
+      g_start_y = (min_y//gsy + 1) * gsy # Skip line index 0 because it is always <= low border.
+    # TODO: if we are really going to support rounded corners then the border rect should clip the interior lines.
+    with self.g(class_='grid'):
+      for g in NumRange(g_start_x, max_x, gsx): # X axis.
+        gx = transform_x(g)
+        self.line((gx, grid_y), (gx, grid_b)) # Vertical lines.
+      for g in NumRange(g_start_y, max_y, gsy):
+        gy = transform_y(g)
+        self.line((grid_x, gy), (grid_r, gy)) # Horizontal lines.
+      self.rect(class_='grid-border', pos=grid_pos, size=grid_size, r=corner_radius, fill='none')
 
     # Axes.
-    if min_y <= 0 and max_y >= 0: # Draw x axis.
-      self.line(transform((0, min_y)), transform((0, max_y)), class_='axis', id='x-axis')
-    if min_x <= 0 and max_x >= 0: # Draw y axis.
-      self.line(transform((min_x, 0)), transform((max_x, 0)), class_='axis', id='y-axis')
+    if min_y <= 0 and max_y >= 0: # Draw X axis.
+      x0 = transform_x(0)
+      self.line((x0, grid_y), (x0, grid_b), class_='axis', id='x-axis')
+    if min_x <= 0 and max_x >= 0: # Draw Y axis.
+      y0 = transform_y(0)
+      self.line((grid_x, y0), (grid_r, y0), class_='axis', id='y-axis')
+
+    # Ticks.
+    dbg_rect((grid_x, grid_b), (grid_w, tick_h), fill='#088') # X axis ticks.
+    dbg_rect((0, grid_y), (tick_w, grid_h), fill='#08F') # Y axis ticks.
+
+    tsx, tsy = unpack_VecOrNum(tick_step)
+    txi, txr = divmod(min_x, tsx)
+    tyi, tyr = divmod(min_y, tsy)
+    t_start_x = txi*tsx + (txr and tsx)
+    t_start_y = tyi*tsx + (tyr and tsy)
+
+    if tick_x:
+      if not callable(tick_x): tick_x = str
+      for x in NumRange(t_start_x, max_x, step=tsx, closed=False):
+        tx = transform_x(x)
+        self.line((tx, grid_b), (tx, grid_b+2), class_='tick')
+        self.text((tx+1, grid_b+2), class_='tick', text=tick_x(x), alignment_baseline='hanging')
+    if tick_y:
+      if not callable(tick_y): tick_y = str
+      for y in NumRange(t_start_y, max_y, step=tsy, closed=True):
+        tx = grid_x - 2
+        ty = transform_y(y)
+        self.line((tx, ty), (grid_x, ty), class_='tick')
+        self.text((tx-1, ty), class_='tick', text=tick_y(y), text_anchor='end', alignment_baseline='middle')
 
     # Series.
     for s in series:
@@ -491,8 +579,8 @@ class Plot(SvgBase):
 # Transforms.
 
 def scale(x:Num=1, y:Num=1) -> str: return f'scale({x},{y})'
-def rotate(degrees:Num, x:Num=0, y:Num=0) -> str: return f'rotate({degrees},{x},{y})'
-def translate(x:Num=0, y:Num=0) -> str: return f'translate({x},{y})'
+def rotate(degrees:Num, x:Num=0, y:Num=0) -> str: return f'rotate({fmt_num(degrees)},{fmt_num(x)},{fmt_num(y)})'
+def translate(x:Num=0, y:Num=0) -> str: return f'translate({fmt_num(x)},{fmt_num(y)})'
 
 
 # Miscellaneous.
--- test/pithy/svg/plots.py
+++ test/pithy/svg/plots.py
@@ -12,9 +12,15 @@ with SvgWriter(stdout, w=1000, h=1000, stroke='black', fill='white', stroke_widt
 
   y = 5
   r = NumRange(0, 1, 1/16, closed=True)
-  with svg.plot(pos=(5, y), size=size, grid_step=0.25, r=0, series=[
-   LineSeries('identity', [(x, x) for x in r]),
-   LineSeries('sin', [(x, sin(x*2*pi)) for x in r])]):
+  with svg.plot(pos=(5, y), size=size, title_h=12, tick_w=32, tick_h=12,
+   title='Identity and Sin',
+   grid_step=0.25,
+   tick_step=(0.25, 0.25),
+   tick_x=True, tick_y=True,
+   series=[
+    LineSeries('identity', [(x, x) for x in r]),
+    LineSeries('sin', [(x, sin(x*2*pi)) for x in r])
+   ]):
     pass
 
 
