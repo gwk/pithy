34473bdb2401b3891959b936ac4baf48135dbbef
George King <george.w.king@gmail.com>
2018-09-14 19:31:16 -0400

    alter baselines test to print capital X.

--- test/pithy/svg/baselines.py
+++ test/pithy/svg/baselines.py
@@ -7,4 +7,4 @@ with Svg(stdout, w=800, h=800) as svg:
   for i, b in enumerate([None] + sorted(alignment_baselines), 1):
     y = i * 50
     svg.line((0, y), (500, y), stroke='#E0E0E0')
-    svg.text((0, y), alignment_baseline=b, style='font-size: 24px; background-color: blue', text=str(b))
+    svg.text((0, y), alignment_baseline=b, style='font-size: 24px; background-color: blue', text=f'X -- {b}')
