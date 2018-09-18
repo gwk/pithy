7d5efe5ab6ef7a66de92701ae9db5332f8f1b4bf
George King <george.w.king@gmail.com>
2018-09-18 08:56:21 -0400

    html-view argument parsing for browser options.

--- pithy/bin/html_view.py
+++ pithy/bin/html_view.py
@@ -1,26 +1,30 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-'''
-Serve a file or `stdin` to a new browser window.
-'''
-
-import subprocess
+from argparse import ArgumentParser
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from sys import argv, stdin
+from ..task import run
 from typing import BinaryIO, NoReturn
+
 
 
 def main() -> None:
 
-  args = argv[1:]
-  if len(args) > 1:
-    exit('specify a single argument or pipe to stdin.')
+  parser = ArgumentParser(description='Serve a file or `stdin` to a browser.')
+  parser.add_argument('-chrome',  action='store_true', help='Use Google Chrome')
+  parser.add_argument('-firefox', action='store_true', help='Use Firefox')
+  parser.add_argument('-safari',  action='store_true', help='Use Safari')
+  parser.add_argument('-stp',  action='store_true', help='Use Safari Technology Preview')
+  parser.add_argument('path', nargs='?', help='The file path to serve (defaults to stdin).')
+  args = parser.parse_args()
 
-  path = args[0] if args else None
+  address = ('localhost', 8000) # TODO: argparse option.
+  host, port = address
+  addr_str = f'http://{host}:{port}'
 
   f: BinaryIO
-  if path:
-    f = open(path, 'rb')
+  if args.path is not None:
+    f = open(args.path, 'rb')
   else:
     f = stdin.detach() # type: ignore
 
@@ -41,11 +45,18 @@ def main() -> None:
         self.wfile.flush()
 
 
-  host, port = address = ('localhost', 8000)
-  addr_str = 'http://{}:{}'.format(host, port)
-  #print(addr_str)
   server = HTTPServer(address, Handler)
-  subprocess.Popen(['open', addr_str]) # race condition!
+
+  # note: the way we tell the OS to open the URL in the browser is a rather suspicious hack:
+  # the `open` command returns and then we launch the web server,
+  # relying on the fact that together the OS and the launched browser take more time to initiate the request
+  # after the `open` process completes than the server does to initialize.
+  if args.chrome:     run(['open', '-a', 'google chrome', addr_str])
+  elif args.firefox:  run(['open', '-a', 'firefox',       addr_str])
+  elif args.safari:   run(['open', '-a', 'safari',        addr_str])
+  elif args.stp:      run(['open', '-a', 'safari technology preview', addr_str])
+  else:               run(['open', addr_str])
+
   server.handle_request()
 
 
