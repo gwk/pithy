6b7df29ad115ed44e11329ab7251ccf1897af333
George King <george.w.king@gmail.com>
2018-09-28 16:18:44 -0400

    pithy.dataclasses conditional import is predicated on sys.version_info for mypy compatibility.

--- pithy/dataclasses/__init__.py
+++ pithy/dataclasses/__init__.py
@@ -1,11 +1,12 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-try:
-  from dataclasses import * # type: ignore
-except ImportError:
+import sys
+if sys.version_info[:2] >= (3, 7):
+  from dataclasses import *
+else:
   import typing
   typing._GenericAlias = ... # type: ignore # Hack 3.6.
-  from .backport import * # type: ignore # Copied from cpython/Lib/dataclasses.py.
+  from .backport import *
 
 # copied from cpython/Lib/dataclasses.py.
 __all__ = ['dataclass',
