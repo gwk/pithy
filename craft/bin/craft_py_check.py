755d5f370f6b4e62868f8bed261a4b2605eb7774
George King <george.w.king@gmail.com>
2018-09-28 16:21:57 -0400

    craft-py-check: MYPYPATH element order is presereved.

--- craft/bin/craft_py_check.py
+++ craft/bin/craft_py_check.py
@@ -8,6 +8,7 @@ from pithy.lex import Lexer
 from pithy.task import runCO
 from argparse import ArgumentParser
 from os import environ
+from typing import Set
 
 
 def main() -> None:
@@ -19,8 +20,11 @@ def main() -> None:
   args = arg_parser.parse_args()
 
   env = environ.copy()
+
+  mypy_path:List[str] = []
   existing_path = env.get('MYPYPATH')
-  mypy_path = {existing_path} if existing_path else set()
+  if existing_path:
+    mypy_path.extend(existing_path.split(':'))
 
   for dep in args.deps:
     spec = find_module_spec(dep)
@@ -34,11 +38,11 @@ def main() -> None:
     path = spec.origin
     assert path_name(path) == '__init__.py'
     search_path = path_dir(path_dir(path))
-    mypy_path.add(search_path)
+    mypy_path.append(search_path)
 
   for p in args.paths:
     if ':' in p: exit(f'bad `-path` argument: {p!r}')
-  mypy_path.update(args.paths)
+  mypy_path.extend(args.paths)
 
   if mypy_path:
     env['MYPYPATH'] = ':'.join(mypy_path)
