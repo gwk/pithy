ec91b72262b26afe5fc7ef09d06834af63be061f
George King <george.w.king@gmail.com>
2018-10-09 09:33:13 -0400

    rewrite loader.

--- pithy/loader.py
+++ pithy/loader.py
@@ -4,93 +4,102 @@
 File open/load dispatch by file extension.
 '''
 
-from io import TextIOWrapper
+from io import BufferedReader, RawIOBase, TextIOBase, TextIOWrapper
 from typing import *
-from typing import IO, TextIO, BinaryIO
+from typing import IO, Iterable, TextIO, BinaryIO
 
 from .io import stderr, errL, errSL
 from .fs import path_ext, path_stem
 
-
+File = Union[TextIO, BinaryIO]
+FileOrPath = Union[File, str]
 LoadFn = Callable[..., Any]
 
 
-def load(file_or_path: Any, ext:str=None, **kwargs) -> Any:
+def _path_cmpd_ext(path:str) -> str:
+  start = 1 if path.startswith('.') else 0 # Account for hidden files.
+  try: idx = path.index('.', start)
+  except ValueError: return ''
+  else: return path[idx:]
+
+
+def _last_ext(cmpd_ext:str) -> str:
+  try: idx = cmpd_ext.rindex('.')
+  except ValueError: return cmpd_ext
+  else: return cmpd_ext[idx:]
+
+
+def _sub_ext(cmpd_ext:str) -> str:
+  try: idx = cmpd_ext.rindex('.')
+  except ValueError: return cmpd_ext
+  else: return cmpd_ext[:idx]
+
+
+def load(file_or_path:FileOrPath, ext:str=None, encoding:str=None, **kwargs:Any) -> Any:
   '''
   Select an appropriate loader based on the file extension, or `ext` if specified.
 
-  If no loader is found, raise an error.
+  If a loader is found, then it is called, passing `kwargs`.
 
-  If a loader is found, then `open` is called with the `open_args` registered by `add_loader`,
-  except updated by any values in `kwargs` whose keys match the loader `open_keys`.
-  The remaining `kwargs` are passed to the loader function.
+  If no loader is found, raises KeyError.
   '''
-  is_file = hasattr(file_or_path, 'read')
-  if is_file:
-    if ext is None:
-      try: path = file_or_path.name
+  if ext is None:
+    if isinstance(file_or_path, str):
+      ext = _path_cmpd_ext(file_or_path)
+    else:
+      try: name = file_or_path.name
       except AttributeError as e:
-        raise ValueError(f'load: file object does not have `name` attribute and no `ext` specified: {file_or_path}') from e
-      ext = path_ext(path)
-  else:
-    path = file_or_path
-    if ext is None:
-      ext = path_ext(path)
-
-  loader = _loaders[ext]
-
-  # Construct args for `open`.
-  open_args = dict(loader.open_args)
-  for k in tuple(open_args):
-    try: v = kwargs[k]
-    except KeyError: pass
-    else: # this arg is meant for `open`, not `load`.
-      open_args[k] = v
-      del kwargs[k] # Safe to delete because kwargs is local.
-
-  if is_file:
-    file = file_or_path
-    if hasattr(file, 'encoding'): # Have text file.
-      if 'b' in open_args['mode']: # Want binary file.
-        raise Exception(f'loader: expected binary file, but received text file: {file}')
-    else: # Have binary file.
-      if open_args['encoding'] is not None: # Want text file.
-        del open_args['buffering'] # TextIOWrapper does not support this argument.
-        file = TextIOWrapper(file, **open_args)
-  else:
-    file = open(file_or_path, **open_args)
-
-  return loader.fn(file, **kwargs)
+        raise ValueError(f'load: no `ext` specified and file does not have `name` attribute: {file_or_path}') from e
+      if not isinstance(name, str):
+        raise ValueError(f'load: no `ext` specified and file has non-string `name` attribute: {name!r}') from e
+      ext = _path_cmpd_ext(name)
+  dispatch_ext = _last_ext(ext)
+  try: load_fn = _loaders[dispatch_ext]
+  except KeyError as e:
+    raise ValueError(f'load: extension {dispatch_ext!r} does not match any available loader: {file_or_path!r}') from e
+  return load_fn(file_or_path, ext=ext, **kwargs)
 
 
-def add_loader(ext: str, _fn: LoadFn, mode='r', buffering=-1, encoding=None, errors=None, newline=None, _dflt=False) -> None:
+def _text_file_for(f:FileOrPath, **kwargs:Any) -> TextIO:
+  if isinstance(f, TextIOWrapper): return f
+  if isinstance(f, str): return open(f, 'r', **kwargs)
+  try: return TextIOWrapper(f, **kwargs) # type: ignore
+  except Exception as e:
+    raise ValueError(f'load: required text file or path; received {f!r}') from e
+
+
+def _binary_file_for(f:FileOrPath) -> BinaryIO:
+  if isinstance(f, BufferedReader): return f
+  if isinstance(f, str): return open(f, 'rb')
+  try: return BufferedReader(f) # type: ignore
+  except Exception as e:
+    raise ValueError(f'load: required binary file or path; received {f!r}') from e
+
+
+def add_loader(ext:str, fn:LoadFn, _dflt=False) -> None:
   '''
-  Register a loader function, which will be called by `muck.load` for matching `ext`.
-  `buffering`, `encoding`, `errors`, and `newline` are all passed on to `open` when it is called by `load`.
+  Register a loader function, which will be called by `load` for matching `ext`.
   `_dflt` is used to mark the default loaders as such so that they can be overridden without triggering an error.
   '''
-  if not ext.startswith('.'):
+  if ext and not ext.startswith('.'):
     raise ValueError(f"file extension does not start with '.': {ext!r}")
-  if not _dflt:
-    try: prev_loader = _loaders[ext]
-    except KeyError: pass
-    else: raise Exception(f'add_loader: extension previously registered: {ext!r}; loader: {prev_loader!r}')
-  _loaders[ext] = Loader(ext=ext, fn=_fn, open_args=(
-    ('mode', mode), ('buffering', buffering), ('encoding', encoding), ('errors', errors), ('newline', newline)))
+  try: prev_loader = _loaders[ext]
+  except KeyError: pass
+  else:
+    if prev_loader not in _dflt_loaders:
+      raise Exception(f'add_loader: extension previously registered: {ext!r}; loader: {prev_loader!r}')
+  _loaders[ext] = fn
+  if _dflt:
+    _dflt_loaders.add(fn)
 
 
-class Loader(NamedTuple):
-  ext: str
-  fn: LoadFn
-  open_args: Tuple[Tuple[str, Any], ...]
+_loaders:Dict[str,LoadFn] = {}
+_dflt_loaders:Set[LoadFn] = set()
 
 
-_loaders: Dict[str, Loader] = {}
-
-
-def load_archive(f: BinaryIO, single_name=None, single_ext=None, **kwargs:Any) -> Any:
+def load_archive(f:FileOrPath, single_name=None, single_ext=None, **kwargs:Any) -> Any:
   from .archive import Archive
-  archive = Archive(f)
+  archive = Archive(_binary_file_for(f))
   if single_name is None and single_ext is None:
     if kwargs:
       raise ValueError('load_archive: `single_name` or `single_ext` not specified; no other options should be set')
@@ -106,89 +115,112 @@ def load_archive(f: BinaryIO, single_name=None, single_ext=None, **kwargs:Any) -
   raise LookupError(f'load_archive: could not find specified {"single_name" if match_exact else "single_ext"} in archive: {single_name!r}; archive.file_names: {archive.file_names}')
 
 
-def load_csv(file: IO, **kwargs) -> Iterator[Sequence[str]]:
+def load_binary(f:FileOrPath, ext:str, **kwargs:Any) -> BinaryIO:
+  assert not kwargs, kwargs # To allow for opening as bytes or text with any encoding.
+  return _binary_file_for(f)
+
+
+#def load_brotli(f:FileOrPath, ext:str, **kwargs:Any) -> Any:
+#  from brotli import decompress
+#  with _binary_file_for(f) as f:
+#    data = decompress(f.read())
+#  sub_ext = _sub_ext(ext)
+#  from gzip import GzipFile
+#  df = GzipFile(mode='rb', fileobj=_binary_file_for(f))
+#  return load(df, ext=sub_ext, **kwargs) # type: ignore
+
+
+def load_csv(f:FileOrPath, ext:str, encoding:str=None, **kwargs:Any) -> Iterator[Sequence[str]]:
   from .csv import load_csv # type: ignore
-  return load_csv(file, **kwargs)
+  return load_csv(_text_file_for(f, newline='', encoding=encoding), **kwargs)
 
 
-def load_gz(f: BinaryIO, sub_ext=None, **kwargs:Any) -> Any:
-  stem = path_stem(f.name)
-  if sub_ext is None:
-    sub_ext = path_ext(stem)
-  if sub_ext == '.tar': # load_archive handles compressed stream faster.
+def load_gz(f:FileOrPath, ext:str, **kwargs:Any) -> Any:
+  sub_ext = _sub_ext(ext)
+  if sub_ext.endswith('.tar'): # load_archive handles compressed tar stream faster.
     return load_archive(f, **kwargs)
   from gzip import GzipFile
-  df = GzipFile(mode='rb', fileobj=f)
-  df.name = stem # strip off '.gz' for secondary dispatch by `load`.
-  return load(df, ext=sub_ext, **kwargs)
+  df = GzipFile(mode='rb', fileobj=_binary_file_for(f))
+  return load(df, ext=sub_ext, **kwargs) # type: ignore
 
 
-def load_json(file: IO, **kwargs) -> Any:
+def load_json(file_or_path:FileOrPath, ext:str, **kwargs:Any) -> Any:
   from .json import load_json # type: ignore
-  return load_json(cast(TextIO, file), **kwargs)
+  return load_json(_text_file_for(file_or_path), **kwargs)
 
-def load_jsonl(file: IO, **kwargs) -> Any:
+def load_jsonl(file_or_path:FileOrPath, ext:str, **kwargs:Any) -> Any:
   from .json import load_jsonl # type: ignore
-  return load_jsonl(cast(TextIO, file), **kwargs)
+  return load_jsonl(_text_file_for(file_or_path), **kwargs)
 
-def load_jsons(file: IO, **kwargs) -> Any:
+def load_jsons(file_or_path:FileOrPath, ext:str, **kwargs:Any) -> Any:
   from .json import load_jsons # type: ignore
-  return load_jsons(cast(TextIO, file), **kwargs)
+  return load_jsons(_text_file_for(file_or_path), **kwargs)
 
 
-def load_pyl(file: IO, **kwargs) -> Any:
+def load_pyl(f:FileOrPath, ext:str, **kwargs:Any) -> Any:
   'Load a python literal AST file (Python equivalent of JSON).'
   from ast import literal_eval
-  return literal_eval(file.read())
+  return literal_eval(_text_file_for(f, **kwargs).read())
 
 
-def load_sqlite(file: IO, **kwargs) -> Any:
+def load_sqlite(f:FileOrPath, ext:str, **kwargs:Any) -> Any:
   from .sqlite import Connection
-  path = file.name # TODO: Figure out if this is sufficient.
+  if not isinstance(f, str):
+    raise ValueError(f'load: sqlite files can only be opened with a string path; received {f!r}')
   return Connection(f'file:{path}?mode=ro', uri=True, **kwargs) # type: ignore # Open in read-only mode.
 
 
-def load_txt(f: TextIO, clip_ends=False) -> Iterable[str]:
+def load_txt(f:FileOrPath, ext:str, clip_ends=False, **kwargs:Any) -> Iterable[str]:
+  f = _text_file_for(f, **kwargs)
   if clip_ends: return (line.rstrip('\n\r') for line in f)
   return f
 
 
-def load_xls(file: BinaryIO) -> Any:
+def load_xls(f:FileOrPath, ext:str, **kwargs:Any) -> Any:
   from xlrd import open_workbook # type: ignore
+  if isinstance(f, str):
+    return open_workbook(filename=f, logfile=stderr, **kwargs)
   # Unfortunately load_xls will not take an open file handle.
   # Since we might be passing in an in-memory file like ArchiveFile,
-  # the best we can do for now is always read file contents into memory.
+  # the best we can do for now is read file contents into memory.
   # Alternative would be to make ArchiveFile conform to mmap protocol
   # (xrld supports passing in mmap objects),
   # or patch xlrd to support passing in an open binary file descriptor.
-  return open_workbook(filename=None, logfile=stderr, file_contents=file.read())
+  return open_workbook(filename=None, logfile=stderr, file_contents=_binary_file_for(f).read(), **kwargs)
 
 
-def load_xz(f: BinaryIO, sub_ext=None, **kwargs:Any) -> Any:
-  stem = path_stem(f.name)
-  if sub_ext is None:
-    sub_ext = path_ext(stem)
-  if sub_ext == '.tar': # load_archive handles compressed stream faster internally.
+def load_xz(f:FileOrPath, ext:str, **kwargs:Any) -> Any:
+  sub_ext = _sub_ext(ext)
+  if sub_ext.endswith('.tar'): # load_archive handles compressed stream faster.
+    # TODO: investigate whether a multithreaded subprocess and/or pipeline between two processes could be faster.
     return load_archive(f, **kwargs)
   from lzma import LZMAFile
-  df = LZMAFile(f)
-  df.name = stem # type: ignore # strip off '.xz' for secondary dispatch by `load`. TODO: loader should not rely on this hack.
-  return load(df, ext=sub_ext, **kwargs)
+  d = LZMAFile(_binary_file_for(f))
+  #b = BufferedReader(d) # type: ignore
+  return load(d, ext=sub_ext, **kwargs) # type: ignore
 
 
+def load_zst(f:FileOrPath, ext:str, **kwargs:Any) -> Any:
+  from zstandard import ZstdDecompressor # type: ignore
+  decompressor = ZstdDecompressor()
+  d = decompressor.stream_reader(_binary_file_for(f))
+  #b = BufferedReader(d)
+  return load(d, ext=_sub_ext(ext), **kwargs)
 
-add_loader('.css',      load_txt,     _dflt=True, mode='r')
-add_loader('.csv',      load_csv,     _dflt=True, mode='r', newline='') # newline specified as per footnote in csv module.
-add_loader('.gz',       load_gz,      _dflt=True, mode='rb')
-add_loader('.json',     load_json,    _dflt=True, mode='r')
-add_loader('.jsonl',    load_jsonl,   _dflt=True, mode='r')
-add_loader('.jsons',    load_jsons,   _dflt=True, mode='r')
-add_loader('.pyl',      load_pyl,     _dflt=True, mode='r')
-add_loader('.sqlite',   load_sqlite,  _dflt=True, mode='rb')
-add_loader('.sqlite3',  load_sqlite,  _dflt=True, mode='rb')
-add_loader('.tar',      load_archive, _dflt=True, mode='rb')
-add_loader('.txt',      load_txt,     _dflt=True, mode='r')
-add_loader('.xls',      load_xls,     _dflt=True, mode='rb')
-add_loader('.xz',       load_xz,      _dflt=True, mode='rb')
-add_loader('.zip',      load_archive, _dflt=True, mode='rb')
 
+add_loader('',          load_binary,  _dflt=True)
+add_loader('.css',      load_txt,     _dflt=True)
+add_loader('.csv',      load_csv,     _dflt=True)
+add_loader('.gz',       load_gz,      _dflt=True)
+add_loader('.json',     load_json,    _dflt=True)
+add_loader('.jsonl',    load_jsonl,   _dflt=True)
+add_loader('.jsons',    load_jsons,   _dflt=True)
+add_loader('.pyl',      load_pyl,     _dflt=True)
+add_loader('.sqlite',   load_sqlite,  _dflt=True)
+add_loader('.sqlite3',  load_sqlite,  _dflt=True)
+add_loader('.tar',      load_archive, _dflt=True)
+add_loader('.txt',      load_txt,     _dflt=True)
+add_loader('.xls',      load_xls,     _dflt=True)
+add_loader('.xz',       load_xz,      _dflt=True)
+add_loader('.zip',      load_archive, _dflt=True)
+add_loader('.zst',      load_zst,     _dflt=True)
--- test/pithy/loader.py
+++ test/pithy/loader.py
@@ -2,3 +2,81 @@
 
 from utest import *
 from pithy.loader import *
+from pithy.loader import _path_cmpd_ext, _last_ext, _sub_ext
+from pithy.json import write_json, write_jsonl
+from pithy.csv import write_csv
+from pithy.task import run
+
+
+lines = ['0\n', '1\n']
+lines_b = [s.encode() for s in lines]
+with open('test.txt', 'w') as f:
+  for line in lines:
+    f.write(line)
+
+utest_seq(lines, load, 'test.txt')
+utest_seq(lines_b, load, 'test.txt', ext='') # Open with binary loader.
+
+header = ('N', 'A')
+rows = [['0', 'a'], ['1', 'b']]
+with open('test.csv', 'w') as f:
+  write_csv(f, header=header, rows=rows)
+
+utest_seq(rows, load, 'test.csv', header=header)
+
+
+d = {'0':0, '1':1}
+with open('test.json', 'w') as f:
+  write_json(f, d)
+
+utest(d, load, 'test.json')
+
+
+with open('test.jsonl', 'w') as f:
+  write_jsonl(f, d)
+  write_jsonl(f, d)
+
+utest_seq([d, d], load, 'test.jsonl')
+
+
+with open('test.jsons', 'w') as f:
+  write_json(f, d)
+  write_json(f, d)
+
+utest_seq([d, d], load, 'test.jsons')
+
+
+# Test each compression method.
+compression_exts = ['.gz', '.xz', '.zst']
+pack_flags = [e.replace('.', '-') for e in compression_exts]
+run(['pack', '-quiet', *pack_flags, 'test.txt'])
+for ext in compression_exts:
+  path = 'test.txt' + ext
+  utest_seq(lines, load, path)
+
+# TODO:
+# '.br'
+# '.pyl'
+# '.sqlite'
+# '.sqlite3'
+# '.tar'
+# '.xls'
+# '.zip'
+
+
+utest('', _path_cmpd_ext, '')
+utest('', _path_cmpd_ext, '.hidden')
+utest('.ext', _path_cmpd_ext, 'stem.ext')
+utest('.ext', _path_cmpd_ext, '.hidden.ext')
+utest('.ext.ext', _path_cmpd_ext, 'stem.ext.ext')
+utest('.ext.ext', _path_cmpd_ext, '.hidden.ext.ext')
+
+utest('', _last_ext, '')
+utest('', _sub_ext, '')
+
+utest('.ext', _last_ext, '.ext')
+utest('', _sub_ext, '.ext')
+
+utest('.b', _last_ext, '.a.b')
+utest('.a', _sub_ext, '.a.b')
+
