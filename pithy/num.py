a6322cf1f8e2917d28bad01fedc8abbc244a80a3
George King <george.w.king@gmail.com>
2018-10-15 11:20:40 -0400

    NumRange is no longer a dataclass to placate typechecker.

--- pithy/num.py
+++ pithy/num.py
@@ -4,7 +4,6 @@
 Numeric utilities.
 '''
 
-from .dataclasses import dataclass
 from typing import Sequence, Iterator, Union
 
 
@@ -14,12 +13,12 @@ Num = Union[int, float]
 _set_attr = object.__setattr__
 
 
-@dataclass(frozen=True)
 class NumRange(Sequence[Num]):
 
   start:Num
   stop:Num
   step:Num
+  closed:bool
   _len:int
 
   def __init__(self, start:Num, stop:Num=None, step:Num=1, *, closed=False) -> None:
@@ -51,3 +50,13 @@ class NumRange(Sequence[Num]):
 
   def __repr__(self):
     return '{}({}, {}, {})'.format(type(self).__name__, self.start, self.stop, self.step)
+
+  def __setattr__(self, name:str, val:str) -> None:
+    raise AttributeError('readonly attributes')
+
+  def __hash__(self) -> int:
+    return hash(self.start)^hash(self.stop)^hash(self.step)^int(self.closed)
+
+  def __eq__(self, other:object) -> bool:
+    return isinstance(other, NumRange) and \
+    self.start == other.start and self.stop == other.stop and self.step == other.step and self.closed == other.closed
