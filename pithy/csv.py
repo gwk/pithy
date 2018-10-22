b3a5a31f686a6764a04105f9b59f5c9d82bf39a0
George King <george.w.king@gmail.com>
2018-10-22 13:50:28 +0100

    csv: write_csv, out_csv support quoting.

--- pithy/csv.py
+++ pithy/csv.py
@@ -36,14 +36,14 @@ def load_csv(file: TextIO,
     header=header)
 
 
-def write_csv(f:TextIO, *, header:Optional[Sequence[str]], rows:Iterable[Sequence]) -> None:
-  w = csv.writer(f)
+def write_csv(f:TextIO, *, quoting:Optional[int]=None, header:Optional[Sequence[str]], rows:Iterable[Sequence]) -> None:
+  w = csv.writer(f, quoting=quoting)
   if header is not None: w.writerow(header)
   w.writerows(rows)
 
 
-def out_csv(*, header:Optional[Sequence[str]], rows:Iterable[Sequence]) -> None:
-  write_csv(f=stdout, header=header, rows=rows)
+def out_csv(*, quoting:Optional[int]=None, header:Optional[Sequence[str]], rows:Iterable[Sequence]) -> None:
+  write_csv(f=stdout, quoting=quoting, header=header, rows=rows)
 
 
 class CSVFileReader(Iterable, ContextManager):
