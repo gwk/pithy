2895c3ea9efefa10168ba4a4742d6789ec60aed1
George King <george.w.king@gmail.com>
2019-02-18 11:07:44 -0500

    factor out pithy.generator.

--- pithy/tree.py
+++ pithy/tree.py
@@ -1,6 +1,7 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from typing import Callable, Generator, Iterable, Iterator, List, Optional, Tuple, TypeVar, Union
+from .generator import GeneratorType as _Generator
 
 
 _T = TypeVar('_T')
@@ -10,11 +11,6 @@ _Stack = Tuple[_T, ...]
 _VisitResult = Union[_R, Generator[_R, None, None]]
 _GetChildrenFn = Callable[[_T], Optional[Iterable[_T]]]
 _VisitFn = Callable[[_T, _Stack, List[_R]], _VisitResult]
-
-# We check if the returned value from the visit function is that of a generator.
-# In order to do so, we must create one and get its runtime type.
-def _g() -> Iterator[None]: yield None
-_Generator = type(_g())
 
 
 class OmitNode(Exception): pass
