3983d0109f42ed2e9076523b6db9cf916a9d332d
George King <george.w.king@gmail.com>
2019-09-04 10:27:20 -0600

    test/pithy/meta: test dispatcher_for_names.

--- test/pithy/meta.py
+++ test/pithy/meta.py
@@ -2,3 +2,16 @@
 
 from utest import *
 from pithy.meta import *
+
+
+
+def disp_a(s:str) -> str: return 'A: ' + s
+
+def disp_b(s:str) -> str: return 'B: ' + s
+
+
+disp = dispatcher_for_names(prefix='disp_')
+
+utest('A: x', disp, 'a', 'x')
+utest('B: y', disp, 'b', 'y')
+
