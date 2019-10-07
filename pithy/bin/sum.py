1e07dd3107e06c2f22fbf5849e2d1a86314e8901
George King <george.w.king@gmail.com>
2019-10-07 18:52:48 -0400

    pith.bin.sum.

---
+++ pithy/bin/sum.py
@@ -0,0 +1,13 @@
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+from sys import argv, stdin, stderr
+
+
+def main() -> None:
+  args = argv[1:] or list(stdin)
+  number_strings = [a.replace(',', '_') for arg in args for a in arg.split()]
+  print(number_strings, file=stderr)
+  try: total = sum(float(s) for s in number_strings)
+  except ValueError as e: exit(f'sum error: {e}')
+  print(total)
+
