6edd3b1a1e69d0345da1cc02f34484563a9c1581
George King <george.w.king@gmail.com>
2019-10-22 09:32:19 -0400

    pithy.unicode.codepoints: codes_desc calculates single-character range gaps.

--- pithy/unicode/codepoints.py
+++ pithy/unicode/codepoints.py
@@ -1,6 +1,6 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from typing import Dict, Iterable
+from typing import Dict, Iterable, List, Set
 from . import CodeRange
 
 
@@ -9,12 +9,32 @@ __all__ = [
 ]
 
 
-def codes_desc(code_ranges:Iterable[CodeRange]) -> str:
-  return ' '.join(codes_range_desc(*p) for p in code_ranges) or 'Ø'
+def codes_desc(code_ranges:Iterable[CodeRange], raw=False) -> str:
+  if raw:
+    return ' '.join(codes_range_desc(*p) for p in code_ranges) or 'Ø'
+  # Calculate single-character subtraction simplifications for ranges.
+  ranges:List[CodeRange] = []
+  subtracted:Set[int] = set()
+  for r in code_ranges: # Assumed to be sorted.
+    if ranges:
+      p = ranges[-1]
+      if p[1] + 1 == r[0] and p[1] != 0x60: # Exclude '`' because it makes standard sym range look weird.
+        ranges[-1] = (p[0], r[1])
+        subtracted.add(p[1])
+      else:
+        ranges.append(r)
+    else:
+      ranges.append(r)
+  if not ranges: return 'Ø'
+  rs = ' '.join(codes_range_desc(*p) for p in ranges)
+  if not subtracted: return rs
+  ss = ' '.join(code_desc(c) for c in subtracted)
+  return f'{rs} - {ss}'
+
 
 def codes_range_desc(l:int, h:int) -> str:
   if l + 1 == h: return code_desc(l)
-  return '{}-{}'.format(code_desc(l), code_desc(h-1))
+  return '{}..{}'.format(code_desc(l), code_desc(h-1))
 
 def code_desc(c:int) -> str:
   assert isinstance(c, int)
