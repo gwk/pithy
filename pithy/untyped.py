16fcfcdadbcfc8eec85acf19f7a3f4becc55abe0
George King <george.w.king@gmail.com>
2019-11-07 18:32:57 -0500

    make pithy.untyped.Immutable generic.

--- pithy/untyped.py
+++ pithy/untyped.py
@@ -1,18 +1,20 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 import re
-from typing import Any, Dict, Iterator, Tuple
+from typing import Any, Dict, Generic, Iterator, Tuple, TypeVar
 
+
+_T = TypeVar('_T')
 
 _setattr = object.__setattr__
 
 
-class Immutable:
+class Immutable(Generic[_T]):
   'Untyped immutable object.'
 
   def __init__(self, obj:Any=None, **kw:Any) -> None:
     if obj is not None:
-      self.__dict__.update(obj)
+      vars(self).update(obj)
     for k, v in kw.items():
       _setattr(self, k, v)
 
@@ -20,18 +22,17 @@ class Immutable:
     args = ', '.join(f'{k if _is_identifier(k) else repr(k)}={v!r}' for k, v in vars(self).items())
     return f'{type(self).__name__}({args})'
 
-  def __getattr__(self, key:str) -> Any:
-    'This is defined to let the typechecker know that the class is untyped.'
-    raise AttributeError(key)
+  def __getattr__(self, key:str) -> _T:
+    return vars(self)[key] # type: ignore
 
-  def __setattr__(self, name, val) -> None:
+  def __setattr__(self, name:str, val:_T) -> None:
     raise AttributeError('Immutable instance attributes are readonly')
 
-  def __delattr__(self, name) -> None:
+  def __delattr__(self, name:str) -> None:
     raise AttributeError('Immutable instance attributes are readonly')
 
-  def __getitem__(self, key:str) -> Any:
-    return getattr(self, key)
+  def __getitem__(self, key:str) -> _T:
+    return vars(self)[key] # type: ignore
 
   def __hash__(self) -> int:
     h = 0
@@ -42,8 +43,8 @@ class Immutable:
   def __eq__(self, other:Any) -> bool:
     return type(self) == type(other) and vars(self) == vars(other)
 
-  def __iter__(self) -> Iterator[Any]:
-    return iter(vars(self).items())
+  def __iter__(self) -> Iterator[_T]:
+    return iter(vars(self).items()) # type: ignore
 
 
 _identifier_re = re.compile(r'(?!\d)\w+')
