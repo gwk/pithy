70b9fac60f98339a946b0c3c8cc366dd188b8c00
George King <george.w.king@gmail.com>
2021-03-07 09:39:21 -0500

    pithy.sqlite: Cursor gains opt, one, col, opt_col, one_col methods; remove the ineffective "select" and "update" function families.

--- pithy/sqlite/__init__.py
+++ pithy/sqlite/__init__.py
@@ -1,6 +1,7 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 import sqlite3
+from sqlite3 import Row
 from typing import Any, Dict, Iterable, Iterator, List, NamedTuple, Optional, Sequence, Tuple, Type, cast
 
 from ..json import render_json
@@ -28,6 +29,49 @@ class Cursor(sqlite3.Cursor):
       if not isinstance(v, py_to_sql_types_tuple):
         args[k] = render_json(v, indent=None)
     return self.execute(query, args)
+
+
+  def opt(self) -> Optional[Row]:
+    'Return a single, optional row.'
+    return cast(Optional[Row], self.fetchone())
+
+
+  def one(self) -> Row:
+    'Return a single, non-optional row.'
+    row = self.fetchone()
+    if row is None: raise ValueError(None)
+    return cast(Row, row)
+
+
+  def col(self) -> Iterable[Any]:
+    'Yield column 0 of each result row.'
+    row = self.fetchone()
+    assert len(row) == 1
+    yield row[0]
+    for row in self:
+      yield row[0]
+
+
+  def opt_col(self) -> Any:
+    if row := self.fetchone():
+      assert len(row) == 1
+      return row[0]
+    else:
+      return None
+
+
+  def one_col(self) -> Any:
+    row = self.fetchone()
+    if row is None: raise ValueError(None)
+    assert len(row) == 1
+    return row[0]
+
+
+  def contains(self, table:str, *where:str, **args:Any) -> bool:
+    'Execute a SELECT query, returning True if the `where` SQL clause results in at least one row.`'
+    for row in self.run('SELECT 1 FROM', table, 'WHERE', *where, 'LIMIT 1', **args):
+      return True
+    return False
 
 
   def insert(self, *, with_='', or_='FAIL', into:str, fields:Optional[Iterable[str]]=None, sql:str, args:Any=()) -> None:
@@ -82,48 +126,13 @@ class Cursor(sqlite3.Cursor):
     self.insert(with_=with_, or_=or_, into=into, fields=fields, sql=f'VALUES ({placeholders})', args=values)
 
 
-  def select(self, *sql:str, **args:Any) -> 'Cursor':
-    'Execute a SELECT query.'
-    return self.run('SELECT', *sql, **args)
-
-
-  def select_opt(self, *sql:str, **args:Any) -> Optional[sqlite3.Row]:
-    'Execute a SELECT query, returning a single row or None.'
-    return self.run('SELECT', *sql, **args).fetchone() # type: ignore
-
-
-  def select_col(self, *sql:str, **args:Any) -> Iterator[Any]:
-    'Execute a SELECT query, returning column 0 of each result row.'
-    for row in self.run('SELECT', *sql, **args):
-      assert len(row) == 1
-      yield row[0]
-
-
-  def select_one_col(self, *sql:str, **args:Any) -> Any:
-    row = self.run('SELECT', *sql, **args).fetchone()
-    if row is None: raise ValueError(None)
-    return row[0]
-
-
-  def contains(self, table:str, *where:str, **args:Any) -> bool:
-    'Execute a SELECT query, returning True if the `where` SQL clause results in at least one row.`'
-    for row in self.run('SELECT 1 FROM', table, 'WHERE', *where, 'LIMIT 1', **args):
-      return True
-    return False
-
-
-  def update(self, table:str, *, cols:Iterable[str], where:str, **args:Any) -> None:
-    bindings = [f'{col} = :{col}' for col in cols]
-    bindings_clause = ', '.join(bindings)
-    self.run('UPDATE', table, 'SET', bindings_clause, 'WHERE', where, **args)
-
 
 class Connection(sqlite3.Connection):
 
   def __init__(self, path:str, uri:bool=False) -> None:
     super().__init__(path, uri=uri)
-    self.row_factory = sqlite3.Row # default for convenience.
-    #self.stmt_cache = {}
+    self.row_factory = Row # Default for convenience.
+
 
   def cursor(self, factory:Optional[type]=None) -> Cursor:
     if factory is None:
@@ -131,36 +140,42 @@ class Connection(sqlite3.Connection):
     assert issubclass(factory, Cursor)
     return cast(Cursor, super().cursor(factory))
 
+
   def run(self, *sql:str, **args:Any) -> Cursor:
     return self.cursor().run(*sql, **args)
 
-
-  def insert_row(self, *, with_='', or_='FAIL', into:str, **kwargs:Any) -> None:
-    return self.cursor().insert_row(with_=with_, or_=or_, into=into, **kwargs)
-
-  def insert_dict(self, *, with_='', or_='FAIL', into:str, fields:Optional[Iterable[str]]=None, args:Dict[str, Any],
-   defaults:Dict[str, Any]={}) -> None:
-    return self.cursor().insert_dict(with_=with_, or_=or_, into=into, fields=fields, args=args, defaults=defaults)
-
-  def insert_seq(self, *, with_='', or_='FAIL', into:str, fields:Optional[Iterable[str]]=None, seq:Sequence[Any]) -> None:
-    return self.cursor().insert_seq(with_=with_, or_=or_, into=into, fields=fields, seq=seq)
-
-
-  def select(self, *sql:str, **args:Any) -> Cursor:
-    return self.cursor().select(*sql, **args)
-
-  def select_opt(self, *sql:str, **args:Any) -> Optional[sqlite3.Row]:
-    return self.cursor().select_opt(*sql, **args)
-
-  def select_col(self, *sql:str, **args:Any) -> Iterator[Any]:
-    return self.cursor().select_col(*sql, **args)
-
-  def select_one_col(self, *sql:str, **args:Any) -> Any:
-    return self.cursor().select_one_col(*sql, **args)
 
   def contains(self, table:str, *where:str, **args:Any) -> bool:
     return self.cursor().contains(table, *where, **args)
 
 
-  def update(self, table:str, *, cols:Iterable[str], where:str, **args:Any) -> None:
-    return self.cursor().update(table, cols=cols, where=where, **args)
+  def insert(self, *, with_='', or_='FAIL', into:str, fields:Optional[Iterable[str]]=None, sql:str, args:Any=()) -> None:
+    '''
+    Execute an insert statement synthesized from `into` (the table name), `fields` (optional), and `sql`.
+    This function is intended to be an intermediate helper for higher level insert functions.
+    '''
+    self.cursor().insert(with_=With_, or_=or_, into=into, fields=fields, sql=sql, args=args)
+
+
+  def insert_row(self, *, with_='', or_='FAIL', into:str, **kwargs:Any) -> None:
+    '''
+    Execute an insert statement inserting the kwargs key/value pairs passed as named arguments.
+    '''
+    self.cursor().insert_row(with_=with_, or_=or_, into=into, **kwargs)
+
+
+  def insert_dict(self, *, with_='', or_='FAIL', into:str, fields:Optional[Iterable[str]]=None, args:Dict[str, Any],
+   defaults:Dict[str, Any]={}) -> None:
+    '''
+    Execute an insert statement inserting the dictionary `args`, synthesized from `into` (the table name) and `fields`.
+    Values are pulled in by name first from the `args` dictionary, then from `defaults`;
+    a KeyError is raised if one of the fields is not provided in either of these sources.
+    '''
+    self.insert_dict(with_=with_, or_=or_, into=into, fields=fields, args=args, defaults=defaults)
+
+
+  def insert_seq(self, *, with_='', or_='FAIL', into:str, fields:Optional[Iterable[str]]=None, seq:Sequence[Any]) -> None:
+    '''
+    Execute an insert statement inserting the sequence `args`, synthesized from `into` (the table name), and `fields`.
+    '''
+    self.insert_seq(with_=with_, or_=or_, into=into, fields=fields, seq=seq)
