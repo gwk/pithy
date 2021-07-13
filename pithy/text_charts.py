4b0148ed584335f9c2719dd3ca34bb957a4ad20d
George King <george.w.king@gmail.com>
2021-07-13 07:37:57 -0700

    pithy.text_charts: chart_seq_inline gains optional width parameter.

--- pithy/text_charts.py
+++ pithy/text_charts.py
@@ -26,12 +26,16 @@ _max = max
 _Num = Union[int,float]
 
 
-def chart_seq_inline(values:Iterable[_Num], max:_Num=0) -> str:
+def chart_seq_inline(values:Iterable[_Num], max:_Num=0, width:int=0) -> str:
   values = tuple(values)
   if not values: return ''
   if max <= 0:
     max = _max(values)
     if max <= 0: return '\u2592' * len(values) # Medium shade block.
+  if width > 0:
+    vals = list(values)
+    step: float = len(vals) / width
+    values = [vals[int(step*i)] for i in range(width)]
   return ''.join(vertical_bars[int(0.5 + (8 * _max(0, _min(1, v/max))))] for v in values)
 
 
