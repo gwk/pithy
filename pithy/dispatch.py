09df55fbf51a94248b4b11a1d8705bdf92a41306
George King <george@theorypractice.dev>
2021-12-27 11:10:12 -0800

    pithy.dispatch: clean up error messages for `key_dispatched`.

--- pithy/dispatch.py
+++ pithy/dispatch.py
@@ -22,11 +22,11 @@ def key_dispatched(key_fn:Optional[Callable[[Any], Any]]=None, *, key:Any=Defaul
 
   if key_fn is not None: # register new dispatcher name with default implementation.
     if not callable(key_fn):
-      raise TypeError(f'argument to key_dispatched() must be key function; received {key_fn!r}')
+      raise TypeError(f'argument to @key_dispatched() must be key function; received {key_fn!r}')
     if key is not Default._:
-      raise ValueError(f'@key_dispatch() takes either positional `key_fn` (decorating default method) or keyword `key`')
+      raise ValueError(f'@key_dispatched() requires either positional `key_fn` (decorating default method) or keyword `key`')
     if key_fn.__closure__ is not None: # type: ignore # Key function cannot be a closure.
-      raise ValueError(f'@key_dispatch() `key_fn` cannot be a closure; recieved {key_fn!r}')
+      raise ValueError(f'@key_dispatched() `key_fn` cannot be a closure; recieved {key_fn!r}')
 
     def dflt_decorator(dflt_method:Callable)->Callable:
       _check_key_dispatched_method(dflt_method)
@@ -34,7 +34,7 @@ def key_dispatched(key_fn:Optional[Callable[[Any], Any]]=None, *, key:Any=Defaul
       name = dflt_method.__name__
       module_registry = _keyed_dispatch_registries[dflt_method.__module__]
       if name in module_registry:
-        raise DispatchKeyError(f'`keyed_dispatch` collision on method name: {name}')
+        raise DispatchKeyError(f'`@key_dispatched()` collision on method name: {name}')
       # Register.
       method_registry: Dict[Any, Callable] = {}
       # Create and return dispatcher method.
@@ -48,7 +48,7 @@ def key_dispatched(key_fn:Optional[Callable[[Any], Any]]=None, *, key:Any=Defaul
 
   else: # key_fn is None; register implementation for key.
     if key is Default._:
-      raise ValueError(f'@key_dispatch() takes either positional `key_fn` (decorating default method) or keyword `key`')
+      raise ValueError(f'@key_dispatched() takes either positional `key_fn` (decorating default method) or keyword `key`')
 
     def decorator(method:Callable) -> Callable:
       _check_key_dispatched_method(method)
@@ -57,9 +57,9 @@ def key_dispatched(key_fn:Optional[Callable[[Any], Any]]=None, *, key:Any=Defaul
       module_registry = _keyed_dispatch_registries[method.__module__]
       try: dispatch, method_registry = module_registry[name]
       except KeyError as e:
-        raise DispatchKeyError(f'`keyed_dispatch` method name was not previously registered: {name}') from e
+        raise DispatchKeyError(f'@key_dispatched() method name was not previously registered: {name}') from e
       if key in method_registry:
-        raise DispatchKeyError(f'`keyed_dispatch` collision on key: {key}') # TODO: show previous implementation.
+        raise DispatchKeyError(f'@key_dispatched() collision on key: {key}') # TODO: show previous implementation.
       method_registry[key] = method
       return dispatch
     return decorator
@@ -67,8 +67,8 @@ def key_dispatched(key_fn:Optional[Callable[[Any], Any]]=None, *, key:Any=Defaul
 
 def _check_key_dispatched_method(method:Callable) -> None:
   if method.__closure__ is not None: # type: ignore # Method cannot be a closure because it is registered globally.
-    raise ValueError(f'@key_dispatch() decorated method cannot be a closure; recieved {method!r}')
+    raise ValueError(f'@key_dispatched() decorated method cannot be a closure; recieved {method!r}')
   sig = inspect.signature(method)
   pars = list(sig.parameters.values())
-  if len(pars) < 2 or pars[0].name != 'self':
-    raise TypeError(f'`keyed_dispatch` decorated method requires at least `self` and one additional parameter; received {method!r}')
+  if len(pars) < 2 or pars[0].name not in ('self', 'cls'):
+    raise TypeError(f'@key_dispatched() decorated method requires at least `self` and one additional parameter; received {method!r}')
