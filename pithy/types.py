0d338e5ffa369e9ed5e2584cf348266be02effd4
George King <george@theorypractice.dev>
2022-04-10 14:51:33 -0400

    pithy.types: improve is_a implementation.

--- pithy/types.py
+++ pithy/types.py
@@ -19,48 +19,58 @@ class Comparable(Protocol):
   def __eq__(self, other:Any) -> bool: ...
 
 
-def is_a(val:Any, T:Union[type, Tuple[type, ...]]) -> bool:
+def is_a(val:Any, T:Union[type,tuple[type,...]]) -> bool:
   '''
-  Test if `val` is of `T`. Unlike `isinstance`,
-  this function works with generic static types.
+  Test if `val` is of `T`.
+  Unlike `isinstance`, this function works with basic generic types.
   '''
-  try: return isinstance(val, T)
-  except TypeError as e:
-    if e.args[0] != 'Subscripted generics cannot be used with class and instance checks': raise
-  RTT = get_origin(T)
+  if isinstance(T, tuple): return any(is_a(val, t) for t in T)
+
   args = get_args(T)
-  try:
-    predicate = _generic_type_predicates[RTT]
-  except KeyError: # Not specialized.
-    if issubclass(RTT, dict): # Two parameters.
-      if issubclass(RTT, Counter): # Counter only has one type parameter.
-        K = args[0]
-        V = int # Note that Counters can have non-int values inserted.
-      else:
-        K, V = args
-      return isinstance(val, RTT) and all(is_a(k, K) and is_a(v, V) for (k, v) in val.items())
-    elif len(args) == 1: # Assume `T` is a single-parameter generic container.
-      E = args[0]
-      return isinstance(val, RTT) and all(is_a(el, E) for el in val)
+  if not args: return isinstance(val, T)
+  RTT = get_origin(T)
+
+  if RTT is None: raise TypeError(f'{T} has no origin type')
+
+  try: predicate = _generic_type_predicates[RTT]
+  except KeyError: pass
+  else: return predicate(val, args)
+
+  if issubclass(RTT, dict): # Two parameters.
+    if issubclass(RTT, Counter): # Counter only has one type parameter.
+      K = args[0]
+      V = int # Note that Counters can have non-int values inserted.
     else:
-      raise TypeError(f'{T} is not a single-parameter generic type; origin type: {RTT}')
-  else: # Specialized.
-    # Union is an extra strange case, because the origin type is not a runtime type either.
-    return (RTT is Union or isinstance(val, RTT)) and predicate(val, args)
+      K, V = args
+    return isinstance(val, RTT) and all(is_a(k, K) and is_a(v, V) for (k, v) in val.items())
+
+  if len(args) == 1: # Assume `T` is a single-parameter generic container.
+    E = args[0]
+    return isinstance(val, RTT) and all(is_a(el, E) for el in val)
+
+  raise TypeError(f'{T} is not a single-parameter generic type; origin type: {RTT}')
 
 
-_Args = Tuple[Any, ...]
+_Args = Tuple[type, ...]
+
 
 def _is_a_Tuple(v:Any, args:_Args) -> bool:
+  if not isinstance(v, tuple): return False
   if len(args) == 2 and args[1] is Ellipsis:
     E = args[0]
     return all(is_a(el, E) for el in v)
   else:
     return len(v) == len(args) and all(is_a(el, E) for (el, E) in zip(v, args))
 
+
+def _is_a_Union(v:Any, args:_Args) -> bool:
+  # Union is an extra strange case, because the origin type is not a runtime type either.
+  return any(is_a(v, Member) for Member in args)
+
+
 _generic_type_predicates: Dict[Any, Callable[[Any, _Args], bool]] = {
   tuple: _is_a_Tuple,
-  Union: lambda v, args: any(is_a(v, Member) for Member in args),
+  Union: _is_a_Union,
 }
 
 
@@ -109,7 +119,7 @@ def is_str_or_pair(val: Any) -> bool: return is_str(val) or is_pair_of_str(val)
 def is_pos_int(val: Any) -> bool: return is_int(val) and bool(val > 0)
 
 
-def req_type(obj: _T, expected: Union[type, Tuple[type, ...]]) -> _T:
+def req_type(obj: _T, expected:Union[type,Tuple[type,...]]) -> _T:
   'Return `obj` if it is of `expected` type, or else raise a descriptive TypeError.'
   if not is_a(obj, expected):
     raise TypeError(f'expected type: {expected}; actual type: {type(obj)};\n  object: {obj!r}')
