c86ec20e961a4ac655cf201a613d06be473021d9
George King <george@theorypractice.dev>
2022-07-19 22:04:47 -0400

    Add license comment.

--- pithy/diff/__init__.py
+++ pithy/diff/__init__.py
@@ -1,5 +1,4 @@
-
-
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from difflib import SequenceMatcher
 from typing import Any, Callable, Dict, List, Sequence, Tuple, TypeVar
