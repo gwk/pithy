328def10f7f5ecc5d15292cc8d6d029117412494
George King <george@theorypractice.dev>
2023-01-02 19:33:02 -0800

    Draft of pithy.html.charts.

---
+++ pithy/html/charts/__main__.py
@@ -0,0 +1,14 @@
+# © 2022 Robert Hall & Associates. All Rights Reserved.
+
+from sys import stderr
+
+import uvicorn # type: ignore[import]
+import watchfiles # This is an optional import for uvicorn but we want to make sure it is installed.
+
+
+def main() -> None:
+  print(f'Serving chart _webtest', file=stderr)
+  uvicorn.run("pithy.html.charts._webtest:app", host='localhost', port=8000, log_level="info", factory=True, reload=True)
+
+
+if __name__ == '__main__': main()
