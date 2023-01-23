002de8015f110db7d455ad3000f504c29b243e58
George King <george@theorypractice.dev>
2023-01-22 20:39:18 -0800

    pithy.html.charts development.

--- pithy/html/charts/_webtest.py
+++ pithy/html/charts/_webtest.py
@@ -2,8 +2,8 @@
 
 
 from pithy.markup import EscapedStr
-from pithy.html import Html, Style
-from pithy.html.charts import BarSeries, chart_style, chart_figure, LinearAxis
+from pithy.html import Html, Style, Div
+from pithy.html.charts import BarSeries, chart_css, chart_figure, LinearAxis
 
 from starlette.applications import Starlette
 from starlette.routing import Route
@@ -21,17 +21,16 @@ def app() -> Starlette:
 async def home_page(request:Request) -> HTMLResponse:
   html = Html.doc(title='TEST')
 
-  html.head.append(Style(ch=EscapedStr(chart_style)))
+  html.head.append(Style(ch=EscapedStr('*, *::before, *::after { box-sizing: border-box; }')))
+  html.head.append(Style(ch=EscapedStr(chart_css)))
 
 
   figure = chart_figure(title='CHART',
   y=LinearAxis(tick_step=10),
   series=[
-    BarSeries(
-      name='Series0',
-      points=[(i, i) for i in range(101)]),
+    BarSeries(name='Series0', points=[(str(i), i) for i in range(101)]),
     #BarSeries(name='Series1', points=[('a', 4), ('b', 5), ('c', 6)]),
-    ])
+  ])
 
   html.body.append(figure)
 
