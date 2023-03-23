b4d6f0969505ab98724f2df912d702120d3ea661
George King <george@theorypractice.dev>
2023-03-23 15:09:18 -0700

    pithy.web.asgi.select: columns with Vis annotations now show the specified join column.

--- pithy/web/asgi/select.py
+++ pithy/web/asgi/select.py
@@ -1,9 +1,11 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-
+from collections import Counter
 from typing import Callable, Iterable, Sequence
 
+from pithy.io import outM
 from pithy.iterable import iter_interleave_sep
+from pithy.string import str_tree, str_tree_iter, str_tree_pairs
 from starlette.authentication import has_required_scope
 from starlette.concurrency import run_in_threadpool
 from starlette.datastructures import QueryParams
@@ -15,7 +17,7 @@ from ...html import (Div, Form, H1, HtmlNode, Input, Label, Main, Pre, Present, 
   Thead, Tr)
 from ...sqlite import Connection
 from ...sqlite.parse import sql_parse_schema_table
-from ...sqlite.schema import Column, Schema, Table
+from ...sqlite.schema import Column, Schema, Table, Vis
 from ...sqlite.util import sql_quote_entity as qe
 
 
@@ -106,7 +108,7 @@ class SelectApp:
     except KeyError: return None
 
     try: schema_name, table_name = sql_parse_schema_table(full_name)
-    except ValueError: raise HTTPException(400, f'invalid table name: {full_name!r}')
+    except ValueError as e: raise HTTPException(400, f'invalid table name: {full_name!r} ({e})')
 
     try: schema = self.schemas[schema_name]
     except KeyError: raise HTTPException(400, f'invalid schema: {schema_name!r}')
@@ -123,21 +125,17 @@ class SelectApp:
 
     distinct = bool(params.get('distinct'))
 
-    en_cols = []
-    for col_name in table.columns_dict:
-      if col_name in en_col_names:
-        en_cols.append(table.columns_dict[col_name])
+    en_cols = [c for c in table.columns if c.name in en_col_names]
 
     where = params.get('where', '')
     order_by = params.get('order_by', '')
 
     limit = int(params.get('limit', 100) or 100)
 
-    select_clause = fmt_select_cols(en_cols, distinct=distinct)
-    schema_table_sql = f'{qe(schema.name)}.{qe(table.name)}'
-    order_by_clause = f'\nORDER BY {order_by}' if order_by else ''
+    header_names, select_clause, from_clause = fmt_select_cols(schema=schema.name, table=table.name, cols=en_cols, distinct=distinct)
     where_clause = f'\nWHERE {where}' if where else ''
-    query = f'{select_clause}\nFROM {schema_table_sql}{where_clause}{order_by_clause}\nLIMIT {limit}'
+    order_by_clause = f'\nORDER BY {order_by}' if order_by else ''
+    query = f'{select_clause}{from_clause}{where_clause}{order_by_clause}\nLIMIT {limit}'
 
     conn = self.get_conn()
     c = conn.cursor()
@@ -151,10 +149,11 @@ class SelectApp:
     count = ''
     if is_ok:
       try:
-        count = c.run(f"SELECT COUNT(1) FROM {schema_table_sql}{where_clause}").one_col()
+        count_int = c.run(f"SELECT COUNT(1) {from_clause}{where_clause}").one_col()
       except Exception as e:
         count = f'Count failed: {e}\n{query}'
-        is_ok = False
+      else:
+        count = f'{count_int:,}'
 
     rows = []
     if is_ok:
@@ -169,26 +168,66 @@ class SelectApp:
       Div(id='query', cl='kv-grid', ch=[
         Label(ch='Query:'), Pre(id='select_query', hx_swap_oob='innerHTML', ch=query),
         Label(ch='Plan:'), Pre(id='select_plan', hx_swap_oob='innerHTML', ch=plan),
-        Label(ch='Count:'), Pre(id='select_count', hx_swap_oob='innerHTML', ch=count and f'{count:,}'),
+        Label(ch='Count:'), Pre(id='select_count', hx_swap_oob='innerHTML', ch=count and f'{count}'),
       ]),
       Div(id='results', ch=HtmlTable(cl='dense', ch=[
-        Thead(ch=Tr(ch=[Th(ch=Div(ch=qe(col.name))) for col in en_cols])),
+        Thead(ch=Tr(ch=[Th(ch=Div(ch=name)) for name in header_names])),
         Tbody(ch=rows)])),
     ]
 
 
-def fmt_select_cols(cols:list[Column], distinct:bool) -> str:
+def fmt_select_cols(schema:str, table:str, cols:list[Column], distinct:bool) -> tuple[list[str],str,str]:
+  '''
+  Return the rendered table header names, the SELECT [cols...] portion, and FROM/JOIN portion of the query.
+  '''
+  all_vis = [col.vis for col in cols if isinstance(col.vis, Vis)]
+  schema_abbrs = abbreviate_schema_names({schema, *(vis.schema for vis in all_vis)})
+  table_abbrs = Counter[str]()
+
+  def table_abbr(schema:str, table:str) -> str:
+    s = schema_abbrs[schema]
+    t = table[0]
+    abbr = f'{s}{t}'
+    if n := table_abbrs[abbr]: abbrN = abbr + str(n)
+    else: abbrN = abbr
+    table_abbrs[abbr] += 1
+    return abbrN
+
+  t_abbr = table_abbr(schema, table)
+
   select = 'SELECT ' + ('DISTINCT ' if distinct else '')
-  parts = [select]
+  header_names = []
+  select_parts = [select]
+  from_parts:list[str] = [f'\nFROM {qe(schema)}.{qe(table)} AS {t_abbr}']
   line_len = len(select)
+
   for i, col in enumerate(cols):
-    qn = qe(col.name)
-    if line_len + len(qn) >= 128:
-      parts.append(',\n  ')
-      line_len = 2
-    elif i:
-      parts.append(', ')
-      line_len += 2
-    parts.append(qn)
-    line_len += len(qn)
-  return ''.join(parts)
+    vis = col.vis
+    if isinstance(vis, Vis) and vis.join:
+      # Generate a join to show the desired visualization column.
+      vis_t = table_abbr(vis.schema, vis.table)
+      qual_col = f'{t_abbr}.{qe(col.name)}'
+      col_name = f'COALESCE({vis_t}.{qe(vis.col)}, {qual_col})'
+      head_name = f'{col.name}: {vis_t}.{vis.col}'
+      from_parts.append(f'\nLEFT JOIN {vis.schema_table} AS {vis_t} ON {qual_col} = {vis_t}.{qe(vis.join_col)}')
+    else:
+      col_name = f'{t_abbr}.{qe(col.name)}'
+      head_name = qe(col.name)
+    if i:
+      if line_len + len(col_name) >= 128:
+        select_parts.append(',\n  ')
+        line_len = 2
+      else:
+        select_parts.append(', ')
+        line_len += 2
+    header_names.append(head_name)
+    select_parts.append(col_name)
+    line_len += len(col_name)
+
+  return header_names, ''.join(select_parts), ''.join(from_parts)
+
+
+def abbreviate_schema_names(schema_names:set[str]) -> dict[str,str]:
+  if len(schema_names) < 2: return { n : '' for n in schema_names } # If only one schema is present then we can omit it entirely.
+  tree = str_tree(sorted(schema_names))
+  return { prefix+suffix : prefix+suffix[:1] for prefix, suffix in str_tree_pairs(tree) }
