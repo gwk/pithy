49a43b540becfb99c5d46b3f76185e0f0e4a779d
George King <george@theorypractice.dev>
2023-06-02 10:35:59 -0400

    html.parse: improve linkify.

--- pithy/html/parse.py
+++ pithy/html/parse.py
@@ -26,7 +26,7 @@ def linkify(text:str, transform:Callable[[str],str]|None=None) -> Iterator[A|str
 linkify_re = re_compile(r'''(?x)
 ( # Must recognize some leading signifier.
   [a-z][a-z0-9.]+:// # Protocol.
-| ([\w-]+\.)+[a-z]+/ # Domain with TLD suffix. Require a trailing slash to avoid matching a filename.
+| ([\w-]+\.)+([a-z]{2,18})/ # Domain with TLD suffix. Require a trailing slash to avoid matching a filename. Alpha names only; ignores the 'xn--*' punycode names.
 | (?<!\d) \d{1,3} \. \d{1,3} \. \d{1,3} \. \d{1,3} (?!=\d) # IPv4 address.
 | localhost:\d+ # Localhost with port.
 )
