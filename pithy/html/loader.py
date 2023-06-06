a9d8d939faef1190d67128f622bd9d9452286613
George King <george@theorypractice.dev>
2023-06-05 23:30:40 -0400

    Clean up imports.

--- pithy/html/loader.py
+++ pithy/html/loader.py
@@ -1,9 +1,9 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from typing import Any, TextIO, BinaryIO, Union, cast
+from typing import Any, BinaryIO, cast, TextIO, Union
 
-from ..html import HtmlNode
 from ..loader import FileOrPath
+from . import HtmlNode
 
 
 def load_html(file_or_path:FileOrPath, encoding:str='utf8', **kwargs:Any) -> Any:
