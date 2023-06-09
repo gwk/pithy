ba1d90345c3709af0b6625ad3330808e1a2f4043
George King <george@theorypractice.dev>
2023-06-09 12:35:13 -0400

    Use list comprehension to satisfy type checker.

--- test/pithy/svg/baselines.py
+++ test/pithy/svg/baselines.py
@@ -5,7 +5,7 @@ from pithy.io import outZ
 
 
 svg = Svg(w=800, h=800)
-for i, b in enumerate([None] + sorted(alignment_baselines), 1):
+for i, b in enumerate([None, *sorted(alignment_baselines)], 1):
   y = i * 50
   svg.append(Line((0, y), (500, y), stroke='#E0E0E0'))
   svg.append(Text(f'X -- {b}', pos=(0, y), alignment_baseline=b, style='font-size: 24px; background-color: blue'))
