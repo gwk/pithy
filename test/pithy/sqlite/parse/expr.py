d131d09fd595747c84b5cd40322aba071d45db33
George King <george@theorypractice.dev>
2023-06-17 08:26:49 -0700

    test/pithy/sqlite/parse/expr.py: reorder test cases.

--- test/pithy/sqlite/parse/expr.py
+++ test/pithy/sqlite/parse/expr.py
@@ -14,10 +14,11 @@ def parse_expr(s:str) -> Any:
   source = Source('<expr>', s)
   return simple_sql_parser.parse('expr', source)
 
-
-utest(['a'], parse_expr, 'a')
+# Strings.
+utest("''" , parse_expr, "''")
 utest("'a'", parse_expr, "'a'")
-utest(['"a"'], parse_expr, '"a"')
+
+# Numbers.
 utest('1', parse_expr, '1')
 utest('+1', parse_expr, '+1')
 utest('-1', parse_expr, '-1')
@@ -27,6 +28,9 @@ utest('-1.0', parse_expr, '-1.0')
 utest('1.0e1', parse_expr, '1.0e1')
 utest('1.0e-1', parse_expr, '1.0e-1')
 utest('1.0e+1', parse_expr, '1.0e+1')
+
+utest(['a'], parse_expr, 'a')
+utest(['"a"'], parse_expr, '"a"')
 
 
 utest(('||', "'a'", "'b'"), parse_expr, "'a' || 'b'")
