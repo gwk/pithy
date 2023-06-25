c1c0846cdd8382864933fa6369dd57ce3c6a7f62
George King <george@theorypractice.dev>
2023-06-25 14:18:36 -0400

    Remove Opt alias.

--- pithy/typing.py
+++ pithy/typing.py
@@ -1,10 +1,8 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from types import TracebackType
-from typing import Optional, Type
+from typing import Type
 
-
-Opt = Optional
 
 # These types are helpful when defining `__exit__`:
 # def __exit__(self, exc_type:OptTypeBaseExc, exc_value:OptBaseExc, traceback:OptTraceback) -> bool: ...
