6058a266c9dd69bc49a3344fea39c2bbcb2552a7
George King <george@theorypractice.dev>
2023-07-03 07:33:42 -0400

    pithy.fs: path_descendants: fix index logic error.

--- test/pithy/path.py
+++ test/pithy/path.py
@@ -3,8 +3,8 @@
 from random import choice as rand_choice, seed as rand_seed
 
 from pithy.path import (abs_or_norm_path, abs_path, insert_path_stem_suffix, is_norm_path, is_path_abs, is_sub_path, norm_path,
-  path_rel_to_dir)
-from utest import utest
+  path_descendants, path_rel_to_dir, PathIsNotDescendantError)
+from utest import utest, utest_exc
 
 
 # abs_or_norm_path normalizes inputs.
@@ -73,6 +73,19 @@ for i in range(1<<10):
     n = norm_path(path)
     utest(True, is_norm_path, n)
 
+
+# path_descendants.
+utest_exc(PathIsNotDescendantError, path_descendants, 'a', 'b')
+
+utest(('a',), path_descendants, 'a', 'a')
+utest(('a',), path_descendants, 'a', 'a', include_start=False)
+utest(('a',), path_descendants, 'a', 'a', include_end=False)
+utest((), path_descendants, 'a', 'a', include_start=False, include_end=False)
+
+utest(('a', 'a/b', 'a/b/c'), path_descendants, 'a/', 'a/b/c')
+utest(('a/b', 'a/b/c'), path_descendants, 'a/', 'a/b/c', include_start=False)
+utest(('a', 'a/b'), path_descendants, 'a/', 'a/b/c', include_end=False)
+utest(('a/b',), path_descendants, 'a/', 'a/b/c', include_start=False, include_end=False)
 
 # path_rel_to_dir.
 utest('.', path_rel_to_dir, '', '')
