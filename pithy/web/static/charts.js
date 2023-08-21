fbe20fdb2fafca0f825620f351a00c6a32c38d30
George King <george@theorypractice.dev>
2023-08-21 14:29:42 -0700

    Move charts.js, charts.css to pithy/web/static.

--- pithy/web/static/charts.js
+++ pithy/web/static/charts.js
@@ -1,1 +1,23 @@
-../../html/charts/charts.js
+"use strict";
+
+console.log('charts.js');
+
+function chartsLinkScrollX(visGrid) {
+  let activeDiv = null;
+  const visScroll = visGrid.querySelector('.vis-scroll');
+  const ticksXScroll = visGrid.querySelector('.ticks-x-scroll');
+  console.assert(visScroll);
+  console.assert(ticksXScroll);
+  function link(div, other) {
+    div.addEventListener('mouseenter', (e) => {
+      activeDiv = e.target;
+    });
+    div.addEventListener("scroll", (e) => {
+      if (e.target !== activeDiv) return;
+      other.scrollLeft = e.target.scrollLeft;
+    });
+  }
+
+  link(visScroll, ticksXScroll);
+  link(ticksXScroll, visScroll);
+}
