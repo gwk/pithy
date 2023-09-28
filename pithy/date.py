b0f47f19c9bfcb748ae4167d17abbdcfd8b3b753
George King <george@theorypractice.dev>
2023-09-28 17:04:10 -0500

    pithy.date: parse_time, parse_time_12h, parse_time_12hmp, time_delta; .web.starlette: req_form_date, req_form_time_12hmp.

--- pithy/date.py
+++ pithy/date.py
@@ -1,5 +1,6 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
+import re
 from dataclasses import dataclass, field
 from datetime import date as Date, datetime as DateTime, time as Time, timedelta as TimeDelta, tzinfo as TZInfo, UTC as tz_utc
 from typing import Iterator, overload, Sequence, TypeVar
@@ -150,6 +151,84 @@ def parse_date(string: str, fmt:str|None=None) -> Date:
   return Date.fromisoformat(string)
 
 
+def parse_time(string:str, tz:TZInfo|None=None) -> Time:
+  '''
+  Parse a time string, accepting either 24-hour (isoformat) or 12-hour (with AM/PM) formats.
+  Isoformat times with an explicit timezone are rejected.
+  Raises ValueError.
+  '''
+  try: isotime = Time.fromisoformat(string)
+  except ValueError: pass
+  else:
+    if isotime.tzinfo is not None: raise ValueError(isotime)
+    if tz: isotime = isotime.replace(tzinfo=tz)
+    return isotime
+  return parse_time_12h(string, tz=tz)
+
+
+def parse_time_12h(string:str, tz:TZInfo|None=None) -> Time:
+  '''
+  Parse a time string in 12-hour (with AM/PM) format.
+  raises ValueError.
+  '''
+  match = time_re.fullmatch(string)
+  if match is None: raise ValueError(string)
+
+  sh, sm, ss, sf, ampm = match.group('h', 'm', 's', 'f', 'ampm')
+
+  h = int(sh)
+  m = int(sm)
+  s = int(ss) if ss is not None else 0
+  f = int(sf) if sf is not None else 0
+  if ampm is None: raise ValueError(f'parse_time_12h: missing AM/PM: {string!r}')
+  if h > 12: raise ValueError(string)
+  if ampm == 'am':
+    if h == 12: h = 0
+  else:
+    assert ampm == 'pm', ampm
+    if h < 12: h += 12
+  return Time(h, m, s, f, tzinfo=tz)
+
+
+time_re = re.compile(r'''(?ix)
+  (?P<h>\d{1,2})
+  : (?P<m>\d{2})
+  (?: # Seconds are optional.
+    : P(?P<s>\d{2})
+    (?: # Fractional seconds are optional.
+      \. (?P<f>\d*)
+    )?
+  )?
+  (?P<ampm>[ap]m)? # AM/PM is optional.
+  ''')
+
+
+def parse_time_12hmp(string:str, tz:TZInfo|None=None) -> Time:
+  '''
+  Parse a time string in 12-hour H:MM AM/PM format.
+  raises ValueError.
+  '''
+  match = time_12hmp_re.fullmatch(string)
+  if match is None: raise ValueError(string)
+
+  sh, sm, ampm = match.groups()
+
+  h = int(sh)
+  m = int(sm)
+  assert 1 <= h <= 12, h
+  assert 0 <= m <= 59, m
+  ampm = ampm.lower()
+  if ampm == 'am':
+    if h == 12: h = 0
+  else:
+    assert ampm == 'pm', ampm
+    if h < 12: h += 12
+  return Time(h, m, tzinfo=tz)
+
+
+time_12hmp_re = re.compile(r'(1[012]|[1-9]):([0-5][0-9]) ?([AaPp][Mm])')
+
+
 def dt_from_ts_utc(ts:float) -> DateTime:
   'Create a DateTime from a UTC timestamp.'
   return DateTime.fromtimestamp(ts, tz_utc)
@@ -166,3 +245,9 @@ def next_day(date_: _DateOrDateTime) -> _DateOrDateTime:
 
 def next_week(date_: _DateOrDateTime) -> _DateOrDateTime:
   return date_ + TimeDelta(days=7)
+
+
+def time_delta(a:Time, b:Time) -> TimeDelta:
+  'Subtract two times, returning a timedelta.'
+  return TimeDelta(hours=a.hour - b.hour, minutes=a.minute - b.minute, seconds=a.second - b.second,
+    microseconds=a.microsecond - b.microsecond)
