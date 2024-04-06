05d7c0af90d6cd7e13bf1190170ed6953adb2cef
George King <george@theorypractice.dev>
2024-04-06 09:22:09 -0700

    pithy.html.parse: linkify uses more conservative regex; update stale docstring.

--- pithy/html/parse.py
+++ pithy/html/parse.py
@@ -10,9 +10,7 @@ from . import A
 
 def linkify(text:str, transform:Callable[[str],str]|None=None) -> Iterator[A|str]:
   '''
-  Create a link element if the text looks like it starts with a URL. Otherwise, return the plain text as-is.
-  If the URL does not appear to span the entire text, create a link element with the entire text as its content.
-  The whitespace, possibly preceded by the characters  `.,;:` terminate a URL.
+  Given a string `text`, return an iterator of strings and A elements.
   '''
   for part in find_matches_interleaved(linkify_re, text):
     if isinstance(part, Match):
@@ -26,9 +24,10 @@ def linkify(text:str, transform:Callable[[str],str]|None=None) -> Iterator[A|str
 linkify_re = re_compile(r'''(?x)
 ( # Must recognize some leading signifier.
   [a-z][a-z0-9.]+:// # Protocol.
-| ([\w-]+\.)+([a-z]{2,18})/ # Domain with TLD suffix. Require a trailing slash to avoid matching a filename. Alpha names only; ignores the 'xn--*' punycode names.
+| ([\w-]+\.)+([a-z]{2,18})/ # Domain with TLD suffix. Require a trailing slash to avoid matching a filename.
+  #^ Alpha names only; ignores the 'xn--*' punycode names.
 | (?<!\d) \d{1,3} \. \d{1,3} \. \d{1,3} \. \d{1,3} (?!=\d) # IPv4 address.
 | localhost:\d+ # Localhost with port.
 )
-[^\s]*(?<![,.:;]) # Accept all non-whitespace characters except for some trailing punctuation.
+[!#-&(-_a-~]*(?<![,.:;]) # Accept most visible ascii characters (reject space and quotes), but omit some trailing punctuation.
 ''')
