b552ee52618c317b7125e2ab88b1876f2d74c40b
George King <george@theorypractice.dev>
2024-08-23 08:11:21 -0700

    pithy.web.browser: factor out add_browser_args, launch_browser; make html-view and serve-dir multithreaded and stop using race condition hack.

--- pithytools/bin/serve_dir.py
+++ pithytools/bin/serve_dir.py
@@ -1,10 +1,11 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from argparse import ArgumentParser
+from threading import Thread
 
 from pithy.http.server import HttpServer, Response
-from pithy.task import run
 from pithy.web.app import Request, WebApp
+from pithy.web.browser import add_browser_args, launch_browser
 
 
 def main() -> None:
@@ -12,36 +13,28 @@ def main() -> None:
   parser = ArgumentParser(description='Serve files from a directory.')
   parser.add_argument('root', default='.', nargs='?', help='Root directory to serve from')
   parser.add_argument('-port', default=8000, type=int, help='Port to listen on')
-  parser.add_argument('-browse', action='store_true', help='Launch the default system browser')
-  parser.add_argument('-chrome',  action='store_true', help='Launch Google Chrome')
-  parser.add_argument('-firefox', action='store_true', help='Launch Firefox')
-  parser.add_argument('-safari',  action='store_true', help='Launch Safari')
-  parser.add_argument('-stp',  action='store_true', help='Launch Safari Technology Preview')
-
+  add_browser_args(parser, add_browse=True)
   args = parser.parse_args()
+
   root = args.root
   host = 'localhost'
   port = args.port
   #address = (host, port)
   addr_str = f'http://{host}:{port}'
-  print(f'Serving {root} on {addr_str}…')
+  print(f'Serving {root!r} on {addr_str}…')
 
   #ignored_paths = { 'apple-touch-icon-precomposed.png' }
 
   app = LocalFileApp(local_dir=root, prevent_client_caching=True, map_bare_names_to_html=False)
   server = HttpServer(host=host, port=port, app=app)
+  server_thread = Thread(target=server.serve_forever)
 
-  # note: the way we tell the OS to open the URL in the browser is a rather suspicious hack:
-  # the `open` command returns and then we launch the web server,
-  # relying on the fact that together the OS and the launched browser take more time to initiate the request
-  # after the `open` process completes than the server does to initialize.
-  if args.browse:   run(['open', addr_str])
-  if args.chrome:   run(['open', '-a', 'google chrome', addr_str])
-  if args.firefox:  run(['open', '-a', 'firefox',       addr_str])
-  if args.safari:   run(['open', '-a', 'safari',        addr_str])
-  if args.stp:      run(['open', '-a', 'safari technology preview', addr_str])
+  server_thread.start()
 
-  try: server.serve_forever()
+  if args.browser: launch_browser(addr_str, args.browser)
+
+  try:
+    server_thread.join()
   except KeyboardInterrupt:
     print('\nKeyboard interrupt received; shutting down server.')
     exit()
