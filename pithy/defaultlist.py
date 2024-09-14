88d569790022a4b63210388e7af2b146531286f1
George King <george@theorypractice.dev>
2024-09-14 09:42:49 -0700

    pithy.defaultlist: DefaultList now adds intervening default values when setitem is called.

--- pithy/defaultlist.py
+++ pithy/defaultlist.py
@@ -9,22 +9,23 @@ _T = TypeVar('_T')
 class DefaultList(list[_T]):
   '''
   A subclass of `list` that adds default elements produced by a factory function
-  when an out-of-bounds element is accessed.
+  when an out-of-bounds element is accessed or set past the end.
   The factory function takes the array index as a its sole parameter.
   '''
 
-  def __init__(self, factory:Callable[[int], _T], iterable:Iterable[_T]=(), fill_length=0):
+  def __init__(self, factory:Callable[[int], _T], iterable:Iterable[_T]=(), fill_length:int=0):
     super().__init__(iterable)
     self.factory = factory
     for i in range(fill_length):
       self.append(factory(i))
 
   @overload
-  def __getitem__(self, index: SupportsIndex) -> _T: ...
-  @overload
-  def __getitem__(self, index: slice) -> list[_T]: ...
+  def __getitem__(self, index: SupportsIndex, /) -> _T: ...
 
-  def __getitem__(self, index):
+  @overload
+  def __getitem__(self, index: slice, /) -> list[_T]: ...
+
+  def __getitem__(self, index, /):
     if isinstance(index, slice):
       end = len(self) if index.stop is None else index.stop
     else:
@@ -32,6 +33,23 @@ class DefaultList(list[_T]):
     for i in range(len(self), end):
       self.append(self.factory(i))
     return super().__getitem__(index)
+
+
+  @overload
+  def __setitem__(self, index: SupportsIndex, value: _T, /) -> None: ...
+
+  @overload
+  def __setitem__(self, index: slice, value: Iterable[_T], /) -> None: ...
+
+  def __setitem__(self, index, value, /):
+    if isinstance(index, slice):
+      end = len(self) if index.stop is None else index.stop
+    else:
+      end = index + 1
+    for i in range(len(self), end):
+      self.append(self.factory(i))
+    super().__setitem__(index, value)
+
 
   def __repr__(self) -> str:
     return '{}({}, {})'.format(type(self).__qualname__, self.factory, super().__repr__())
--- test/pithy/defaultlist.ut.py
+++ test/pithy/defaultlist.ut.py
@@ -1,10 +1,18 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
+from operator import getitem, setitem
+
 from pithy.defaultlist import DefaultList
-from utest import utest_seq
+from utest import utest, utest_seq
 
 
 utest_seq([0, 1], DefaultList, lambda i: i, fill_length=2)
 utest_seq([-1, -1], DefaultList, lambda i: i, [-1, -1])
 
-l = DefaultList(lambda i: i, fill_length=2)
+for i in range(3):
+  utest(i, getitem, DefaultList(lambda i: i), i)
+
+for i in range(3):
+  l = DefaultList(lambda i: i)
+  utest(None, setitem, l, i, 9)
+  utest(9, getitem, l, i)
