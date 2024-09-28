fdd67697422044f6fecdafe4cf264190559cf583
George King <george@theorypractice.dev>
2024-09-28 07:44:23 -0700

    pithy.html.dom: dom_events.

---
+++ pithy/html/dom.py
@@ -0,0 +1,10 @@
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+
+dom_events = {
+  "mouse": ["mousedown", "mouseup", "click", "dblclick", "mousemove", "mouseover", "mousewheel", "mouseout", "contextmenu"],
+  "touch": ["touchstart", "touchmove", "touchend", "touchcancel"],
+  "keyboard": ["keydown", "keypress", "keyup"],
+  "form": ["focus", "blur", "change", "submit"],
+  "window": ["scroll", "resize", "hashchange", "load", "unload"]
+}
