e95aa860b79a8b27d00dd22d6b081a25c596c91b
George King <george@theorypractice.dev>
2024-10-23 20:09:32 -0700

    pithy.clock: use time.perf_counter; messages use "prev -> current" labels; quiet option; report method.

--- pithy/clock.py
+++ pithy/clock.py
@@ -1,19 +1,24 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from sys import stderr
-from time import time as now
+from time import perf_counter as now
+from typing import Self
 
 from .typing import OptBaseExc, OptTraceback, OptTypeBaseExc
 
 
 class Clock:
+  '''
+  A simple ContextManager for timing code blocks.
+  '''
 
-  def __init__(self, name:str):
+  def __init__(self, name:str, quiet:bool=False) -> None:
     self.name = name
-    self.times: list[tuple[float, str]] = [(now(), 'start')]
+    self.quiet = quiet
+    self.times:list[tuple[float,str]] = [(now(), 'start')]
 
 
-  def __enter__(self) -> 'Clock': return self
+  def __enter__(self) -> Self: return self
 
 
   def __exit__(self, exc_type:OptTypeBaseExc, exc_value:OptBaseExc, traceback:OptTraceback) -> None:
@@ -22,10 +27,22 @@ class Clock:
 
   def split(self, label:str) -> None:
     t = now()
-    print(f'clock: {self.name}: {label}: {t - self.times[-1][0]:.04f}', file=stderr)
     self.times.append((t, label))
+    if not self.quiet:
+      prev_time, prev_label = self.times[-2]
+      print(f'clock: {self.name}: {prev_label} -> {label}: {t - prev_time:.04f}', file=stderr)
 
 
   def total(self) -> None:
     t = now()
-    print(f'clock: {self.name}: TOTAL: {t - self.times[0][0]:.04f}', file=stderr)
+    self.times.append((t, 'end'))
+    if not self.quiet:
+      print(f'clock: {self.name}: TOTAL: {t - self.times[0][0]:.04f}', file=stderr)
+
+
+  def report(self) -> list[tuple[float,str]]:
+    times = self.times
+    rows = [(times[i][0] - times[i-1][0], f'{times[i-1][1]} -> {times[i][1]}') for i in range(1, len(times))]
+    if self.times[-1][1] == 'end':
+      rows.append((times[-1][0] - times[0][0], 'TOTAL'))
+    return rows
