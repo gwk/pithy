f72606fbc27c25ff245f53b20417ddaacbd3af06
George King <george@theorypractice.dev>
2024-12-07 20:15:17 -0800

    pithy.argparse: improve CommmandParser to handle multiple levels of commands.

--- pithy/argparse.py
+++ pithy/argparse.py
@@ -11,32 +11,54 @@ class ArgParser(ArgumentParser):
   '''
 
 
-
 class CommandParser(ArgParser):
   '''
-  A subclass of ArgParser that has a lazy `commands` subparser.
-  The subparser is defined as lazy because subparsers will be of type CommandParser and may not themselves define subcommands.
+  A subclass of ArgParser that is intended for configuration with subcommand parsers.
+  Use `add_command()` to add subcommands.
+  The result of this function is itself a CommandParser due to the way that ArgumentParser implements `add_subparsers`
+  and `add_parser`.
+
+  To create multiple levels of subcommands, use `add_parent_command()` to create intermediate parent parsers.
   '''
 
-
   @cached_property
-  def commands(self) -> _SubParsersAction:
+  def _commands_subparsers(self) -> _SubParsersAction:
     commands = self.add_subparsers(required=True, dest='command', help='Available commands.')
     self.epilog = "For help with a specific command, pass '-h' to that command."
     return commands
 
 
+  def _check_is_valid_parent(self) -> None:
+    if self.get_default('main_fn') is not None:
+      raise Exception('CommandParser has a `main_fn` function set; '
+        'use `add_parent_command()` instead of `add_command()` to create intermediate parent parsers.')
+
+
   def add_command(self, main_fn:Callable[[Namespace],None], name:str|None=None, **kwargs) -> 'CommandParser':
     '''
     Add a command to the parser.
-    By default, `name` will be derived from `main_fn` by removing the 'main_' prefix and replacing underscores with hyphens.
+    By default, `name` will be derived from `main_fn` by removing any 'main_' prefix and replacing underscores with hyphens.
     '''
+    self._check_is_valid_parent()
+
     if not name:
       name = main_fn.__name__.removeprefix('main_').replace('_', '-')
 
-    command = self.commands.add_parser(name, **kwargs)
-    command.set_defaults(main=main_fn)
-    return command # type: ignore[no-any-return]
+    command = self._commands_subparsers.add_parser(name, **kwargs)
+    assert isinstance(command, CommandParser)
+    command.set_defaults(main_fn=main_fn)
+    return command
+
+
+  def add_parent_command(self, name:str, **kwargs) -> 'CommandParser':
+    '''
+    Add a parent command to the parser.
+    '''
+    self._check_is_valid_parent()
+    command = self._commands_subparsers.add_parser(name, **kwargs)
+    assert isinstance(command, CommandParser)
+    return command
+
 
 
   def parse_and_run_command(self, args:Sequence[str]|None=None) -> Namespace:
@@ -44,5 +66,5 @@ class CommandParser(ArgParser):
     Parse arguments and run the command.
     '''
     ns = self.parse_args(args)
-    ns.main(ns)
+    ns.main_fn(ns)
     return ns
---
+++ test/pithy/argparse.ut.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+from pithy.argparse import CommandParser, Namespace
+from utest import utest_val
+
+
+parser = CommandParser()
+
+def main_cmdB(ns:Namespace):
+  assert 0 # Not called.
+
+cmdA_parser = parser.add_parent_command('cmdA')
+cmdA_parser.add_argument('-a_arg', help='An argument.')
+
+cmdB_parser = parser.add_parent_command('cmdB')
+cmdB_parser.add_argument('-b_arg', help='A second argument.')
+
+def main_cmdAA(ns:Namespace):
+  utest_val(ns.a_arg, 'arg1')
+  utest_val(ns.aa_arg, 'arg2')
+
+def main_cmdBB(ns:Namespace):
+  assert 0 # Not called.
+
+cmdAA_parser = cmdA_parser.add_command(main_cmdAA)
+cmdAA_parser.add_argument('-aa_arg', help='A subcommand argument.')
+
+cmdBB_parser = cmdB_parser.add_command(main_cmdBB)
+cmdBB_parser.add_argument('-bb_arg', help='A second subcommand argument.')
+
+parser.parse_and_run_command(['cmdA', '-a_arg', 'arg1', 'cmdAA', '-aa_arg', 'arg2'])
