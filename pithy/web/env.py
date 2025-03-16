789cc5690583d69f3ef43b70976dc58f9eed40a5
George King <george@theorypractice.dev>
2025-03-16 14:07:39 -0700

    pithy.web.env: refactor web_external_addr to rely on new web_external_host.

--- pithy/web/env.py
+++ pithy/web/env.py
@@ -16,9 +16,9 @@ def web_data_dir() -> str:
   return environ.get('WEB_DATA_DIR', './data')
 
 
-def web_port() -> int:
-  'Get the server port specified by the environment variable WEB_PORT. Defaults to 8000.'
-  return int(environ.get('WEB_PORT', '8000'))
+def web_proto() -> str:
+  'Get the web server protocol specified by the environment variable WEB_PROTO. Defaults to `http`.'
+  return environ.get('WEB_PROTO', 'http')
 
 
 def web_host() -> str:
@@ -26,21 +26,37 @@ def web_host() -> str:
   return environ.get('WEB_HOST', 'localhost')
 
 
-def is_web_https() -> bool:
-  'Return True if the environment has WEB_HTTPS set to a common true-like string value.'
-  return bool_str_vals[environ.get('WEB_HTTPS', '0')]
+def web_port() -> int:
+  'Get the server port specified by the environment variable WEB_PORT. Defaults to 8000.'
+  return int(environ.get('WEB_PORT', '8000'))
 
 
-def web_external_addr(default:str|Raise=Raise._) -> str:
+def web_addr() -> str:
+  'Get the web server address specified by the environment variables WEB_PROTO, WEB_HOST, and WEB_PORT.'
+  return f'{web_proto()}://{web_host()}:{web_port()}'
+
+
+def web_external_host(default:str|Raise=Raise._) -> str:
   '''
-  Return the external address of the web server, specified by the environment variables WEB_EXTERNAL_ADDR.
-  Returns default if specified (and not `Raise._`) or else raises KeyError.
+  Get the external web server host name specified by the environment variable WEB_EXTERNAL_HOST.
+  This function does not default to `web_host()` because doing so can lead to errors in production.
+  If the environment variable is not set, return `default`, or raise KeyError.
   '''
   try:
-    return environ['WEB_EXTERNAL_ADDR']
+    return environ['WEB_EXTERNAL_HOST']
   except KeyError:
     if default is Raise._: raise
     return default
+
+
+def web_external_addr() -> str:
+  '''
+  Return the external address of the web server, defined to use the "https:" protocol and WEB_EXTERNAL_HOST.
+  Raises KeyError if WEB_EXTERNAL_HOST is not set.
+  '''
+  host = web_external_host()
+  if host == 'localhost': return web_addr()
+  return f'https://{host}'
 
 
 def web_dev_uid() -> int:
