15fcfdd0b428c659d62220ddde41507f829160ef
George King <george@theorypractice.dev>
2025-06-10 09:13:35 -0700

    pithy.default: add @final to Default, Raise.

--- pithy/default.py
+++ pithy/default.py
@@ -1,12 +1,13 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 from enum import Enum
-from typing import TypeVar, Union
+from typing import final, TypeVar, Union
 
 
 _T = TypeVar('_T')
 
 
+@final
 class Default(Enum):
   '''
   Singleton class and value to indicate a default parameter value,
@@ -18,6 +19,7 @@ class Default(Enum):
   _ = 0
 
 
+@final
 class Raise(Enum):
   '''
   Raise serves the same purpose as Default,
