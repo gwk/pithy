f4d37265194a740553c3645896d646a49d39ccca
George King <george@theorypractice.dev>
2025-10-09 13:39:23 -0700

    Comment.

--- pithy/encodings.py
+++ pithy/encodings.py
@@ -35,6 +35,7 @@ assert len(base62_alphabet) == 62
 # In VSCode, the double-click behavior also works for the numeric characters '²³¹¼½¾',
 # but in macOS applications only the ten ASCII digits will double-click as a block.
 # Older versions of macOS had problems outputting the 'ÿ' on the command line, but this appears to be fixed as of 2024-10.
+# I believe this was a bug in editline/libedit.
 base128_alphabet = (
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyzª'
   'µºÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ').encode('latin1')
