7aabc633ceeebb303fab3919d455bf51158d0ac9
George King <george@theorypractice.dev>
2025-11-21 11:57:18 -0800

    pithy.logging: refactor render_log_fn; add colors.

--- pithy/logging.py
+++ pithy/logging.py
@@ -1,9 +1,10 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from json import dump as write_json
+from json import dumps as render_json
 from sys import stdout
 from typing import Any, Literal
 
+from .ansi import RST, TXT_C, TXT_G, TXT_N, TXT_R, TXT_Y
 from .encode import encode_obj
 
 
@@ -14,6 +15,13 @@ This is a very lightweight logging implementation.
 LogLevel = Literal['debug', 'info', 'warn', 'error']
 
 log_levels:tuple[str,...] = LogLevel.__args__ # type: ignore[attr-defined]
+
+log_level_colors = {
+  'debug': TXT_C,
+  'info': TXT_G,
+  'warn': TXT_Y,
+  'error': TXT_R,
+}
 
 
 log_file = stdout
@@ -26,59 +34,68 @@ def logD(_:str, **kwargs:Any) -> None:
   '''
   Log a debug message.
   '''
-  log_fn('debug', _, **kwargs)
+  _log('debug', _, **kwargs)
 
 
 def logI(_:str, **kwargs:Any) -> None:
   '''
   Log an info message.
   '''
-  log_fn('info', _, **kwargs)
+  _log('info', _, **kwargs)
 
 
 def logW(_:str, **kwargs:Any) -> None:
   '''
   Log a warning message.
   '''
-  log_fn('warn', _, **kwargs)
+  _log('warn', _, **kwargs)
+
 
 def logE(_:str, **kwargs:Any) -> None:
   '''
   Log an error message.
   '''
-  log_fn('error', _, **kwargs)
+  _log('error', _, **kwargs)
 
 
 def log(level:str, _:str, **kwargs:Any) -> None:
   '''
   Log a message at the given level with optional key-value pairs.
   '''
-  if level not in log_levels: log_fn('info', f'Invalid log level: {level}.')
-  log_fn(level, _, **kwargs)
+  if level not in log_levels: print(render_log_fn('info', f'Invalid log level: {level}.'))
+  print(render_log_fn(level, _, **kwargs), flush=True)
 
 
-def log_as_text(level:str, _:str='', **kwargs:Any) -> None:
+def _log(level:str, _:str, **kwargs:Any) -> None:
+  '''
+  Internal log function that chooses render function and flushes output.
+  '''
+  print(render_log_fn(level, _, **kwargs), flush=True)
+
+
+
+def render_log_text(level:str, _:str='', **kwargs:Any) -> str:
   '''
   Log a message at the given level with optional key-value pairs.
   The output format is logfmt.
   '''
   msg = (' ' + _) if _ else ''
-  pairs = '  '.join(f'{k}:{v!r}' for k,v in kwargs.items())
+  pairs = '  '.join(f'{TXT_N}{k}{RST}:{v!r}' for k,v in kwargs.items())
   if pairs: pairs = '  ' + pairs
-  print(f'{level}:', msg, pairs, sep='', file=log_file)
+  clr = log_level_colors.get(level, '')
+  rst = RST if clr else ''
+  return f'{clr}{level}{rst}:{msg}{pairs}'
 
 
-def log_as_json(level:str, _:str='', **kwargs:Any) -> None:
+def render_log_json(level:str, _:str='', **kwargs:Any) -> str:
   '''
-  Log a message at the given level with optional key-value pairs.
-  The output format is JSON.
+  Render a log message as a JSON string.
   '''
   # Construct the log record so that level and message are rendered first.
   items = {'level': level}
-  if _: kwargs['_'] = _
+  if _: items['_'] = _
   items.update(kwargs)
-  write_json(items, log_file, ensure_ascii=False, indent=None, separators=(',', ':'), default=encode_obj)
-  print(file=log_file)
+  return render_json(items, ensure_ascii=False, indent=None, separators=(',', ':'), default=encode_obj)
 
 
-log_fn = log_as_text if is_log_tty else log_as_json
+render_log_fn = render_log_text if is_log_tty else render_log_json
