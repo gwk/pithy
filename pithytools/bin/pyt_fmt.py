fb410ad09eb2b74e9185a546aed1577bfd57e50a
George King <george@theorypractice.dev>
2026-01-12 15:37:13 -0800

    pithytools.bin.pyt_fmt: support directory output arg.

--- pithytools/bin/pyt_fmt.py
+++ pithytools/bin/pyt_fmt.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 from sys import stdout
 
 from pithy.loader import load
+from pithy.path import path_join, path_name_stem
 
 
 def main() -> None:
@@ -11,7 +12,8 @@ def main() -> None:
   parser.add_argument('template', help='the template file.')
   parser.add_argument('-args', required=True,
     help='path to a file containing argument data for the template. Supports all pithy.loader formats that return dictionaries.')
-  parser.add_argument('-output', help='path to write the formatted output; defaults to stdout.')
+  parser.add_argument('-output',
+    help='path to write the formatted output; defaults to stdout. A trailing slash indicates a directory.')
 
   args = parser.parse_args()
 
@@ -23,10 +25,16 @@ def main() -> None:
   if not isinstance(args_content, dict):
     exit(f'pyt_fmt error: args file must contain a dictionary; received: {type(args_content).__name__}')
 
-  formatted_content = template_content.format(**args_content)
+  try: formatted_content = template_content.format(**args_content)
+  except KeyError as e:
+    exit(f'pyt_fmt error: missing argument for template placeholder: {e}')
 
-  if args.output:
-    with open(args.output, 'w') as f:
+  if output_path := args.output:
+    if output_path.endswith('/'):
+      if not args.template.endswith('.pyt'):
+        exit('pyt_fmt error: when specifying an output directory, the template file must have a .pyt extension.')
+      output_path = path_join(output_path, path_name_stem(args.template))
+    with open(output_path, 'w') as f:
       f.write(formatted_content)
   else:
     stdout.write(formatted_content)
