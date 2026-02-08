f5f1d8cc16403b19d60c89da068c9d2cec698aa5
George King <george@theorypractice.dev>
2026-02-08 12:10:49 -0800

    pithy.digest: fix module docstring.

--- pithy/digest.py
+++ pithy/digest.py
@@ -1,6 +1,6 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-'Print file hashes. The first argument is the format; remaining args are the paths.'
+'Digest functions by name.'
 
 import hashlib
 from typing import Callable
