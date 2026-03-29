75946232d98023a75d41837a50ee00d1a6c62bbc
George King <george@theorypractice.dev>
2026-03-29 12:34:17 -0700

    pithy.json.fmt: simplify state machine logic of fmt_json_bytes.

--- pithy/json/fmt.py
+++ pithy/json/fmt.py
@@ -8,16 +8,18 @@ from warnings import warn
 def fmt_json_bytes(bytes_or_file:bytes|Reader[bytes]) -> Iterator[bytes]:
   '''
   Format JSON bytes. The JSON input does not have to be well-formed.
-  The output is the input, with alterations only to whitespace.
-  This is useful for pretty-printing JSON that is malformed.
+  This is useful for pretty-printing JSON, including input that is malformed.
+  The output identical to the input, with the following changes:
+  * space, '\n', '\r', '\t' are replaced with formatted whitespace (spaces and newlines only).
   The output style is lispy, with closing braces/brackets on the same line as the last item.
   '''
   file:Reader[bytes] = BytesIO(bytes_or_file) if isinstance(bytes_or_file, bytes) else bytes_or_file
 
-  s_start, s_open, s_close, s_comma, s_colon, s_mid, s_str, s_str_esc = range(8)
+  s_start, s_open_inline, s_open_break, s_close, s_comma, s_colon, s_mid, s_str, s_str_esc = range(9)
   # States:
   # s_start: before any tokens have been seen.
-  # s_open: an open token (`{` or `[`).
+  # s_open_inline: an open token (`{` or `[`) whose first child will be on the same line.
+  # s_open_break: an open token (`{` or `[`) whose first child will be on the next line.
   # s_close: a close token (`}` or `]`).
   # s_comma: a comma token (`,`).
   # s_colon: a colon token (`:`).
@@ -27,7 +29,6 @@ def fmt_json_bytes(bytes_or_file:bytes|Reader[bytes]) -> Iterator[bytes]:
 
   indent = 0
   state = s_start
-  is_next_inline = True
   while byte := file.read(1):
     prev_state = state
 
@@ -44,7 +45,15 @@ def fmt_json_bytes(bytes_or_file:bytes|Reader[bytes]) -> Iterator[bytes]:
     elif byte == b'"':
       state = s_str
     elif byte in b'{[':
-      state = s_open
+      # Open brackets preceded by a newline or the document start inline their first child.
+      # s_open_inline propagates: consecutive inline opens all inline their first child.
+      # s_open_break precedes a newline, so the following open is inlined.
+      # s_comma and s_close both trigger a newline before the next token, so the following item can is inlined.
+      # All other predecessors (s_colon, s_mid, s_str) are mid-line, requiring s_open_break.
+      if prev_state in (s_start, s_open_inline, s_open_break, s_comma, s_close):
+        state = s_open_inline
+      else:
+        state = s_open_break
     elif byte in b'}]':
       state = s_close
     elif byte == b',':
@@ -56,28 +65,23 @@ def fmt_json_bytes(bytes_or_file:bytes|Reader[bytes]) -> Iterator[bytes]:
 
     if (
      prev_state == s_colon or
-     (prev_state == s_open and is_next_inline) or
-     (prev_state not in (s_open, s_close) and state == s_close)):
+     prev_state == s_open_inline or
+     (prev_state not in (s_open_inline, s_open_break, s_close) and state == s_close)):
       yield b' '
-    elif prev_state == s_open and state != s_close:
+    elif prev_state == s_open_break and state != s_close:
       yield b'\n'
       yield b'  ' * indent
-      is_next_inline = True
 
     if prev_state in (s_comma, s_close) and state not in (s_comma, s_close): # Done closing; emit newline/indent.
       yield b'\n'
       yield b'  ' * indent
-      is_next_inline = True
 
     yield byte
 
-    if state == s_open:
+    if state in (s_open_inline, s_open_break):
       indent += 1
     elif state == s_close:
       indent -= 1
-
-    if state != s_open:
-      is_next_inline = False
 
   # Final newline for non-empty output.
   if state != s_start:
