c51fb3ef75354e8ba4954798ce0b74809648245e
George King <george@theorypractice.dev>
2026-05-22 11:35:07 -0700

    pithy.defaultlist: fix fill behavior for slicing and boundary cases; add annotations.

--- pithy_/pithy/defaultlist.py
+++ pithy_/pithy/defaultlist.py
@@ -1,6 +1,6 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from typing import Callable, Iterable, overload, SupportsIndex, TypeVar
+from typing import Callable, cast, Iterable, overload, SupportsIndex, TypeVar
 
 
 _T = TypeVar('_T')
@@ -15,40 +15,52 @@ class DefaultList(list[_T]):
 
   def __init__(self, factory:Callable[[int], _T], iterable:Iterable[_T]=(), fill_length:int=0):
     super().__init__(iterable)
+    if not callable(factory): raise TypeError('factory must be callable')
     self.factory = factory
-    for i in range(fill_length):
+    for i in range(len(self), fill_length):
       self.append(factory(i))
 
   @overload
-  def __getitem__(self, index: SupportsIndex, /) -> _T: ...
+  def __getitem__(self, index:SupportsIndex, /) -> _T: ...
 
   @overload
-  def __getitem__(self, index: slice, /) -> list[_T]: ...
+  def __getitem__(self, index:slice, /) -> list[_T]: ...
 
-  def __getitem__(self, index, /):
+  def __getitem__(self, index:SupportsIndex|slice, /) -> _T|list[_T]:
     if isinstance(index, slice):
       end = len(self) if index.stop is None else index.stop
     else:
-      end = index + 1
+      end = int(index) + 1
     for i in range(len(self), end):
       self.append(self.factory(i))
     return super().__getitem__(index)
 
 
   @overload
-  def __setitem__(self, index: SupportsIndex, value: _T, /) -> None: ...
+  def __setitem__(self, index:SupportsIndex, value:_T, /) -> None: ...
 
   @overload
-  def __setitem__(self, index: slice, value: Iterable[_T], /) -> None: ...
+  def __setitem__(self, index:slice, value:Iterable[_T], /) -> None: ...
 
-  def __setitem__(self, index, value, /):
+  def __setitem__(self, index:SupportsIndex|slice, value:_T|Iterable[_T], /) -> None:
     if isinstance(index, slice):
-      end = len(self) if index.stop is None else index.stop
+      # `list` has rather weird behavior when the slice indices are out of bounds:
+      # it appends the values to the end, regardless of any "gap" between len(self) and slc.start.
+      # DefaultList instead fills up to slc.start, provided that slc.start is non-negative.
+      if index.start is not None and index.start > len(self):
+        for i in range(len(self), index.start):
+          self.append(self.factory(i))
+      super().__setitem__(index, cast(Iterable[_T], value))
     else:
-      end = index + 1
-    for i in range(len(self), end):
-      self.append(self.factory(i))
-    super().__setitem__(index, value)
+      # If index is positive and out-of-bounds, then fill up to it.
+      idx = int(index)
+      if idx > len(self):
+        for i in range(len(self), idx):
+          self.append(self.factory(i))
+      if idx == len(self): # super __setitem__ will fail with IndexError.
+        self.append(cast(_T, value))
+      else:
+        super().__setitem__(index, cast(_T, value))
 
 
   def __repr__(self) -> str:
--- pithy_/pithy/defaultlist.ut.py
+++ pithy_/pithy/defaultlist.ut.py
@@ -1,18 +1,39 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from operator import getitem, setitem
+from operator import getitem
+from typing import Iterable
 
 from pithy.defaultlist import DefaultList
 from utest import utest, utest_seq
 
 
-utest_seq([0, 1], DefaultList, lambda i: i, fill_length=2)
-utest_seq([-1, -1], DefaultList, lambda i: i, [-1, -1])
+ident = lambda idx: idx
+
+utest_seq([0, 1], DefaultList, ident, fill_length=2)
+utest_seq([-1, -1], DefaultList, ident, [-1, -1])
+utest_seq([-1, -1], DefaultList, ident, [-1, -1], fill_length=2)
+utest_seq([-1, -1, 2, 3], DefaultList, ident, [-1, -1], fill_length=4)
+
 
 for i in range(3):
-  utest(i, getitem, DefaultList(lambda i: i), i)
+  utest(i, getitem, DefaultList(ident), i)
 
-for i in range(3):
-  l = DefaultList(lambda i: i)
-  utest(None, setitem, l, i, 9)
-  utest(9, getitem, l, i)
+
+def set_idx_and_ret[T](l:list[T], idx:int, val:T) -> list[T]:
+ l[idx] = val
+ return l
+
+
+def set_slc_and_ret[T](l:list[T], slc:slice, val:Iterable[T]) -> list[T]:
+ l[slc] = val
+ return l
+
+
+utest_seq([-1], set_idx_and_ret, DefaultList(ident), 0, -1)
+utest_seq([0, 1, -1], set_idx_and_ret, DefaultList(ident), 2, -1)
+
+utest_seq([-1, -2], set_slc_and_ret, [], slice(0, 2), [-1, -2])
+utest_seq([-1, -2], set_slc_and_ret, DefaultList(ident), slice(0, 2), [-1, -2])
+
+utest_seq([-3, -4], set_slc_and_ret, [], slice(2, 4), [-3, -4]) # Builtin list behavior with out-of-bounds slices is weird.
+utest_seq([0, 1, -3, -4], set_slc_and_ret, DefaultList(ident), slice(2, 4), [-3, -4])
