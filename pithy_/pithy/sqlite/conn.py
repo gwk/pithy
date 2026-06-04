bc92b0af3dc06e8acae7429ed218ab23ece78204
George King <george@theorypractice.dev>
2026-06-04 16:25:45 -0700

    pithy.sqlite.conn: attach() no longer performs ATTACH inside of an unintentional transaction.

--- pithy_/pithy/sqlite/conn.py
+++ pithy_/pithy/sqlite/conn.py
@@ -113,7 +113,7 @@ class Conn(sqlite3.Connection):
     `mode` must be one of '' (default, omitted in the SQL statement), 'ro', 'rw', 'rwc', or 'memory'.
     '''
     uri = sqlite_file_uri(path, mode=mode)
-    self.execute(f'ATTACH DATABASE {sql_quote_entity(uri)} AS {sql_quote_entity(name)}')
+    self.cursor().execute(f'ATTACH DATABASE {sql_quote_entity(uri)} AS {sql_quote_entity(name)}')
 
 
   def validate(self, query:str) -> None:
