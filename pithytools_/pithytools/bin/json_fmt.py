b1321cfd198ad5a4b557d0c233f56d68b1ef66b6
George King <george@theorypractice.dev>
2026-07-05 09:49:57 -0700

    pithytools json_fmt_in_place: use write_formatted_json_bytes.
    json-fmt, json-fmt-in-place: add CLI options; use write_formatted_json_bytes.

--- pithytools_/pithytools/bin/json_fmt.py
+++ pithytools_/pithytools/bin/json_fmt.py
@@ -1,22 +1,25 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-from sys import argv, stdin, stdout
+from argparse import ArgumentParser
+from sys import stdin, stdout
 
 from pithy.json.fmt import write_formatted_json_bytes
 
 
 def main() -> None:
+  parser = ArgumentParser(description='Format JSON input.')
+  parser.add_argument('paths', nargs='*', default=['-'], help='input file paths; defaults to stdin.')
+  parser.add_argument('-fix', action='store_true', help='fix malformed JSON (missing/extra commas; comments).')
+  parser.add_argument('-trailing-commas', action='store_true', help='allow trailing commas in fixed output.')
+  parser.add_argument('-comments', action='store_true', help='preserve comments instead of stripping them.')
+  args = parser.parse_args()
+
   out_raw = stdout.buffer
-  args = argv[1:]
-  if not args:
-    args = ['-']
-  for path in args:
-    if path == '-':
-      f = stdin.buffer
-    else:
-      f = open(path, 'rb')
+  for path in args.paths:
+    f = stdin.buffer if path == '-' else open(path, 'rb')
     with f:
-      write_formatted_json_bytes(out_raw, f, fix=False)
+      write_formatted_json_bytes(out_raw, f, fix=args.fix, allow_trailing_commas=args.allow_trailing_commas,
+        allow_comments=args.allow_comments)
 
 
 if __name__ == '__main__': main()
--- pithytools_/pithytools/bin/json_fmt_in_place.py
+++ pithytools_/pithytools/bin/json_fmt_in_place.py
@@ -1,21 +1,30 @@
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
-import json
-import sys
+from argparse import ArgumentParser
 
 from pithy.fs import walk_files
 from pithy.io import errL
+from pithy.json.fmt import write_formatted_json_bytes
 
 
 def main() -> None:
+  parser = ArgumentParser(description='Format JSON files in place.')
+  parser.add_argument('paths', nargs='*', help='paths to search for JSON files; directories are walked recursively.')
+  parser.add_argument('-fix', action='store_true', help='fix malformed JSON (missing/extra commas; comments).')
+  parser.add_argument('-trailing-commas', action='store_true', help='allow trailing commas in fixed output.')
+  parser.add_argument('-comments', action='store_true', help='preserve comments instead of stripping them.')
+  args = parser.parse_args()
 
-  for path in walk_files(*sys.argv[1:], file_exts='.json'):
+  for path in walk_files(*args.paths, file_exts=['.json', '.jsonc']):
     print(path)
     try:
-      with open(path) as f:
-        o = json.load(f)
-      with open(path, 'w') as f:
-        json.dump(o, f, sort_keys=True, indent=2)
-        f.write('\n')
+      with open(path, 'rb') as f_in:
+        input = f_in.read()
+      with open(path, 'wb') as f_out:
+        write_formatted_json_bytes(f_out, input, fix=args.fix, allow_trailing_commas=args.trailing_commas,
+          allow_comments=args.comments)
     except Exception as e:
       errL(f'error: {path}: {e}')
+
+
+if __name__ == '__main__': main()
