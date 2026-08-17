cb924c83ad0543fe0cfedc5e33d91bac99aa684a
George King <george@theorypractice.dev>
2026-08-17 14:10:43 -0700

    Bump vector to 0.57.0.

--- ops/linux/vector/install.bash
+++ ops/linux/vector/install.bash
@@ -9,15 +9,15 @@ src_dir=$(dirname $0)
 cd "$src_dir"
 
 machine_arch=$(uname -m)
-vector_version='0.55.0'
+vector_version='0.57.0'
 
 case "${machine_arch}" in
   "x86_64"|"amd64")
     vector_arch="x86_64"
-    vector_sha256='e0221681b1cd1f93c46008fde19c5ac5811718d10a803a4e320ff4e72ab9e4a9' ;;
+    vector_sha256='4d156e6859e235b366f5b77121ae59d5440c93acab215c45f30f3fc839d20f65' ;;
   "aarch64"|"arm64")
     vector_arch="aarch64"
-    vector_sha256='00ce049bd42291165eb207b413e9fa8afacacf2e8ac4312e7dc89488a6ec4e4c' ;;
+    vector_sha256='6290f6fae406b61272cdf849e9cc9fa015761efbb592d2359d86ec4345d10aa3' ;;
   *)
     fail "Unsupported architecture: ${machine_arch}" ;;
 esac
