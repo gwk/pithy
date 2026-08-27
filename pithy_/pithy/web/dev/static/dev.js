77f0462907c4b0c391adc97021e46f0e50233531
George King <george@theorypractice.dev>
2026-08-27 14:22:18 -0700

    pithy.web.dev: common page layout.

---
+++ pithy_/pithy/web/dev/static/dev.js
@@ -0,0 +1,10 @@
+// Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
+'use strict';
+
+addEventListener('DOMContentLoaded', () => {
+  const currentPath = window.location.pathname;
+  const currentLink = [...document.querySelectorAll('body > nav a')]
+    .find(link => link.pathname === currentPath);
+  if (currentLink) currentLink.classList.add('current-nav-section-link');
+});
